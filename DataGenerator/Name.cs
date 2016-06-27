966edb5b02dd5624e93195146a1f03d5399a0391
Robert Andersson <inputfalken@gmail.com>
2016-06-28 00:32:51 +0200

    init

---
+++ DataGenerator/Name.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DataGenerator
+{
+    internal class Name
+    {
+        private static readonly Random Random = new Random();
+        private static List<string> FirstNames { get; }
+        private static List<string> LastNames { get; }
+        private string FirstName { get; }
+        private string LastName { get; }
+        /// <summary>
+        ///     Will run once to populate lists which will contain random data reed from text files
+        /// </summary>
+        static Name() {
+            FirstNames = ReadFromFile("data/firstNames.txt");
+            LastNames = ReadFromFile("data/lastNames.txt");
+        }
+
+        /// <summary>
+        ///     Will run on every instance to give data
+        /// </summary>
+        public Name() {
+            lock (Random)
+                FirstName = FirstNames[Random.Next(FirstNames.Count)];
+            lock (Random)
+                LastName = LastNames[Random.Next(LastNames.Count)];
+        }
+
+
+        private static List<string> ReadFromFile(string filePath) {
+            var list = new List<string>();
+            using (var reader = new StreamReader(filePath)) {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    list.Add(line);
+
+                return list;
+            }
+        }
+        public override string ToString() => $"{FirstName} {LastName}";
+    }
+}
---
+++ DataGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGenerator
+{
+    internal static class Program
+    {
+        private static void Main(string[] args) {
+            var name = new Name();
+        }
+    }
+}
