62c669494e15363cc99258140b9fe704b205469c
Robert Andersson <inputfalken@gmail.com>
2016-06-29 15:01:08 +0200

    currently dies on population of lists

--- DataGenerator/Program.cs
+++ DataGenerator/Program.cs
@@ -9,18 +9,20 @@ namespace DataGenerator
     {
         //TODO ADD A CONTAINER CLASS NAMES DataGenHelperClass Which name and other data can be called from
         private static void Main(string[] args) {
-            var names = new List<Name>();
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+            //var names = new List<Name>();
+            //var stopwatch = new Stopwatch();
+            //stopwatch.Start();
 
-            for (var i = 0; i < 1000; i++) {
-                names.Add(new Name());
-            }
-            stopwatch.Stop();
+            //for (var i = 0; i < 1000; i++) {
+            //    names.Add(new Name());
+            //}
+            //stopwatch.Stop();
 
 
-            names.ForEach(Console.WriteLine);
-            Console.WriteLine(stopwatch.Elapsed);
+            //names.ForEach(Console.WriteLine);
+            //Console.WriteLine(stopwatch.Elapsed);
+            var country = new Country();
+            Console.WriteLine(country.Title);
         }
     }
 }
--- DataGenerator/Types/Country.cs
+++ DataGenerator/Types/Country.cs
@@ -6,12 +6,31 @@ using System.Threading.Tasks;
 
 namespace DataGenerator.Types
 {
-    //TODO CREATE HELPER CLASS
     internal class Country
     {
-        private static IReadOnlyList<string> ReadOnlyList { get; set; }
+        private static List<string> Titles { get; set; }
+
+        private static List<string> Initials { get; set; }
+        public string Title { get; private set; }
+
+        public string Initial { get; private set; }
+
         static Country() {
-            ReadOnlyList = DataGenHelperClass.ReadFromFile("Country/country.txt");
+            var readFromFile = DataGenHelperClass.ReadFromFile("Country/country.txt");
+            //foreach (var line in readFromFile) {
+            //    var strings = line.Split('|');
+            //    Initials.Add(strings[0]);
+            //    Titles.Add(strings[1]);
+            //}
+            var strings = readFromFile[0].Split('|');
+            //PROGRAMS DIES HERE
+            Titles.Add(strings[1]);
+            Initials.Add(strings[0]);
+        }
+
+        public Country() {
+            Title = DataGenHelperClass.FetchRandomItem(Titles);
+            Initial = DataGenHelperClass.FetchRandomItem(Initials);
         }
     }
 }
