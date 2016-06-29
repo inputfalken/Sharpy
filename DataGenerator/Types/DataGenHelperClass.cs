f3f7465c4299619a0c7cc0c8589f9fc5d18c6ad9
Robert Andersson <inputfalken@gmail.com>
2016-06-29 02:54:21 +0200

    added class to contain static methods which will be used alot

---
+++ DataGenerator/Types/DataGenHelperClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGenerator.Types
+{
+    internal static class DataGenHelperClass
+    {
+        private static readonly Random Random = new Random();
+        private static string Path { get; } = "Data/Types/";
+
+        public static IReadOnlyList<string> ReadFromFile(string filePath) {
+            var list = new List<string>();
+            using (var reader = new StreamReader(Path + filePath)) {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    list.Add(line);
+
+                return list;
+            }
+        }
+
+        public static T FetchRandomItem<T>(IReadOnlyList<T> iReadOnlyList)
+            => iReadOnlyList[Randomer(iReadOnlyList.Count)];
+
+        private static int Randomer(int limit) {
+            lock (Random)
+                return Random.Next(limit);
+        }
+    }
+}
