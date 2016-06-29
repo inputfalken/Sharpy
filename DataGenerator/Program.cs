6696f03380ed6f28742f068fd6683dab38a49738
Robert Andersson <inputfalken@gmail.com>
2016-06-29 16:07:05 +0200

    cleared code

--- DataGenerator/Program.cs
+++ DataGenerator/Program.cs
@@ -9,14 +9,8 @@ namespace DataGenerator
     {
         //TODO ADD A CONTAINER CLASS NAMES DataGenHelperClass Which name and other data can be called from
         private static void Main(string[] args) {
-            var names = new List<Name>();
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
-            for (var i = 0; i < 1000; i++)
-                names.Add(new Name());
-            stopwatch.Stop();
-
-            Console.WriteLine(stopwatch.Elapsed);
+            
         }
+        
     }
 }
