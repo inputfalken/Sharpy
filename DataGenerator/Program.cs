6dd527b6876a2977dc78fa3b03c8824ce56adfba
Robert Andersson <inputfalken@gmail.com>
2016-06-29 15:57:09 +0200

    removed brackets from forloop

--- DataGenerator/Program.cs
+++ DataGenerator/Program.cs
@@ -9,18 +9,14 @@ namespace DataGenerator
     {
         //TODO ADD A CONTAINER CLASS NAMES DataGenHelperClass Which name and other data can be called from
         private static void Main(string[] args) {
-            //var names = new List<Name>();
-            //var stopwatch = new Stopwatch();
-            //stopwatch.Start();
+            var names = new List<Name>();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (var i = 0; i < 1000; i++)
+                names.Add(new Name());
+            stopwatch.Stop();
 
-            //for (var i = 0; i < 1000; i++) {
-            //    names.Add(new Name());
-            //}
-            //stopwatch.Stop();
-
-
-            //names.ForEach(Console.WriteLine);
-            //Console.WriteLine(stopwatch.Elapsed);
+            Console.WriteLine(stopwatch.Elapsed);
         }
     }
 }
