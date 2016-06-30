9299cfaa9f6e495656fb5bfb4e2943740c79bf50
Robert Andersson <inputfalken@gmail.com>
2016-06-30 12:20:35 +0200

    Made code easier to read

--- DataGenerator/Types/Name/FirstName.cs
+++ DataGenerator/Types/Name/FirstName.cs
@@ -6,30 +6,28 @@ namespace DataGenerator.Types.Name
     internal class FirstName : Name
     {
         private const string Directory = "Name";
-        private static IReadOnlyList<string> Male { get; set; }
-        private static IReadOnlyList<string> Female { get; set; }
-        private static IReadOnlyList<string> Mixed { get; set; }
 
         public FirstName(Gender gender) {
             switch (gender) {
                 case Gender.Female:
-                    if (Female == null)
-                        Female = DataGenHelperClass.ReadFromFile($"{Directory}/femaleNames.txt");
+                    Female = Female ?? DataGenHelperClass.ReadFromFile($"{Directory}/femaleNames.txt");
                     Data = DataGenHelperClass.FetchRandomItem(Female);
                     break;
                 case Gender.Male:
-                    if (Male == null)
-                        Male = DataGenHelperClass.ReadFromFile($"{Directory}/maleNames.txt");
+                    Male = Male ?? DataGenHelperClass.ReadFromFile($"{Directory}/maleNames.txt");
                     Data = DataGenHelperClass.FetchRandomItem(Male);
                     break;
                 case Gender.Mixed:
-                    if (Mixed == null)
-                        Mixed = DataGenHelperClass.ReadFromFile($"{Directory}/firstNames.txt");
+                    Mixed = Mixed ?? DataGenHelperClass.ReadFromFile($"{Directory}/firstNames.txt");
                     Data = DataGenHelperClass.FetchRandomItem(Mixed);
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(gender), gender, null);
             }
         }
+
+        private static IReadOnlyList<string> Male { get; set; }
+        private static IReadOnlyList<string> Female { get; set; }
+        private static IReadOnlyList<string> Mixed { get; set; }
     }
 }
