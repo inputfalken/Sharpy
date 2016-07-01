f190b9f98c28f117e0fcdda116d87a69e4de5b1c
Robert Andersson <inputfalken@gmail.com>
2016-07-01 21:40:23 +0200

    renamed factory

---
+++ DataGenerator/DataFactory.cs
@@ -0,0 +1,21 @@
+﻿using DataGenerator.Types;
+using DataGenerator.Types.Name;
+
+namespace DataGenerator
+{
+    internal class DataFactory
+    {
+        //Create a method where the user can use the _datagenerator on a provided list
+        private readonly IGenerator<string> _dataGenerator;
+
+        public DataFactory(IGenerator<string> dataGenerator) {
+            _dataGenerator = dataGenerator;
+        }
+
+
+        public Data<string> FirstName(Gender gender)
+            => new FirstName(gender, _dataGenerator);
+
+        public Data<string> Country() => new Country(_dataGenerator);
+    }
+}
--- DataGenerator/Program.cs
+++ DataGenerator/Program.cs
@@ -11,10 +11,11 @@ namespace DataGenerator
         private static void Main(string[] args) {
             var stopwatch = new Stopwatch();
             var names = new List<Data<string>>();
+            var dataFactory = new DataFactory(new RandomGenerator());
+
             stopwatch.Start();
-            var stringGenerator = Factory.StringGenerator(new RandomGenerator());
             for (var i = 0; i < 10000; i++)
-                names.Add(stringGenerator.FirstName(Gender.Female));
+                names.Add(dataFactory.FirstName(Gender.Female));
             stopwatch.Stop();
             //TODO OVERRIDE TOSTRING
             Console.WriteLine(names[0].ToString());
