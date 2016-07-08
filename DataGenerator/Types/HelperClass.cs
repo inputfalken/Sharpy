3525218db5aa650dfbbad49e362b65cfaf737d2a
Robert Andersson <inputfalken@gmail.com>
2016-07-08 03:08:56 +0200

    removed unused method

--- DataGenerator/Types/HelperClass.cs
+++ DataGenerator/Types/HelperClass.cs
@@ -8,18 +8,6 @@ namespace DataGenerator.Types
     {
         private static readonly Random Random = new Random();
 
-
-        public static List<string> ReadFromFile(string filePath) {
-            var list = new List<string>();
-            using (var reader = new StreamReader(filePath)) {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                    list.Add(line);
-
-                return list;
-            }
-        }
-
         public static int Randomer(int limit) {
             lock (Random)
                 return Random.Next(limit);
