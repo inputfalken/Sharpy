4e1a498969d47c45ef44158cbc31960e3a8cb716
Robert Andersson <inputfalken@gmail.com>
2016-07-14 17:35:56 +0200

    added class named filter to contain various filters which contains a filter to remove duplicated data

---
+++ DataGenerator/Types/Filter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGenerator.Types {
+    public static class Filter {
+        /// <summary>
+        ///     Filters repeated strings from argument
+        /// </summary>
+        /// <param name="enumerable"></param>
+        /// <returns></returns>
+        public static List<T> RepeatedData<T>(IEnumerable<T> enumerable)
+            => enumerable.GroupBy(s => s)
+                .Where(g => g.Any())
+                .Select(grouping => grouping.Key)
+                .ToList();
+    }
+}
--- DataGenerator/Types/Name/NameFactory.cs
+++ DataGenerator/Types/Name/NameFactory.cs
@@ -15,16 +15,6 @@ namespace DataGenerator.Types.Name {
             _nameRepositories = JsonConvert.DeserializeObject<IEnumerable<NameRepository>>(File.ReadAllText(FilePath));
         }
 
-        /// <summary>
-        ///     Filters repeated strings from argument
-        /// </summary>
-        /// <param name="enumerable"></param>
-        /// <returns></returns>
-        private static List<string> RemoveDuplicatedName(IEnumerable<string> enumerable)
-            => enumerable.GroupBy(s => s)
-                .Where(g => g.Any())
-                .Select(grouping => grouping.Key)
-                .ToList();
 
         /// <summary>
         ///     Initialises a function that generates last names whose data is not filtered at all.
@@ -33,14 +23,14 @@ namespace DataGenerator.Types.Name {
         ///     Returns a function which will generate a random last name without any filtering
         /// </returns>
         public Func<string> LastNameInitialiser()
-            => GenerateName(RemoveDuplicatedName(_nameRepositories.SelectMany(repository => repository.LastNames)));
+            => GenerateName(Filter.RepeatedData(_nameRepositories.SelectMany(repository => repository.LastNames)));
 
         /// <summary>
         /// Returns a collection of unique last names whose data is not filtered
         /// </summary>
         /// <returns></returns>
         public List<string> LastNameCollection()
-            => RemoveDuplicatedName(_nameRepositories.SelectMany(repository => repository.LastNames));
+            => Filter.RepeatedData(_nameRepositories.SelectMany(repository => repository.LastNames));
 
         /// <summary>
         ///     Initialises a function that generates last names whose data is filtered by Country.
