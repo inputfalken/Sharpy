0016f7ca92702004b1ff0c874621453293dc9282
Robert Andersson <inputfalken@gmail.com>
2016-07-19 04:34:08 +0200

    removed country files & added a datefile

--- DataGenerator/Program.cs
+++ DataGenerator/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using DataGenerator.Types;
 using DataGenerator.Types.Name;
 
---
+++ DataGenerator/Types/Date/DateFactory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DataGenerator.Types.Date {
+    internal static class DateFactory {
+        public static DateTime BirthDate(int age) {
+            return new DateTime(DateTime.UtcNow.Year - age, 10, 5);
+        }
+    }
+}
