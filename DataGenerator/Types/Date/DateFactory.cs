95d137b1f529621fa97dcd587073b9278d188f5b
Robert Andersson <inputfalken@gmail.com>
2016-07-19 15:15:44 +0200

    added nodatime

--- DataGenerator/Types/Date/DateFactory.cs
+++ DataGenerator/Types/Date/DateFactory.cs
@@ -2,6 +2,7 @@
 
 namespace DataGenerator.Types.Date {
     internal static class DateFactory {
+      //In here create a range of methods doing anything from generating a birth date to generate a date time within a specifik range from arguments....
         public static DateTime BirthDate(int age) {
             return new DateTime(DateTime.UtcNow.Year - age, 10, 5);
         }
