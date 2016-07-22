81af80e9cecbc9d40f2546ab2c47957210f7c7ce
Robert Andersson <inputfalken@gmail.com>
2016-07-22 15:16:16 +0200

    reworked datefactory

--- DataGen/Types/Date/DateFactory.cs
+++ DataGen/Types/Date/DateFactory.cs
@@ -1,74 +1,57 @@
 ﻿using System;
 using NodaTime;
+using static NodaTime.Period;
 
 namespace DataGen.Types.Date {
     public static class DateFactory {
         private static LocalDate CurrentLocalDate
             => SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
 
-        //Instead of method overloading use default values? User could then decide which data should be randomed by using named arguments
-        public static LocalDate RandomFutureDate(int years) {
-            if (years < 0)
-                throw new ArgumentException("Years cannot be negative");
-            return years == 0
-                ? FutureDateWithinCurrentYear()
-                : RandomLocalDate(CurrentLocalDate.Year + years);
-        }
-
-        private static LocalDate FutureDateWithinCurrentYear() {
-            //Random a value between current month and last
-            var month = HelperClass.Randomizer(CurrentLocalDate.Month, 12);
-            var daysInMonth = CalendarSystem.Iso.GetDaysInMonth(CurrentLocalDate.Year, month);
-            //Is true if month gets randomized to current month
-            var date = month == CurrentLocalDate.Month
-                //Makes sure that that you cannot get a date whos date isn't passed
-                ? HelperClass.Randomizer(CurrentLocalDate.Day + 1, daysInMonth)
-                : RandomizeDate(daysInMonth);
-            return new LocalDate(CurrentLocalDate.Year, month, date);
-        }
-
         /// <summary>
-        ///     Returns a LocalDate with a random month and the current year minus years arg
+        /// Will Return the current days plus the arguments
         /// </summary>
         /// <param name="years"></param>
+        /// <param name="month"></param>
+        /// <param name="days"></param>
         /// <returns></returns>
-        public static LocalDate RandomPreviousDate(int years) {
-            if (years < 0)
-                throw new ArgumentException("Argument cannot be negative");
-            return years == 0
-                ? LocalDateWithinCurrentYear()
-                : RandomLocalDate(CurrentLocalDate.Year - years);
+        public static LocalDate AdditionToCurrentDate(int years = 0, int month = 0, int days = 0) {
+            if (years < 0 || month < 0 || days < 0)
+                throw new ArgumentException("Arguments cannot be negative");
+            return CurrentLocalDate
+                .PlusYears(years)
+                .PlusMonths(month)
+                .PlusDays(days);
+        }
+
+        /// <summary>
+        ///     Will Return the current days minus the arguments
+        /// </summary>
+        /// <param name="years"></param>
+        /// <param name="month"></param>
+        /// <param name="days"></param>
+        /// <returns></returns>
+        public static LocalDate SubtractionToCurrentDate(int years = 0, int month = 0, int days = 0) {
+            if (years < 0 || month < 0 || days < 0)
+                throw new ArgumentException("Arguments cannot be negative");
+            return
+                CurrentLocalDate
+                    .Minus(FromYears(years))
+                    .Minus(FromMonths(month))
+                    .Minus(FromDays(days));
         }
 
 
         /// <summary>
-        ///     Makes sure the local date returned cannot be a date in the future
+        /// Will Return a random date at year of the argument
         /// </summary>
+        /// <param name="year"></param>
         /// <returns></returns>
-        private static LocalDate LocalDateWithinCurrentYear() {
-            var month = HelperClass.Randomizer(1, CurrentLocalDate.Month);
-            var daysInMonth = CalendarSystem.Iso.GetDaysInMonth(CurrentLocalDate.Year, month);
-            //Is true if month gets randomized to current month
-            var date = month == CurrentLocalDate.Month
-                //Makes sure that you cannot get the current date or a date which hasn't yet passed 
-                ? HelperClass.Randomizer(1, CurrentLocalDate.Day - 1)
-                : RandomizeDate(daysInMonth);
-            return new LocalDate(CurrentLocalDate.Year, month, date);
-        }
-
-        private static LocalDate RandomLocalDate(int years) {
+        //TODO add some testing
+        public static LocalDate RandomDateSpecifikYear(int year) {
             var month = HelperClass.Randomizer(1, 12);
-            var date = RandomizeDate(CalendarSystem.Iso.GetDaysInMonth(years, month));
-            return new LocalDate(years, month, date);
+            var daysInMonth = CalendarSystem.Iso.GetDaysInMonth(year, month);
+            var day = HelperClass.Randomizer(daysInMonth);
+            return new LocalDate(year, month, day);
         }
-
-
-        /// <summary>
-        ///     Returns a date within a month
-        /// </summary>
-        /// <param name="maxDays"></param>
-        /// <returns></returns>
-        private static int RandomizeDate(int maxDays)
-            => HelperClass.Randomizer(1, maxDays);
     }
 }
--- DataGeneratorTests/Types/Date/DateFactoryTests.cs
+++ DataGeneratorTests/Types/Date/DateFactoryTests.cs
@@ -6,83 +6,171 @@ using NodaTime;
 namespace DataGeneratorTests.Types.Date {
     [TestClass]
     public class DateFactoryTests {
-        #region Previous Random Date With Year Only
+        #region Date Subtraction Exception handling
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void BirthYearNegativeValue()
-            => DateFactory.RandomPreviousDate(-1);
+        public void SubtractNegativeValue()
+            => DateFactory.SubtractionToCurrentDate(-1);
+
+        #endregion
+
+        #region Date Subtraction no args
 
         [TestMethod]
-        public void BirthYearTwentyYear() {
-            var randomPreviousDate = DateFactory.RandomPreviousDate(20);
-            var currentYear = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date.Year;
-            Assert.IsTrue(randomPreviousDate.Year == currentYear - 20);
-        }
-
-        [TestMethod]
-        public void BirthYearTenYear() {
-            var randomPreviousDate = DateFactory.RandomPreviousDate(10);
-            var currentYear = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date.Year;
-            Assert.IsTrue(randomPreviousDate.Year == currentYear - 10);
-        }
-
-        [TestMethod]
-        public void BirthYearZeroYears() {
-            var randomPreviousDate = DateFactory.RandomPreviousDate(0);
-            var currentYear = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date.Year;
-            Assert.IsTrue(randomPreviousDate.Year == currentYear);
-        }
-
-        [TestMethod]
-        public void PreviousDateWithinCurrentYear() {
-            const int minYear = 0;
-            var randomPreviousDate = DateFactory.RandomPreviousDate(minYear);
+        public void SubtractNoArgs() {
+            var randomPreviousDate = DateFactory.SubtractionToCurrentDate();
             var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
-            Assert.IsTrue(randomPreviousDate < now);
+            Assert.IsTrue(randomPreviousDate == now);
         }
 
         #endregion
 
-        #region Future Random Date with year only argument
+        #region Date Subtraction with Year arg
+
+        [TestMethod]
+        public void SubtractOneYear() {
+            const int years = 1;
+            var randomPreviousDate = DateFactory.SubtractionToCurrentDate(years: years);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomPreviousDate == now.Minus(Period.FromYears(1)));
+        }
+
+        [TestMethod]
+        public void SubtractHundredYear() {
+            const int years = 100;
+            var randomPreviousDate = DateFactory.SubtractionToCurrentDate(years: years);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomPreviousDate == now.Minus(Period.FromYears(100)));
+        }
+
+        #endregion
+
+        #region Subtraction With Month arg
+
+        [TestMethod]
+        public void SubtractionDateTestOneMonth() {
+            var randomFutureDate = DateFactory.SubtractionToCurrentDate(month: 1);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomFutureDate == now.Minus(Period.FromMonths(1)));
+        }
+
+        [TestMethod]
+        public void SubtractionDateTestHundredMonth() {
+            var randomFutureDate = DateFactory.SubtractionToCurrentDate(month: 100);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomFutureDate == now.Minus(Period.FromMonths(100)));
+        }
+
+        #endregion
+
+        #region Subtraction With Days arg
+
+        [TestMethod]
+        public void SubtractionDateTestOneDay() {
+            var randomFutureDate = DateFactory.SubtractionToCurrentDate(days: 1);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomFutureDate == now.Minus(Period.FromDays(1)));
+        }
+
+        [TestMethod]
+        public void SubtractionDateTestHundredDays() {
+            var randomFutureDate = DateFactory.SubtractionToCurrentDate(days: 100);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomFutureDate == now.Minus(Period.FromDays(100)));
+        }
+
+        #endregion
+
+        #region Date Addition Exception handling 
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
-        public void RandomFutureDateNegativeValue()
-            => DateFactory.RandomFutureDate(-1);
+        public void RandomFutureDateNegativeYear()
+            => DateFactory.AdditionToCurrentDate(years: -1);
 
         [TestMethod]
-        public void RandomFutureDateTenYears() {
-            const int years = 10;
-            var randomFutureDate = DateFactory.RandomFutureDate(years);
-            var currentYear = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date.Year;
-            Assert.IsTrue(randomFutureDate.Year == currentYear + years);
-        }
+        [ExpectedException(typeof(ArgumentException))]
+        public void RandomFutureDateNegativeMonth()
+            => DateFactory.AdditionToCurrentDate(month: -1);
 
         [TestMethod]
-        public void RandomFutureDateOneYears() {
-            const int years = 1;
-            var randomFutureDate = DateFactory.RandomFutureDate(years);
-            var currentYear = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date.Year;
-            Assert.IsTrue(randomFutureDate.Year == currentYear + years);
-        }
+        [ExpectedException(typeof(ArgumentException))]
+        public void RandomFutureDateNegativedate()
+            => DateFactory.AdditionToCurrentDate(days: -1);
 
         [TestMethod]
-        public void RandomFutureDateHundredYears() {
-            const int years = 100;
-            var randomFutureDate = DateFactory.RandomFutureDate(years);
-            var currentYear = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date.Year;
-            Assert.IsTrue(randomFutureDate.Year == currentYear + years);
-        }
+        [ExpectedException(typeof(ArgumentException))]
+        public void RandomFutureDateAllArgsNegative()
+            => DateFactory.AdditionToCurrentDate(years: -1, month: -1, days: -1);
+
+        #endregion
+
+        #region Date Addition no args
 
         [TestMethod]
-        public void RandomFutureDateWithinCurrentYear() {
-            const int years = 0;
-            var randomFutureDate = DateFactory.RandomFutureDate(years);
-            var currentYear = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
-            Assert.IsTrue(randomFutureDate > currentYear);
+        public void AddNoArgs() {
+            var expected = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            var result = DateFactory.AdditionToCurrentDate();
+            Assert.IsTrue(result == expected);
         }
 
         #endregion
+
+        #region Date Addition with year arg
+
+        [TestMethod]
+        public void AddOneYear() {
+            const int years = 1;
+            var randomFutureDate = DateFactory.AdditionToCurrentDate(years);
+            var currentYear = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date.Year;
+            Assert.IsTrue(randomFutureDate.Year == currentYear + years);
+        }
+
+        [TestMethod]
+        public void AddHundredYears() {
+            const int years = 100;
+            var randomFutureDate = DateFactory.AdditionToCurrentDate(years);
+            var currentYear = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date.Year;
+            Assert.IsTrue(randomFutureDate.Year == currentYear + years);
+        }
+
+        #endregion
+
+        #region Future Random Date With Month Argument
+
+        [TestMethod]
+        public void FutureDateTestOneMonth() {
+            var randomFutureDate = DateFactory.AdditionToCurrentDate(month: 1);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomFutureDate == now.PlusMonths(1));
+        }
+
+        [TestMethod]
+        public void FutureDateTestHundredMonth() {
+            var randomFutureDate = DateFactory.AdditionToCurrentDate(month: 100);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomFutureDate == now.PlusMonths(100));
+        }
+
+        #endregion
+
+        #region Date Additon with days arg
+
+        public void FutureDateTestOneDay() {
+            var randomFutureDate = DateFactory.AdditionToCurrentDate(days: 1);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomFutureDate == now.PlusDays(1));
+        }
+
+        [TestMethod]
+        public void FutureDateTestHundredDays() {
+            var randomFutureDate = DateFactory.AdditionToCurrentDate(days: 100);
+            var now = SystemClock.Instance.Now.InZone(DateTimeZoneProviders.Bcl.GetSystemDefault()).Date;
+            Assert.IsTrue(randomFutureDate == now.PlusDays(100));
+        }
+
+        #endregion
+
     }
 }
