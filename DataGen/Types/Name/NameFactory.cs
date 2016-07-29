d591eed6497a05976c512010d1dfae47cfe55628
Robert Andersson <inputfalken@gmail.com>
2016-07-29 03:16:11 +0200

    put namerepository in a seperate class

--- DataGen/Types/Name/NameFactory.cs
+++ DataGen/Types/Name/NameFactory.cs
@@ -9,10 +9,6 @@ using Newtonsoft.Json;
 
 namespace DataGen.Types.Name {
     public static class NameFactory {
-        private const string FilePath = "Data/Types/Name/data.json";
-
-        private static IEnumerable<NameRepository> NameRepositories
-            => JsonConvert.DeserializeObject<IEnumerable<NameRepository>>(File.ReadAllText(FilePath));
 
 
         /// <summary>
@@ -25,7 +21,7 @@ namespace DataGen.Types.Name {
             params string[] countries) {
             var list = new List<string>();
             foreach (var country in countries) {
-                var firstOrDefault = NameRepositories.FirstOrDefault(repository => repository.Origin.Country == country);
+                var firstOrDefault = NameRepository.FilterByCountry(country);
                 if (firstOrDefault == null)
                     throw new NullReferenceException($"Country: {country} was not found");
                 list.AddRange(func(firstOrDefault));
@@ -43,7 +39,7 @@ namespace DataGen.Types.Name {
             params Region[] regions) {
             var list = new List<string>();
             foreach (var region in regions)
-                foreach (var nameRepository in FilterByRegion(region))
+                foreach (var nameRepository in NameRepository.FilterByRegion(region))
                     list.AddRange(func(nameRepository));
             return Filter.RepeatedData(list);
         }
@@ -55,63 +51,10 @@ namespace DataGen.Types.Name {
         /// <returns></returns>
         public static IEnumerable<string> NameCollection(Func<NameRepository, IEnumerable<string>> func) {
             var list = new List<string>();
-            foreach (var nameRepository in NameRepositories)
+            foreach (var nameRepository in NameRepository.NameRepositories)
                 list.AddRange(func(nameRepository));
             return Filter.RepeatedData(list);
         }
 
-
-        private static IEnumerable<NameRepository> FilterByRegion(Region region) {
-            switch (region) {
-                case Region.Europe:
-                    return NameRepositories.Where(repository => repository.Origin.Region == Europe);
-                case Region.CentralAmerika:
-                    return NameRepositories.Where(repository => repository.Origin.Region == CentralAmerica);
-                case Region.NorthAmerica:
-                    return NameRepositories.Where(repository => repository.Origin.Region == NorthAmerica);
-                case Region.SouthAmerica:
-                    return NameRepositories.Where(repository => repository.Origin.Region == SouthAmerica);
-            }
-            throw new ArgumentOutOfRangeException(nameof(region), region, null);
-        }
-
-        // Is generated from json
-        // ReSharper disable once ClassNeverInstantiated.Global
-        public class NameRepository {
-            public NameRepository(IEnumerable<string> femaleFirstNames, IEnumerable<string> maleFirstNames,
-                IEnumerable<string> lastNames, string country, string region) {
-                FemaleFirstNames = femaleFirstNames;
-                MaleFirstNames = maleFirstNames;
-                LastNames = lastNames;
-                Origin = new Origin(country, region);
-            }
-
-            public IEnumerable<string> FemaleFirstNames { get; }
-            public IEnumerable<string> LastNames { get; }
-            public IEnumerable<string> MaleFirstNames { get; }
-            public Origin Origin { get; }
-
-            public IEnumerable<string> MixedFirstNames
-                => FemaleFirstNames.Concat(MaleFirstNames);
-        }
-
-        public class Origin {
-            public readonly string Country;
-            public readonly string Region;
-
-            public Origin(string country, string region) {
-                Country = country;
-                Region = region;
-            }
-        }
-
-        #region Regions
-
-        private const string Europe = "Europe";
-        private const string NorthAmerica = "North America";
-        private const string SouthAmerica = "South America";
-        private const string CentralAmerica = "Central America";
-
-        #endregion
     }
 }
