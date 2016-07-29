4efaf03a6da6b5e73b23e31f5d6311ae25880fe4
Robert Andersson <inputfalken@gmail.com>
2016-07-29 03:16:11 +0200

    put namerepository in a seperate class

---
+++ DataGen/Types/Name/NameRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGen.Types.Name
+{
+        // Is generated from json
+        // ReSharper disable once ClassNeverInstantiated.Global
+        public class NameRepository {
+            public NameRepository(IEnumerable<string> femaleFirstNames, IEnumerable<string> maleFirstNames,
+                IEnumerable<string> lastNames, string country, string region) {
+                FemaleFirstNames = femaleFirstNames;
+                MaleFirstNames = maleFirstNames;
+                LastNames = lastNames;
+                Origin = new Origin(country, region);
+            }
+
+            public IEnumerable<string> FemaleFirstNames { get; }
+            public IEnumerable<string> LastNames { get; }
+            public IEnumerable<string> MaleFirstNames { get; }
+            public Origin Origin { get; }
+
+            public IEnumerable<string> MixedFirstNames
+                => FemaleFirstNames.Concat(MaleFirstNames);
+        }
+
+        public class Origin {
+            public readonly string Country;
+            public readonly string Region;
+
+            public Origin(string country, string region) {
+                Country = country;
+                Region = region;
+            }
+        }
+}
