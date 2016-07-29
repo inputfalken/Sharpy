5c4b39e13d1aba29d4da3535f50ce2c6ed5597eb
Robert Andersson <inputfalken@gmail.com>
2016-07-29 03:21:53 +0200

    moved name specifik logic to name class

--- DataGen/Types/Name/NameFactory.cs
+++ DataGen/Types/Name/NameFactory.cs
@@ -9,10 +9,6 @@ using Newtonsoft.Json;
 
 namespace DataGen.Types.Name {
     public static class NameFactory {
-        private const string FilePath = "Data/Types/Name/data.json";
-
-        private static IEnumerable<NameRepository> NameRepositories
-            => JsonConvert.DeserializeObject<IEnumerable<NameRepository>>(File.ReadAllText(FilePath));
 
 
         /// <summary>
@@ -25,7 +21,7 @@ namespace DataGen.Types.Name {
             params string[] countries) {
             var list = new List<string>();
             foreach (var country in countries) {
-                var firstOrDefault = NameRepositories.FirstOrDefault(repository => repository.Origin.Country == country);
+                var firstOrDefault = NameRepository.FilterByCountry(country);
                 if (firstOrDefault == null)
                     throw new NullReferenceException($"Country: {country} was not found");
                 list.AddRange(func(firstOrDefault));
@@ -43,7 +39,7 @@ namespace DataGen.Types.Name {
             params Region[] regions) {
             var list = new List<string>();
             foreach (var region in regions)
-                foreach (var nameRepository in FilterByRegion(region))
+                foreach (var nameRepository in NameRepository.FilterByRegion(region))
                     list.AddRange(func(nameRepository));
             return Filter.RepeatedData(list);
         }
@@ -55,34 +51,10 @@ namespace DataGen.Types.Name {
         /// <returns></returns>
         public static IEnumerable<string> NameCollection(Func<NameRepository, IEnumerable<string>> func) {
             var list = new List<string>();
-            foreach (var nameRepository in NameRepositories)
+            foreach (var nameRepository in NameRepository.NameRepositories)
                 list.AddRange(func(nameRepository));
             return Filter.RepeatedData(list);
         }
 
-
-        private static IEnumerable<NameRepository> FilterByRegion(Region region) {
-            switch (region) {
-                case Region.Europe:
-                    return NameRepositories.Where(repository => repository.Origin.Region == Europe);
-                case Region.CentralAmerika:
-                    return NameRepositories.Where(repository => repository.Origin.Region == CentralAmerica);
-                case Region.NorthAmerica:
-                    return NameRepositories.Where(repository => repository.Origin.Region == NorthAmerica);
-                case Region.SouthAmerica:
-                    return NameRepositories.Where(repository => repository.Origin.Region == SouthAmerica);
-            }
-            throw new ArgumentOutOfRangeException(nameof(region), region, null);
-        }
-
-
-        #region Regions
-
-        private const string Europe = "Europe";
-        private const string NorthAmerica = "North America";
-        private const string SouthAmerica = "South America";
-        private const string CentralAmerica = "Central America";
-
-        #endregion
     }
 }
--- DataGen/Types/Name/NameRepository.cs
+++ DataGen/Types/Name/NameRepository.cs
@@ -1,38 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-namespace DataGen.Types.Name
-{
-        // Is generated from json
-        // ReSharper disable once ClassNeverInstantiated.Global
-        public class NameRepository {
-            public NameRepository(IEnumerable<string> femaleFirstNames, IEnumerable<string> maleFirstNames,
-                IEnumerable<string> lastNames, string country, string region) {
-                FemaleFirstNames = femaleFirstNames;
-                MaleFirstNames = maleFirstNames;
-                LastNames = lastNames;
-                Origin = new Origin(country, region);
-            }
+namespace DataGen.Types.Name {
+    // Is generated from json
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class NameRepository {
+        private const string FilePath = "Data/Types/Name/data.json";
 
-            public IEnumerable<string> FemaleFirstNames { get; }
-            public IEnumerable<string> LastNames { get; }
-            public IEnumerable<string> MaleFirstNames { get; }
-            public Origin Origin { get; }
+        public static IEnumerable<NameRepository> NameRepositories
+            => JsonConvert.DeserializeObject<IEnumerable<NameRepository>>(File.ReadAllText(FilePath));
 
-            public IEnumerable<string> MixedFirstNames
-                => FemaleFirstNames.Concat(MaleFirstNames);
+        public NameRepository(IEnumerable<string> femaleFirstNames, IEnumerable<string> maleFirstNames,
+            IEnumerable<string> lastNames, string country, string region) {
+            FemaleFirstNames = femaleFirstNames;
+            MaleFirstNames = maleFirstNames;
+            LastNames = lastNames;
+            Origin = new Origin(country, region);
         }
 
-        public class Origin {
-            public readonly string Country;
-            public readonly string Region;
+        public IEnumerable<string> FemaleFirstNames { get; }
+        public IEnumerable<string> LastNames { get; }
+        public IEnumerable<string> MaleFirstNames { get; }
+        private Origin Origin { get; }
 
-            public Origin(string country, string region) {
-                Country = country;
-                Region = region;
+        public IEnumerable<string> MixedFirstNames
+            => FemaleFirstNames.Concat(MaleFirstNames);
+
+        public static IEnumerable<NameRepository> FilterByRegion(Region region) {
+            switch (region) {
+                case Region.Europe:
+                    return NameRepositories.Where(repository => repository.Origin.Region == Europe);
+                case Region.CentralAmerika:
+                    return NameRepositories.Where(repository => repository.Origin.Region == CentralAmerica);
+                case Region.NorthAmerica:
+                    return NameRepositories.Where(repository => repository.Origin.Region == NorthAmerica);
+                case Region.SouthAmerica:
+                    return NameRepositories.Where(repository => repository.Origin.Region == SouthAmerica);
             }
+            throw new ArgumentOutOfRangeException(nameof(region), region, null);
         }
+
+        public static NameRepository FilterByCountry(string country)
+            => NameRepositories.FirstOrDefault(repository => repository.Origin.Country == country);
+
+        #region Regions
+
+        private const string Europe = "Europe";
+        private const string NorthAmerica = "North America";
+        private const string SouthAmerica = "South America";
+        private const string CentralAmerica = "Central America";
+
+        #endregion
+    }
+
+
+    public class Origin {
+        public readonly string Country;
+        public readonly string Region;
+
+        public Origin(string country, string region) {
+            Country = country;
+            Region = region;
+        }
+    }
 }
