3d7c06412724866ef39c0e7af1d4f9a4505cc546
Robert Andersson <inputfalken@gmail.com>
2016-08-06 04:29:16 +0200

    moved predicate method into filter class and made it virtual and made FilterByMethod Abstract

--- DataGen/Types/Filter.cs
+++ DataGen/Types/Filter.cs
@@ -1,23 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace DataGen.Types {
     public abstract class Filter<TData, TEnum> : IEnumerable<TData> {
-        protected Filter(IEnumerable<TData> result) {
-            Result = result;
+        protected Filter(IEnumerable<TData> enumerable) {
+            Enumerable = enumerable;
         }
 
-        private IEnumerable<TData> Result { get; }
+        private IEnumerable<TData> Enumerable { get; }
+        protected ICollection<TData> Collection { get; } = new Collection<TData>();
 
         public IEnumerable<TData> RemoveRepeatedData()
-            => Result.GroupBy(s => s)
+            => Enumerable.GroupBy(s => s)
                 .Where(g => g.Any())
                 .Select(grouping => grouping.Key);
 
-        public IEnumerator<TData> GetEnumerator() => Result.GetEnumerator();
+        public IEnumerator<TData> GetEnumerator() => Enumerable.GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        protected virtual void PopulateCollectionWhere(Func<TData, bool> predicate) {
+            var enumerator = GetEnumerator();
+            while (enumerator.MoveNext())
+                if (predicate(enumerator.Current))
+                    Collection.Add(enumerator.Current);
+            enumerator.Dispose();
+        }
+
         public abstract Filter<TData, TEnum> FilterBy(TEnum tenum, params string[] args);
     }
 }
--- DataGen/Types/NameCollection/NameFilter.cs
+++ DataGen/Types/NameCollection/NameFilter.cs
@@ -4,40 +4,31 @@ using System.Linq;
 
 namespace DataGen.Types.NameCollection {
     public class NameFilter : Filter<Name, FilterArg> {
-        public NameFilter(IEnumerable<Name> result) : base(result) {
+        public NameFilter(IEnumerable<Name> enumerable) : base(enumerable) {
         }
 
         public override Filter<Name, FilterArg> FilterBy(FilterArg filterArg, params string[] args) {
             switch (filterArg) {
                 case FilterArg.Male:
-                    Predicate(name => name.Type == 1);
+                    PopulateCollectionWhere(name => name.Type == 1);
                     break;
                 case FilterArg.Female:
-                    Predicate(name => name.Type == 2);
+                    PopulateCollectionWhere(name => name.Type == 2);
                     break;
                 case FilterArg.Lastname:
-                    Predicate(name => name.Type == 3);
+                    PopulateCollectionWhere(name => name.Type == 3);
                     break;
                 case FilterArg.Country:
-                    Predicate(name => args.Contains(name.Country));
+                    PopulateCollectionWhere(name => args.Contains(name.Country));
                     break;
                 case FilterArg.Region:
-                    Predicate(name => args.Contains(name.Region));
+                    PopulateCollectionWhere(name => args.Contains(name.Region));
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(filterArg), filterArg, null);
             }
-            return new NameFilter(Names);
+            return new NameFilter(Collection);
         }
 
-        private List<Name> Names { get; } = new List<Name>();
-
-        private void Predicate(Func<Name, bool> predicate) {
-            var enumerator = GetEnumerator();
-            while (enumerator.MoveNext())
-                if (predicate(enumerator.Current))
-                    Names.Add(enumerator.Current);
-            enumerator.Dispose();
-        }
     }
 }
