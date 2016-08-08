c5fd00f2100e786d6b7b2154cd8c6a9dc2a23974
Robert Andersson <inputfalken@gmail.com>
2016-08-08 14:16:46 +0200

    removed subtraction and addition methods

--- DataGen/Types/Date/DateFactory.cs
+++ DataGen/Types/Date/DateFactory.cs
@@ -27,39 +27,6 @@ namespace DataGen.Types.Date {
             return localDates;
         }
 
-        /// <summary>
-        ///     Will add days, months, years to a date
-        /// </summary>
-        /// <param name="localDate">Will be defaulted to today if not specified</param>
-        /// <param name="years">Years to be added</param>
-        /// <param name="month">Months to be added</param>
-        /// <param name="days">Days to be added </param>
-        /// <returns></returns>
-        public static LocalDate Addition(LocalDate localDate = default(LocalDate), uint years = 0, uint month = 0,
-            uint days = 0) {
-            var date = localDate == default(LocalDate) ? CurrentLocalDate : localDate;
-            return date
-                .Plus(FromYears(years))
-                .Plus(FromMonths(month))
-                .Plus(FromDays(days));
-        }
-
-        /// <summary>
-        ///     Will subtract from current date
-        /// </summary>
-        /// <param name="localDate">Will be defaulted to today if not specified</param>
-        /// <param name="years">Years to be subtracted</param>
-        /// <param name="months">Months to be subtracted</param>
-        /// <param name="days">Days to be subtracted</param>
-        /// <returns></returns>
-        public static LocalDate Subtraction(LocalDate localDate = default(LocalDate), uint years = 0, uint months = 0,
-            uint days = 0) {
-            var date = localDate == default(LocalDate) ? CurrentLocalDate : localDate;
-            return date
-                .Minus(FromYears(years))
-                .Minus(FromMonths(months))
-                .Minus(FromDays(days));
-        }
 
         //Can add an bool arg and use ternary operator to make this into one method
         public static LocalDate CreatePastDate(LocalDate date)
