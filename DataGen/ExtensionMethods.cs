040a2d8565b4bad33fdb84ded59db7fdd6d75804
Robert Andersson <inputfalken@gmail.com>
2016-08-09 11:21:39 +0200

    added documentation

--- DataGen/ExtensionMethods.cs
+++ DataGen/ExtensionMethods.cs
@@ -4,6 +4,14 @@ using static System.Linq.Enumerable;
 
 namespace DataGen {
     public static class ExtensionMethods {
+        /// <summary>
+        ///     Will add elements to list in pattern by func
+        /// </summary>
+        /// <param name="list">the list to obtain the sequence</param>
+        /// <param name="ammount">the ammount of elements</param>
+        /// <param name="func">will return the element with created by arguments</param>
+        /// <param name="sequenceValue">The start value of the sequence</param>
+        /// <returns></returns>
         public static IEnumerable<TSource> Sequence<TSource>(this IList<TSource> list, int ammount,
             Func<TSource, int, TSource> func, TSource sequenceValue = default(TSource)) {
             foreach (var i in Range(1, ammount))
@@ -11,6 +19,9 @@ namespace DataGen {
             return list;
         }
 
+        /// <summary>
+        ///     Will perform a foreach loop
+        /// </summary>
         public static void ForEach<TSource>(this IEnumerable<TSource> enumerable, Action<TSource> func) {
             foreach (var source in enumerable)
                 func(source);
