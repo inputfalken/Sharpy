8a6abf33bf57f374d2c7d3cb03ff386966f3137f
Robert Andersson <inputfalken@gmail.com>
2016-08-09 02:30:44 +0200

    added two tests for sequence method
    added 2 more tests

--- DataGen/ExtensionMethods.cs
+++ DataGen/ExtensionMethods.cs
@@ -4,10 +4,10 @@ using static System.Linq.Enumerable;
 
 namespace DataGen {
     public static class ExtensionMethods {
-        public static IEnumerable<TSource> Sequence<TSource>(this IList<TSource> list, int current,
-            Func<TSource, int, TSource> func, TSource defaultValue = default(TSource)) {
-            foreach (var i in Range(1, current))
-                list.Add(func(defaultValue, i));
+        public static IEnumerable<TSource> Sequence<TSource>(this IList<TSource> list, int ammount,
+            Func<TSource, int, TSource> func, TSource sequenceValue = default(TSource)) {
+            foreach (var i in Range(1, ammount))
+                list.Add(func(sequenceValue, i));
             return list;
         }
 
---
+++ Tests/ExtensionMethodsTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataGen;
+using NUnit.Framework;
+
+namespace Tests {
+    [TestFixture]
+    public class ExtensionMethodsTest {
+        #region Sequence
+
+        [Test]
+        public void Sequence_Arg_IntList_WithMultiplikationOfThree() {
+            var expected = new List<int> { 3, 6, 9, 12, 15 };
+            var result = new List<int>().Sequence(5, (i, i1) => i * i1, 3);
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [Test]
+        public void Sequence_Arg_IntList_WithMulitplikationOfFive() {
+            var expected = new List<int> { 5, 10, 15, 20, 25 };
+            var result = new List<int>().Sequence(ammount: 5, func: (i, i1) => i * i1, sequenceValue: 5);
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+
+        [Test]
+        public void Sequence_Arg_IntList_WithDivision() {
+            var expected = new List<double> { 12, 6, 4 };
+            var result = new List<double>().Sequence(ammount: 3, func: (i, i1) => i / i1, sequenceValue: 12);
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [Test]
+        public void Sequence_Arg_StringList() {
+            var expected = new List<string>() { "foo", "foofoo", "foofoofoo", "foofoofoofoo" };
+            var result = new List<string>().Sequence(4, (s, i) =>
+                    Enumerable.Repeat(s, i).Aggregate((s1, s2) => s1 + s2), "foo");
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        #endregion
+    }
+}
