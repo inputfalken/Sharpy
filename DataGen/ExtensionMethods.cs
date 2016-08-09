4f607681e60a938bae2252d417af54d96302a69d
Robert Andersson <inputfalken@gmail.com>
2016-08-09 02:01:33 +0200

    renamed args

--- DataGen/ExtensionMethods.cs
+++ DataGen/ExtensionMethods.cs
@@ -4,10 +4,10 @@ using static System.Linq.Enumerable;
 
 namespace DataGen {
     public static class ExtensionMethods {
-        public static IEnumerable<TSource> Sequence<TSource>(this IList<TSource> list, int length,
-            Func<TSource, int, TSource> func, TSource start = default(TSource)) {
-            foreach (var i in Range(1, length))
-                list.Add(func(start, i));
+        public static IEnumerable<TSource> Sequence<TSource>(this IList<TSource> list, int current,
+            Func<TSource, int, TSource> func, TSource defaultValue = default(TSource)) {
+            foreach (var i in Range(1, current))
+                list.Add(func(defaultValue, i));
             return list;
         }
 
