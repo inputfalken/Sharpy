b3b643f43089c177a2bf419e46024c7109f82042
Robert Andersson <inputfalken@gmail.com>
2016-08-09 13:40:42 +0200

    added a function to retreive random elements

--- DataGen/Types/Filter.cs
+++ DataGen/Types/Filter.cs
@@ -23,6 +23,21 @@ namespace DataGen.Types {
                 .Where(g => g.Any())
                 .Select(grouping => grouping.Key);
 
+
+        public TData RetrieveRandomElement() {
+            var current = default(TData);
+            var count = 0;
+            foreach (var element in this) {
+                count++;
+                if (HelperClass.Randomizer(count) == 0)
+                    current = element;
+            }
+            if (count == 0)
+                throw new InvalidOperationException("Sequence was empty");
+            return current;
+        }
+
+
         public IEnumerator<TData> GetEnumerator() => Enumerable.GetEnumerator();
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
