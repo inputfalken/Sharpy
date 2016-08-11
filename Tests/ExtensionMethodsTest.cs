106e35ec367221aa5904ac7de26c9142d139c0cd
Robert Andersson <inputfalken@gmail.com>
2016-08-11 02:36:00 +0200

    ran code cleanup

---
+++ Tests/ExtensionMethodsTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataGen;
+using NUnit.Framework;
+
+namespace Tests {
+    [TestFixture]
+    public class ExtensionMethodsTest {
+        [Test]
+        public void Sequence_Arg_IntList_WithDivisionFromTwelve() {
+            var expected = new List<double> { 12, 6, 4 };
+            var result = new List<double>().Sequence(3, (i, i1) => i / i1, 12);
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [Test]
+        public void Sequence_Arg_IntList_WithMulitplikationFromFive() {
+            var expected = new List<int> { 5, 10, 15, 20, 25 };
+            var result = new List<int>().Sequence(5, (i, i1) => i * i1, 5);
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [Test]
+        public void Sequence_Arg_IntList_WithMultiplikationFromThree() {
+            var expected = new List<int> { 3, 6, 9, 12, 15 };
+            var result = new List<int>().Sequence(5, (i, i1) => i * i1, 3);
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+
+        [Test]
+        public void Sequence_Arg_StringList() {
+            var expected = new List<string> { "foo", "foofoo", "foofoofoo", "foofoofoofoo" };
+            var result = new List<string>().Sequence(4, (s, i) =>
+                Enumerable.Repeat(s, i).Aggregate((s1, s2) => s1 + s2), "foo");
+            Assert.IsTrue(expected.SequenceEqual(result));
+        }
+    }
+}
