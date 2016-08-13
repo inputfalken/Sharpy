98794c77afc409fa110cac247dd0db026cab3e3c
Robert Andersson <inputfalken@gmail.com>
2016-08-13 20:16:54 +0200

    added tests for date factory

---
+++ Tests/DateFactoryTest.cs
@@ -0,0 +1,33 @@
+﻿using DataGen.Types.Date;
+using NodaTime;
+using NUnit.Framework;
+
+namespace Tests {
+    [TestFixture]
+    public class DateFactoryTest {
+        [Test]
+        //Todo Test with sero day, month, year and create tests for mail factory as well as sequence in the date factory
+        public void CreateDate_Arg_Subtract_True() {
+            var currentLocalDate = DateFactory.CurrentLocalDate;
+            const int day = 1;
+            const int year = 1;
+            const int month = 2;
+            var result = DateFactory.Date(new LocalDate(day, month, year));
+            Assert.AreEqual(currentLocalDate.Day - day, result.Day);
+            Assert.AreEqual(currentLocalDate.Year - year, result.Year);
+            Assert.AreEqual(currentLocalDate.Month - month, result.Month);
+        }
+
+        [Test]
+        public void CreateDate_Arg_Subtract_False() {
+            var currentLocalDate = DateFactory.CurrentLocalDate;
+            const int day = 1;
+            const int year = 1;
+            const int month = 2;
+            var result = DateFactory.Date(new LocalDate(day, month, year), false);
+            Assert.AreEqual(currentLocalDate.Day + day, result.Day);
+            Assert.AreEqual(currentLocalDate.Year + year, result.Year);
+            Assert.AreEqual(currentLocalDate.Month + month, result.Month);
+        }
+    }
+}
