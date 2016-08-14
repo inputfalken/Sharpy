6833d9fb6cdd7f30187c8cede193eea0b4484fd7
Robert Andersson <inputfalken@gmail.com>
2016-08-15 01:29:14 +0200

    renamed arguments and created added method to use for indexOf

--- DataGen/Types/String/StringFilter.cs
+++ DataGen/Types/String/StringFilter.cs
@@ -8,24 +8,23 @@ namespace DataGen.Types.String {
         }
 
 
-        public StringFilter StartsWith(params string[] args) => new StringFilter(args.SelectMany(s
-            => this.Where(username => username.IndexOf(s, StringComparison.CurrentCultureIgnoreCase) == 0)));
+        public StringFilter StartsWith(params string[] args)
+            => new StringFilter(args.SelectMany(arg => this.Where(s => IndexOf(s, arg) == 0)));
 
-        public StringFilter DoesNotStartWith(params string[] args) => new StringFilter(args.SelectMany(s
-            => this.Where(username => username.IndexOf(s, StringComparison.CurrentCultureIgnoreCase) != 0)));
+        public StringFilter DoesNotStartWith(params string[] args)
+            => new StringFilter(args.SelectMany(arg => this.Where(s => IndexOf(s, arg) != 0)));
 
-        public StringFilter Contains(params string[] args) {
-            if (args.Length == 1)
-                return new StringFilter(this.Where(username =>
-                        username.IndexOf(args[0], StringComparison.CurrentCultureIgnoreCase) >= 0));
-            var selectMany = args.SelectMany(s =>
-                    this.Where(username => username.IndexOf(s, StringComparison.CurrentCultureIgnoreCase) >= 0));
-            return new StringFilter(RemoveRepeatedData(selectMany));
-        }
+        public StringFilter Contains(params string[] args)
+            => args.Length == 1
+                ? new StringFilter(this.Where(s => IndexOf(s, args[0]) >= 0))
+                : new StringFilter(RemoveRepeatedData(args.SelectMany(arg => this.Where(s => IndexOf(s, arg) >= 0))));
 
-        public StringFilter DoesNotContain(params string[] args) => new StringFilter(args.SelectMany(s
-            => this.Where(username => username.IndexOf(s, StringComparison.CurrentCultureIgnoreCase) < 0)));
+        public StringFilter DoesNotContain(params string[] args)
+            => new StringFilter(args.SelectMany(arg => this.Where(s => IndexOf(s, arg) < 0)));
 
         public StringFilter ByLength(int length) => new StringFilter(this.Where(s => s.Length == length));
+
+        private static int IndexOf(string str, string substring)
+            => str.IndexOf(substring, StringComparison.CurrentCultureIgnoreCase);
     }
 }
