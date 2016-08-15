90e2a898f2e98d7d491bcde566d061296467af22
Robert Andersson <inputfalken@gmail.com>
2016-08-15 11:56:09 +0200

    reoerderd methods

--- DataGen/Types/String/StringFilter.cs
+++ DataGen/Types/String/StringFilter.cs
@@ -7,22 +7,19 @@ namespace DataGen.Types.String {
         public StringFilter(IEnumerable<string> enumerable) : base(enumerable) {
         }
 
+        public StringFilter DoesNotStartWith(string arg) => new StringFilter(this.Where(s => IndexOf(s, arg) != 0));
+        public StringFilter DoesNotContain(string arg) => new StringFilter(this.Where(s => !s.Contains(arg)));
 
-        public StringFilter StartsWith(params string[] args) {
-            return args.Length == 1
+        public StringFilter StartsWith(params string[] args)
+            => args.Length == 1
                 ? new StringFilter(this.Where(s => IndexOf(s, args[0]) == 0))
-                : new StringFilter(this.Where(s => args.Any(arg => IndexOf(s,arg) == 0)));
-        }
-
-        public StringFilter DoesNotStartWith(string arg)
-            => new StringFilter(this.Where(s => IndexOf(s, arg) != 0));
+                : new StringFilter(this.Where(s => args.Any(arg => IndexOf(s, arg) == 0)));
 
         public StringFilter Contains(params string[] args)
             => args.Length == 1
                 ? new StringFilter(this.Where(s => s.Contains(args[0])))
                 : new StringFilter(this.Where(s => args.Any(s.Contains)));
 
-        public StringFilter DoesNotContain(string arg) => new StringFilter(this.Where(s => !s.Contains(arg)));
 
         public StringFilter ByLength(int length) {
             if (length < 1) throw new ArgumentOutOfRangeException($"{nameof(length)} is below 1");
