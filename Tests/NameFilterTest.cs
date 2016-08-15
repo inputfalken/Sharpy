4f4a6bb939e9f962b0f9eefa865dd1ba2f3e91a1
Robert Andersson <inputfalken@gmail.com>
2016-08-15 15:20:40 +0200

    added tests with multiple arguments for region

--- Tests/NameFilterTest.cs
+++ Tests/NameFilterTest.cs
@@ -32,8 +32,126 @@ namespace Tests {
         #region Region
 
         [Test]
+        public void NameFilter_ByRegion_OneArg() {
+            var vladimir = new Name(1, "", "region1", "Vladimir");
+            var john = new Name(1, "", "region2", "John");
+            var jack = new Name(1, "", "region3", "Jack");
+            var gustavo = new Name(1, "doo", "region1", "gustavo");
+            var james = new Name(1, "woop", "region1", "James");
+            var lisa = new Name(1, "scooby", "region2", "Lisa");
+            var albin = new Name(1, "bar", "region2", "Albin");
+            var wilma = new Name(1, "bar", "region3", "wilma");
+            var jens = new Name(1, "bar", "region3", "jens");
+            var bob = new Name(1, "bar", "region4", "bob");
+            var william = new Name(1, "bar", "region4", "william");
+            var jenkins = new Name(1, "bar", "region4", "jenkins");
+            var enumerable = new[] {
+                vladimir, john, jack,
+                bob, william, jenkins,
+                gustavo, james, lisa,
+                albin, wilma, jens
+            };
+            var expected = new[] {
+                vladimir, gustavo, james
+            };
+            var result = new NameFilter(enumerable).ByRegion("region1");
+            //Makes sure that each object region is equal to current region.
+            Assert.IsTrue(result.SequenceEqual(expected));
+        }
+
+        [Test]
+        public void NameFilter_ByRegion_TwoArg() {
+            var vladimir = new Name(1, "", "region1", "Vladimir");
+            var john = new Name(1, "", "region2", "John");
+            var jack = new Name(1, "", "region3", "Jack");
+            var gustavo = new Name(1, "doo", "region1", "gustavo");
+            var james = new Name(1, "woop", "region1", "James");
+            var lisa = new Name(1, "scooby", "region2", "Lisa");
+            var albin = new Name(1, "bar", "region2", "Albin");
+            var wilma = new Name(1, "bar", "region3", "wilma");
+            var jens = new Name(1, "bar", "region3", "jens");
+            var bob = new Name(1, "bar", "region4", "bob");
+            var william = new Name(1, "bar", "region4", "william");
+            var jenkins = new Name(1, "bar", "region4", "jenkins");
+            var enumerable = new[] {
+                vladimir, john, jack,
+                bob, william, jenkins,
+                gustavo, james, lisa,
+                albin, wilma, jens
+            };
+            var expected = new[] {
+                vladimir, jack, gustavo,
+                james, wilma, jens
+            };
+            var result = new NameFilter(enumerable).ByRegion("region1", "region3");
+            //Makes sure that each object region is equal to current region.
+            Assert.IsTrue(result.SequenceEqual(expected));
+        }
+
+        [Test]
+        public void NameFilter_ByRegion_ThreeArgs() {
+            var vladimir = new Name(1, "", "region1", "Vladimir");
+            var john = new Name(1, "", "region2", "John");
+            var jack = new Name(1, "", "region3", "Jack");
+            var gustavo = new Name(1, "doo", "region1", "gustavo");
+            var james = new Name(1, "woop", "region1", "James");
+            var lisa = new Name(1, "scooby", "region2", "Lisa");
+            var albin = new Name(1, "bar", "region2", "Albin");
+            var wilma = new Name(1, "bar", "region3", "wilma");
+            var jens = new Name(1, "bar", "region3", "jens");
+            var bob = new Name(1, "bar", "region4", "bob");
+            var william = new Name(1, "bar", "region4", "william");
+            var jenkins = new Name(1, "bar", "region4", "jenkins");
+            var enumerable = new[] {
+                vladimir, john, jack,
+                bob, william, jenkins,
+                gustavo, james, lisa,
+                albin, wilma, jens
+            };
+            var expected = new[] {
+                vladimir, john, jack,
+                gustavo, james, lisa,
+                albin, wilma, jens
+            };
+            var result = new NameFilter(enumerable).ByRegion("region1", "region2", "region3");
+            //Makes sure that each object region is equal to current region.
+            Assert.IsTrue(result.SequenceEqual(expected));
+        }
+
+        [Test]
+        public void NameFilter_ByRegion_FourArgs() {
+            var vladimir = new Name(1, "", "region1", "Vladimir");
+            var john = new Name(1, "", "region2", "John");
+            var jack = new Name(1, "", "region3", "Jack");
+            var gustavo = new Name(1, "doo", "region1", "gustavo");
+            var james = new Name(1, "woop", "region1", "James");
+            var lisa = new Name(1, "scooby", "region2", "Lisa");
+            var albin = new Name(1, "bar", "region2", "Albin");
+            var wilma = new Name(1, "bar", "region3", "wilma");
+            var jens = new Name(1, "bar", "region3", "jens");
+            var bob = new Name(1, "bar", "region4", "bob");
+            var william = new Name(1, "bar", "region4", "william");
+            var jenkins = new Name(1, "bar", "region4", "jenkins");
+            var enumerable = new[] {
+                vladimir, john, jack,
+                bob, william, jenkins,
+                gustavo, james, lisa,
+                albin, wilma, jens
+            };
+            var expected = new[] {
+                vladimir, john, jack,
+                bob, william, jenkins,
+                gustavo, james, lisa,
+                albin, wilma, jens
+            };
+            var result = new NameFilter(enumerable).ByRegion("region1", "region2", "region3", "region4");
+            //Makes sure that each object region is equal to current region.
+            Assert.IsTrue(result.SequenceEqual(expected));
+        }
+
+        [Test]
         public void NameFilter_Arg_Region_Foobar()
-            => Assert.Throws<ArgumentException>(() => Names.ByRegion( "foobar"));
+            => Assert.Throws<ArgumentException>(() => Names.ByRegion("foobar"));
 
         [Test]
         public void NameFilter_Arg_Region_CentralAmerica() {
