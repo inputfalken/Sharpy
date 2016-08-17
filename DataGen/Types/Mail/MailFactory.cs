b3792e81eb2c16d77e4a531ae7de97acb5aaa409
Robert Andersson <inputfalken@gmail.com>
2016-08-17 04:11:24 +0200

    mail now passes all tests

--- DataGen/Types/Mail/MailFactory.cs
+++ DataGen/Types/Mail/MailFactory.cs
@@ -5,17 +5,15 @@ using System.Linq;
 namespace DataGen.Types.Mail {
     public class MailFactory {
         /// <summary>
-        ///     Since this is static it will not get created for each new instance of this object. Which means this can keep track
-        ///     of all created mails.
-        ///     Cons: List will be huge if use alot of mails are created.
+        /// Will work as a history for each instance.
         /// </summary>
-        private static readonly List<string> CreatedMails = new List<string>();
+        private readonly List<string> _createdMails = new List<string>();
 
         /// <summary>
         ///     Used for separating strings with symbols
         /// </summary>
         private static readonly List<char> Separators = new List<char> {
-            '_', '.', '-'
+            '.', '_', '-'
         };
 
         private readonly List<string> _emailDomains = new List<string>();
@@ -39,7 +37,23 @@ namespace DataGen.Types.Mail {
         /// <param name="text">the string/strings used to construct a mail string</param>
         /// <returns></returns>
         public string Mail(params string[] text) {
-            return "";
+            if (text.Length == 0) throw new Exception("Argument cannot be zero");
+            foreach (var emailDomain in _emailDomains) {
+                if (text.Length == 1) {
+                    var address = $"{text[0]}@{emailDomain}";
+                    if (_createdMails.Contains(address)) continue;
+                    _createdMails.Add(address);
+                    return address;
+                }
+                //If length is not equal to 1 we start using separators
+                foreach (var separator in Separators) {
+                    var address = text.Aggregate((s, s1) => $"{s}{separator}{s1}") + $"@{emailDomain}";
+                    if (_createdMails.Contains(address)) continue;
+                    _createdMails.Add(address);
+                    return address;
+                }
+            }
+            throw new Exception("Could not create an unique mail");
         }
     }
 }
