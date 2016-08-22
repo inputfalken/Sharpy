2fb79b96ef948ee95727bae1a2e2b7db695c8f35
Robert Andersson <inputfalken@gmail.com>
2016-08-23 00:09:55 +0200

    added firstname,lastname,mail,username

--- DataGen/Person.cs
+++ DataGen/Person.cs
@@ -4,6 +4,24 @@ using DataGen.Types.String;
 
 namespace DataGen {
     public class Person {
+        private static readonly NameFilter NameFilter = DataCollections.CommonNames.Value.ByCountry("sweden");
+        private static readonly StringFilter UserNameFilter = DataCollections.CommonUserNames.Value.ByLength(8);
+        private static readonly MailFactory MailFactory = new MailFactory();
 
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string MailAddress { get; }
+        public string Username { get; }
+
+        public Person() {
+            FirstName = NameFilter.ByType(NameTypes.MixedNames).RandomItem.Data;
+            LastName = NameFilter.ByType(NameTypes.LastNames).RandomItem.Data;
+            Username = UserNameFilter.RandomItem;
+            MailAddress = MailFactory.Mail(FirstName, LastName);
+        }
+
+        public override string ToString() {
+            return $"Name: {FirstName} {LastName} \nMail: {MailAddress}\nUser Name: {Username}";
+        }
     }
 }
