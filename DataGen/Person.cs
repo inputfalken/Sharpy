b1df7284b3f7bf450d65f15c9598c64bb6d3440e
Robert Andersson <inputfalken@gmail.com>
2016-08-23 00:20:08 +0200

    added a number

--- DataGen/Person.cs
+++ DataGen/Person.cs
@@ -1,10 +1,17 @@
-﻿using DataGen.Types.Mail;
+﻿using System.Linq;
+using DataGen.Types.CountryCode;
+using DataGen.Types.Mail;
 using DataGen.Types.Name;
 using DataGen.Types.String;
 
 namespace DataGen {
     public class Person {
-        private static readonly NameFilter NameFilter = DataCollections.CommonNames.Value.ByCountry("sweden");
+        private const string Country = "sweden";
+        private static readonly NameFilter NameFilter = DataCollections.CommonNames.Value.ByCountry(Country);
+
+        private static readonly CountryCode CountryCode =
+            DataCollections.CountryCodeFilters.Value.ByCountry(Country).First();
+
         private static readonly StringFilter UserNameFilter = DataCollections.CommonUserNames.Value.ByLength(8);
         private static readonly MailFactory MailFactory = new MailFactory();
 
@@ -12,16 +19,18 @@ namespace DataGen {
         public string LastName { get; }
         public string MailAddress { get; }
         public string Username { get; }
+        public string PhoneNumber { get; }
 
         public Person() {
             FirstName = NameFilter.ByType(NameTypes.MixedNames).RandomItem.Data;
             LastName = NameFilter.ByType(NameTypes.LastNames).RandomItem.Data;
             Username = UserNameFilter.RandomItem;
             MailAddress = MailFactory.Mail(FirstName, LastName);
+            PhoneNumber = CountryCode.CreateRandomNumber(4, "39");
         }
 
         public override string ToString() {
-            return $"Name: {FirstName} {LastName} \nMail: {MailAddress}\nUser Name: {Username}";
+            return $"Name: {FirstName} {LastName}\nPhone: {PhoneNumber} \nMail: {MailAddress}\nUser Name: {Username}";
         }
     }
 }
