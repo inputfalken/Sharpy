5d711de3de5e767aadfce7b1fe2c900cf2eb6126
Robert Andersson <inputfalken@gmail.com>
2016-08-23 15:48:27 +0200

    indented comment

--- DataGen/Types/CountryCode/CountryCode.cs
+++ DataGen/Types/CountryCode/CountryCode.cs
@@ -14,7 +14,7 @@ namespace DataGen.Types.CountryCode {
         }
 
         ///<summary>
-        ///  Will create a phone number by randoming numbers including country code
+        ///     Will create a phone number by randoming numbers including country code
         /// <param name="length">The length of the number</param>
         /// <param name="preNumber">Optional number that will be used before the random numbers</param>
         /// </summary>
@@ -35,7 +35,7 @@ namespace DataGen.Types.CountryCode {
         }
 
         ///<summary>
-        ///  This overLoad will also randomize a phone number length within min and max length
+        ///     This overLoad will also randomize a phone number length within min and max length
         /// <param name="minLength">Min length of the phone number</param>
         /// <param name="maxLength">Max length of the phone number</param>
         /// <param name="preNumber">Optional number that will be used before the random numbers</param>
