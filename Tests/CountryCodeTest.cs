4d49c8c2104be26f1f1be1e0b26abffc9b848ab6
Robert Andersson <inputfalken@gmail.com>
2016-08-23 16:14:23 +0200

    added a simple test

---
+++ Tests/CountryCodeTest.cs
@@ -0,0 +1,16 @@
+﻿using DataGen.Types.CountryCode;
+using NUnit.Framework;
+
+namespace Tests {
+    [TestFixture]
+    public class CountryCodeTest {
+        [Test]
+        public void CreateRandomNumber_Args_Length5_PreNumber35() {
+            var countryCode = new CountryCode("foo", "+20");
+            var randomNumber = countryCode.CreateRandomNumber(5, "35");
+            const int exepectedLength = 10;
+            Assert.AreEqual(exepectedLength, randomNumber.Length);
+            Assert.IsTrue(randomNumber.Contains("+2035"));
+        }
+    }
+}
