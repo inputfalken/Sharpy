48097185088a208fde219167e9e8b2a1843646d4
Robert Andersson <inputfalken@gmail.com>
2016-08-27 18:00:28 +0200

    added a more user friendly approuch to generate data

---
+++ DataGen/Sharpy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DataGen.Types.Mail;
+using static DataGen.DataCollections;
+
+namespace DataGen {
+    public static class Sharpy {
+        public static T Map<T>(Action<T, Config> func) where T : new() {
+            var t = new T();
+            func(t, new Config());
+            return t;
+        }
+    }
+
+    public class Config {
+        public Config() {
+        }
+
+        private static readonly MailGenerator MailGenerator = new MailGenerator();
+        public string RandomName => Names.Value.RandomItem.Data;
+        public string UserName => UserNames.Value.RandomItem;
+        public Func<string, string, string> MailAddress => MailGenerator.Mail;
+    }
+}
