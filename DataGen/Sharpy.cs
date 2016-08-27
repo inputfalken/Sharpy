b95b0bb2a5eb4ebfb5feb0f6e07e7dcfaffadc8b
Robert Andersson <inputfalken@gmail.com>
2016-08-28 01:57:37 +0200

    config now also uses PhoneNumberGenerator in it's constructor
    There's also a prop giving a func which will run random phone number

--- DataGen/Sharpy.cs
+++ DataGen/Sharpy.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DataGen.Types.CountryCode;
 using DataGen.Types.Mail;
 using DataGen.Types.Name;
 using DataGen.Types.String;
@@ -7,7 +8,8 @@ using static DataGen.DataCollections;
 namespace DataGen {
     public static class Sharpy {
         public static Func<T> CreateGenerator<T>(Action<T, Config> func) where T : new() =>
-            CreateGenerator(func, new Config(Names.Value, UserNames.Value, new MailGenerator()));
+            CreateGenerator(func,
+                new Config(Names.Value, UserNames.Value, new MailGenerator(), CountryCodes.Value.RandomItem));
 
 
         public static Func<T> CreateGenerator<T>(Action<T, Config> func, Config config) where T : new() => () => {
@@ -17,18 +19,21 @@ namespace DataGen {
         };
 
         public static T Generate<T>(Action<T, Config> func) where T : new() =>
-            Generate(func, new Config(Names.Value, UserNames.Value, new MailGenerator()));
+            Generate(func, new Config(Names.Value, UserNames.Value, new MailGenerator(), CountryCodes.Value.RandomItem));
 
         public static T Generate<T>(Action<T, Config> func, Config config) where T : new() =>
             CreateGenerator(func, config)();
     }
 
     public class Config {
+        private PhoneNumberGenerator PhoneNumberGenerator { get; }
         private NameFilter NameFilter { get; }
         private StringFilter Usernames { get; }
         private MailGenerator MailGenerator { get; }
 
-        public Config(NameFilter nameFilter, StringFilter usernames, MailGenerator mailGenerator) {
+        public Config(NameFilter nameFilter, StringFilter usernames, MailGenerator mailGenerator,
+            PhoneNumberGenerator phoneNumberGenerator) {
+            PhoneNumberGenerator = phoneNumberGenerator;
             NameFilter = nameFilter;
             Usernames = usernames;
             MailGenerator = mailGenerator;
@@ -37,5 +42,6 @@ namespace DataGen {
         public string RandomName => NameFilter.RandomItem.Data;
         public string UserName => Usernames.RandomItem;
         public Func<string, string, string> MailAddress => MailGenerator.Mail;
+        public Func<int, string, string> PhoneNumber => PhoneNumberGenerator.RandomNumber;
     }
 }
