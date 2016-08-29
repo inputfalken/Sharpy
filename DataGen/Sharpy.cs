d62753695a9301d8c4534770a597680e1ea5a6da
Robert Andersson <inputfalken@gmail.com>
2016-08-29 02:29:57 +0200

    reordered methods

--- DataGen/Sharpy.cs
+++ DataGen/Sharpy.cs
@@ -14,16 +14,6 @@ namespace DataGen {
         private static readonly Fetcher DefaultFetcher = new Fetcher(Names.Value, UserNames.Value,
             new MailGenerator("gmail.com", "hotmail.com", "yahoo.com"), CountryCodes.Value.RandomItem);
 
-        public static Func<T> CreateGenerator<T>(Action<T, Fetcher> action) where T : new() =>
-            CreateGenerator(action, DefaultFetcher);
-
-
-        public static T Generate<T>(Action<T, Fetcher> action) where T : new() =>
-            Generate(action, DefaultFetcher);
-
-        public static T Generate<T>(Action<T, Fetcher> action, Fetcher fetcher) where T : new() =>
-            CreateGenerator(action, fetcher)();
-
         public static Func<T> CreateGenerator<T>(Action<T, Fetcher> action, Fetcher fetcher)
             where T : new() => () => {
             var t = new T();
@@ -31,12 +21,21 @@ namespace DataGen {
             return t;
         };
 
+        public static Func<T> CreateGenerator<T>(Action<T, Fetcher> action) where T : new() =>
+            CreateGenerator(action, DefaultFetcher);
 
         public static Func<T> CreateGenerator<T>(Func<Fetcher, T> func) => ()
             => func(DefaultFetcher);
 
         public static Func<T> CreateGenerator<T>(Func<Fetcher, T> func, Fetcher fetcher)
             => () => func(fetcher);
+
+
+        public static T Generate<T>(Action<T, Fetcher> action) where T : new() =>
+            Generate(action, DefaultFetcher);
+
+        public static T Generate<T>(Action<T, Fetcher> action, Fetcher fetcher) where T : new() =>
+            CreateGenerator(action, fetcher)();
     }
 
     public class Fetcher {
