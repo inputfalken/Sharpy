17128e43addb1d4b4e7a4004c5ca5d71af38aa8e
Robert Andersson <inputfalken@gmail.com>
2016-08-29 11:06:43 +0200

    added fetcher to a seperate file

---
+++ DataGen/Fetcher.cs
@@ -0,0 +1,40 @@
+using DataGen.Types;
+using DataGen.Types.CountryCode;
+using DataGen.Types.Date;
+using DataGen.Types.Mail;
+using DataGen.Types.Name;
+using DataGen.Types.String;
+using NodaTime;
+
+namespace DataGen {
+    public class Fetcher {
+        private PhoneNumberGenerator PhoneNumberGenerator { get; }
+        private NameFilter NameFilter { get; }
+        private StringFilter Usernames { get; }
+        private MailGenerator MailGenerator { get; }
+
+        public Fetcher(NameFilter nameFilter = null, StringFilter usernames = null,
+            MailGenerator mailGenerator = null,
+            PhoneNumberGenerator phoneNumberGenerator = null) {
+            PhoneNumberGenerator = phoneNumberGenerator ?? DataCollections.CountryCodes.Value.RandomItem;
+            NameFilter = nameFilter ?? DataCollections.Names.Value;
+            Usernames = usernames ?? DataCollections.UserNames.Value;
+            MailGenerator = mailGenerator ?? new MailGenerator("gmail.com", "hotmail.com", "yahoo.com");
+        }
+
+        public string Name => NameFilter.RandomItem.Data;
+        public string UserName => Usernames.RandomItem;
+        public bool Bool => Number(2) != 0;
+        public int Number(int maxNum) => HelperClass.Randomizer(maxNum);
+        public int Number(int minNum, int maxNum) => HelperClass.Randomizer(minNum, maxNum);
+        public LocalDate DateByAge(int age) => DateGenerator.RandomDateByAge(age);
+        public LocalDate DateByYear(int year) => DateGenerator.RandomDateByYear(year);
+
+        public string PhoneNumber(string preNumber = null, int length = 4) =>
+            PhoneNumberGenerator.RandomNumber(length, preNumber);
+
+        public string MailAdress(string name, string secondName = null) => secondName == null
+            ? MailGenerator.Mail(name)
+            : MailGenerator.Mail(name, secondName);
+    }
+}
--- DataGen/Sharpy.cs
+++ DataGen/Sharpy.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Linq;
-using DataGen.Types;
-using DataGen.Types.CountryCode;
-using DataGen.Types.Date;
 using DataGen.Types.Mail;
-using DataGen.Types.Name;
-using DataGen.Types.String;
-using NodaTime;
 using static DataGen.DataCollections;
 
 namespace DataGen {
@@ -39,36 +33,5 @@ namespace DataGen {
 
         public static T Generate<T>(Action<T, Fetcher> action, Fetcher fetcher) where T : new() =>
             CreateGenerator(action, fetcher)();
-    }
-
-    public class Fetcher {
-        private PhoneNumberGenerator PhoneNumberGenerator { get; }
-        private NameFilter NameFilter { get; }
-        private StringFilter Usernames { get; }
-        private MailGenerator MailGenerator { get; }
-
-        public Fetcher(NameFilter nameFilter = null, StringFilter usernames = null,
-            MailGenerator mailGenerator = null,
-            PhoneNumberGenerator phoneNumberGenerator = null) {
-            PhoneNumberGenerator = phoneNumberGenerator ?? CountryCodes.Value.RandomItem;
-            NameFilter = nameFilter ?? Names.Value;
-            Usernames = usernames ?? UserNames.Value;
-            MailGenerator = mailGenerator ?? new MailGenerator("gmail.com", "hotmail.com", "yahoo.com");
-        }
-
-        public string Name => NameFilter.RandomItem.Data;
-        public string UserName => Usernames.RandomItem;
-        public bool Bool => Number(2) != 0;
-        public int Number(int maxNum) => HelperClass.Randomizer(maxNum);
-        public int Number(int minNum, int maxNum) => HelperClass.Randomizer(minNum, maxNum);
-        public LocalDate DateByAge(int age) => DateGenerator.RandomDateByAge(age);
-        public LocalDate DateByYear(int year) => DateGenerator.RandomDateByYear(year);
-
-        public string PhoneNumber(string preNumber = null, int length = 4) =>
-            PhoneNumberGenerator.RandomNumber(length, preNumber);
-
-        public string MailAdress(string name, string secondName = null) => secondName == null
-            ? MailGenerator.Mail(name)
-            : MailGenerator.Mail(name, secondName);
     }
 }
