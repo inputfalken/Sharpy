9d0e4b27bb42605457345d76717f5779c116bd6f
Robert Andersson <inputfalken@gmail.com>
2016-09-02 18:15:28 +0200

    updated docs

--- DataGen/Sharpy.cs
+++ DataGen/Sharpy.cs
@@ -5,7 +5,7 @@ using static DataGen.DataCollections;
 namespace DataGen {
     public static class Sharpy {
         ///<summary>
-        ///     This is the field which gets used if you use the method which do not ask for a fetcher
+        ///     This is the field which gets used if you use the method which do not ask for a randomizer
         /// </summary>
         private static readonly Randomizer DefaultRandomizer = new Randomizer(new Config(Names.Value, UserNames.Value,
             new MailGenerator(new[] { "gmail.com", "hotmail.com", "yahoo.com" }, false), CountryCodes.Value.RandomItem));
@@ -24,14 +24,14 @@ namespace DataGen {
         ///     For a type with a consturctor you can just new up an instance of the type and satisfy the constructor args.
         ///     if you don't have a constructor you can new up an instance and assign it's props/fields
         ///     For examples please visit https://github.com/inputfalken/Sharpy
-        ///     You need to specify a new fetcher for this overload the data used for the fetcher is found in the DataCollections class
+        ///     You need to specify a new randomizer for this overload the data used for the fetcher is found in the DataCollections class
         /// </summary>
         public static Generator<T> CreateGenerator<T>(Func<Randomizer, T> func, Randomizer randomizer)
             => new Generator<T>(() => func(randomizer), randomizer.Config);
 
         ///<summary>
         ///     Gives a new instance of the type used
-        ///     You need to specify a new fetcher for this overload the data used for the fetcher is found in the DataCollections class
+        ///     You need to specify a new randomizer for this overload the data used for the fetcher is found in the DataCollections class
         /// </summary>
         public static T Generate<T>(Func<Randomizer, T> func, Randomizer randomizer) => func(randomizer);
 
