0390578b4e382622811c9e404860ddf81c334db3
Robert Andersson <inputfalken@gmail.com>
2016-09-17 00:19:30 +0200

    added summary to code & name
    Former-commit-id: e1742a8a7e9760b62227d780b00480f3331cadfa

--- Sharpy/Types/CountryCode/CountryCode.cs
+++ Sharpy/Types/CountryCode/CountryCode.cs
@@ -3,11 +3,23 @@ using Sharpy.Types.Enums;
 
 namespace Sharpy.Types.CountryCode {
     public class CountryCode {
+
         public bool IsParsed { get; }
 
+        /// <summary>
+        ///     Name of the Country
+        /// </summary>
         public Country Name { get; }
+        /// <summary>
+        ///     The country code for the Country
+        /// </summary>
         public string Code { get; }
 
+        /// <summary>
+        ///     gets populated by the JSON deserializer
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="code"></param>
         public CountryCode(string name, string code) {
             Code = code;
             Country country;
