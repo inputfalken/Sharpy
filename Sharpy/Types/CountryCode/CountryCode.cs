35b5faf375dcd853dfb875b69eb09ff8d4ff0c4e
Robert Andersson <inputfalken@gmail.com>
2016-09-16 15:21:20 +0200

    removes countrycode filter and splitted up number generator
    Former-commit-id: 483741b83a5f4571a12398e2902ef705779b9c06

---
+++ Sharpy/Types/CountryCode/CountryCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Sharpy.Types.Enums;
+
+namespace Sharpy.Types.CountryCode {
+    public class CountryCode {
+        public bool IsParsed { get; }
+
+        public Country Name { get; }
+        public string Code { get; }
+
+        public CountryCode(string name, string code) {
+            Code = code;
+            Country country;
+            if (!Enum.TryParse(name, out country)) return;
+            Name = country;
+            IsParsed = true;
+        }
+    }
+}
