08d55e1efe986a2465beddbd1bcc3874b805ff5b
Robert Andersson <inputfalken@gmail.com>
2016-09-18 15:13:55 +0200

    removes unused field
    Former-commit-id: 1e481c5157a895465187396e9712d77ba596c015

--- Sharpy/Factory.cs
+++ Sharpy/Factory.cs
@@ -7,11 +7,6 @@ namespace Sharpy {
     /// </summary>
     public static class Factory {
         /// <summary>
-        ///     This is the field which gets used if you use the method which do not ask for a randomizer
-        /// </summary>
-        private static readonly Randomizer DefaultRandomizer = new Randomizer(new Config());
-
-        /// <summary>
         ///     Returns a Generator which you can use to create one instance or a collection of type given
         ///     For a type with a consturctor you can just new up an instance of the type and satisfy the constructor args.
         ///     if you don't have a constructor you can new up an instance and assign it's props/fields
