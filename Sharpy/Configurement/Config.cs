028a3c66acf2aa35d9a113a8f6f24fc191e95f44
Robert Andersson <inputfalken@gmail.com>
2016-09-30 22:14:10 +0200

    Removes Nameconfig file and updates usages.
    Former-commit-id: d75788c51aed14732a3d3b39d956106ee70dd6c4

--- Sharpy/Configurement/Config.cs
+++ Sharpy/Configurement/Config.cs
@@ -5,9 +5,11 @@ using System.Text;
 using Newtonsoft.Json;
 using Sharpy.Enums;
 using Sharpy.Properties;
+using Sharpy.Types;
 using Sharpy.Types.CountryCode;
 using Sharpy.Types.Date;
 using Sharpy.Types.Mail;
+using Sharpy.Types.Name;
 using Sharpy.Types.String;
 
 namespace Sharpy.Configurement {
@@ -19,14 +21,77 @@ namespace Sharpy.Configurement {
 
         internal Config() {
             DateGenerator = new DateGenerator(Random);
-            MailGenerator = new MailGenerator(new[] { "gmail.com", "hotmail.com", "yahoo.com" }, Random, false);
+            MailGenerator = new MailGenerator(new[] {"gmail.com", "hotmail.com", "yahoo.com"}, Random, false);
             PhoneNumberGenerator = new PhoneNumberGenerator(new CountryCode("UnitedStates", "+1"), Random);
+        }
+
+        private Fetcher<Name> _fetcher;
+
+        private static Lazy<Fetcher<Name>> LazyNames { get; } =
+            new Lazy<Fetcher<Name>>(
+                () => new Fetcher<Name>(JsonConvert.DeserializeObject<IEnumerable<Name>>(
+                    Encoding.UTF8.GetString(Resources.NamesByOrigin))));
+
+        internal Fetcher<Name> Fetcher {
+            get { return _fetcher ?? LazyNames.Value; }
+            private set { _fetcher = value; }
+        }
+
+        public Config FilterNameByString(Func<string, bool> predicate) {
+            Fetcher = new Fetcher<Name>(Fetcher.Where(name => predicate(name.Data)));
+            return this;
+        }
+
+        /// <summary>
+        ///     This filters the names by each Country provided
+        /// </summary>
+        /// <param name="countries"></param>
+        /// <returns></returns>
+        public Config FilterNameByOrigin(params Country[] countries) {
+            Fetcher = ByCountry(countries);
+            return this;
+        }
+
+
+        /// <summary>
+        ///     This filters the names by each Region provided
+        /// </summary>
+        /// <param name="regions"></param>
+        /// <returns></returns>
+        public Config FilterNameByOrigin(params Region[] regions) {
+            Fetcher = ByRegion(regions);
+            return this;
+        }
+
+        private static int IndexOf(string str, string substring)
+            => str.IndexOf(substring, StringComparison.CurrentCultureIgnoreCase);
+
+        private Fetcher<Name> ByCountry(params Country[] args)
+            => new Fetcher<Name>(Fetcher.Where(name => args.Contains(name.Country)));
+
+
+        private Fetcher<Name> ByRegion(params Region[] args)
+            => new Fetcher<Name>(Fetcher.Where(name => args.Contains(name.Region)));
+
+
+        internal Fetcher<Name> Type(NameType nameType) {
+            switch (nameType) {
+                case NameType.FemaleFirstName:
+                    return new Fetcher<Name>(Fetcher.Where(name => name.Type == 1));
+                case NameType.MaleFirstName:
+                    return new Fetcher<Name>(Fetcher.Where(name => name.Type == 2));
+                case NameType.LastName:
+                    return new Fetcher<Name>(Fetcher.Where(name => name.Type == 3));
+                case NameType.MixedFirstName:
+                    return new Fetcher<Name>(Fetcher.Where(name => name.Type == 1 | name.Type == 2));
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(nameType), nameType, null);
+            }
         }
 
         internal Random Random { get; private set; } = new Random();
         internal DateGenerator DateGenerator { get; }
 
-        internal NameConfig NameConfig { get; } = new NameConfig();
 
 
         private static Lazy<IEnumerable<CountryCode>> LazyCountryCodes { get; } =
@@ -48,15 +113,6 @@ namespace Sharpy.Configurement {
             private set { _userNamesField = value; }
         }
 
-        /// <summary>
-        ///     Is used for configuring Names
-        /// </summary>
-        /// <param name="func"></param>
-        /// <returns></returns>
-        public Config Name(Func<NameConfig, NameConfig> func) {
-            func(NameConfig);
-            return this;
-        }
 
 
         /// <summary>
