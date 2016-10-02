c73c1b27d528a57fcc74392ac4bad588baf2e3f8
Robert Andersson <inputfalken@gmail.com>
2016-10-03 00:34:31 +0200

    Randomizer no longer depends on Name object.

--- Sharpy/Types/Randomizer.cs
+++ Sharpy/Types/Randomizer.cs
@@ -33,7 +33,7 @@ namespace Sharpy.Types {
         /// <summary>
         ///     Gives a random name, it could be a female first name, male first name and a lastname.
         /// </summary>
-        public string Name() => Config.Names.RandomItem(Config.Random).Data;
+        public string Name() => Config.Name();
 
         /// <summary>
         ///     Gives a random name based on type of argument.
