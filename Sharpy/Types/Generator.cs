1d354e8cb492ecd6b1df806f2f545ac7bbdf0e0b
Robert Andersson <inputfalken@gmail.com>
2016-10-08 23:32:39 +0200

    Configure is gone
    Former-commit-id: c7baaccc460a6de77b502e322614238b312ae692

--- Sharpy/Types/Generator.cs
+++ Sharpy/Types/Generator.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using Sharpy.Configurement;
+using System.Linq;
+using Sharpy.Enums;
+using Sharpy.Types.CountryCode;
+using Sharpy.Types.Mail;
 
 namespace Sharpy.Types {
     /// <summary>
@@ -11,16 +14,14 @@ namespace Sharpy.Types {
     public sealed class Generator<T> {
         internal Generator(Func<T> func, Randomizer randomizer) {
             Randomizer = randomizer;
-            Config = randomizer.Config;
             Func = func;
         }
 
         /// <summary>
         ///     Can be used to change settings for the randomizer
         /// </summary>
-        internal Randomizer Randomizer { get; }
+        private Randomizer Randomizer { get; }
 
-        public Config Config { get; }
         private Func<T> Func { get; }
 
         /// <summary>
@@ -37,6 +38,86 @@ namespace Sharpy.Types {
         public IEnumerable<T> Generate(int ammount) {
             for (var i = 0; i < ammount; i++)
                 yield return Func();
+        }
+
+
+        /// <summary>
+        ///     Executes the predicate on each name.
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public Generator<T> Name(Func<string, bool> predicate) {
+            Randomizer.Names = new Fetcher<Name.Name>(Randomizer.Names.Where(name => predicate(name.Data)));
+            return this;
+        }
+
+        /// <summary>
+        ///     This filters the names by each Country provided
+        /// </summary>
+        /// <param name="countries"></param>
+        /// <returns></returns>
+        public Generator<T> Name(params Country[] countries) {
+            Randomizer.Names = new Fetcher<Name.Name>(Randomizer.ByCountry(countries));
+            return this;
+        }
+
+
+        /// <summary>
+        ///     This filters the names by each Region provided
+        /// </summary>
+        /// <param name="regions"></param>
+        /// <returns></returns>
+        public Generator<T> Name(params Region[] regions) {
+            Randomizer.Names = new Fetcher<Name.Name>(Randomizer.ByRegion(regions));
+            return this;
+        }
+
+        /// <summary>
+        ///     Lets you set the providers for the mail addresses.
+        ///     You can also a set a bool for wether the addreses will be unique.
+        ///     If set to unique numbers will be appended in case of replicate mail address.
+        /// </summary>
+        /// <param name="providers"></param>
+        /// <param name="uniqueAddresses">For Unique Addresses</param>
+        /// <returns></returns>
+        public Generator<T> Mail(IEnumerable<string> providers, bool uniqueAddresses = false) {
+            Randomizer.MailGenerator = new MailGenerator(providers, Randomizer.Random, uniqueAddresses);
+            return this;
+        }
+
+        /// <summary>
+        ///     Lets you change the country code
+        /// </summary>
+        /// <param name="country"></param>
+        /// <param name="uniqueNumbers"></param>
+        /// <returns></returns>
+        public Generator<T> CountryCode(Country country, bool uniqueNumbers = false) {
+            Randomizer.PhoneNumberGenerator =
+                new PhoneNumberGenerator(Randomizer.LazyCountryCodes.Value.Single(number => number.Name == country),
+                    Randomizer.Random,
+                    uniqueNumbers);
+            return this;
+        }
+
+
+        /// <summary>
+        ///     Executes the predicate on each username.
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public Generator<T> UserName(Func<string, bool> predicate) {
+            Randomizer.UserNames = new Fetcher<string>(Randomizer.UserNames.Where(predicate));
+            return this;
+        }
+
+        /// <summary>
+        ///     Will set a seed for the generator to use
+        /// </summary>
+        /// <param name="seed"></param>
+        /// <returns></returns>
+        public Generator<T> Seed(int seed) {
+            Randomizer.Random = new Random(seed);
+            return this;
         }
     }
 }
--- Sharpy/Types/Randomizer.cs
+++ Sharpy/Types/Randomizer.cs
@@ -1,14 +1,25 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
 using NodaTime;
-using Sharpy.Configurement;
 using Sharpy.Enums;
+using Sharpy.Properties;
+using Sharpy.Types.CountryCode;
+using Sharpy.Types.Date;
+using Sharpy.Types.Mail;
 
 namespace Sharpy.Types {
     /// <summary>
     ///     Will randomize all data that these methods return.
     /// </summary>
     public sealed class Randomizer {
-        internal Config Config { get; } = new Config();
+        public Randomizer() {
+            DateGenerator = new DateGenerator(Random);
+            MailGenerator = new MailGenerator(new[] {"gmail.com", "hotmail.com", "yahoo.com"}, Random, false);
+            PhoneNumberGenerator = new PhoneNumberGenerator(new CountryCode.CountryCode("UnitedStates", "+1"), Random);
+        }
 
 
         /// <summary>
@@ -28,17 +39,22 @@ namespace Sharpy.Types {
         /// <summary>
         ///     Gives a random name, it could be a female first name, male first name and a lastname.
         /// </summary>
-        public string Name() => Config.Name();
+        public string Name() => Names.RandomItem(Random).Data;
+
 
         /// <summary>
         ///     Gives a random name based on type of argument.
         /// </summary>
-        public string Name(NameType nameType) => Config.Name(nameType);
+        public string Name(NameType nameType) {
+            if (!Dictionary.ContainsKey(nameType))
+                Dictionary.Add(nameType, new Fetcher<string>(Type(nameType).Select(name => name.Data)));
+            return Dictionary[nameType].RandomItem(Random);
+        }
 
         /// <summary>
         ///     Gives a random username from a huge collection.
         /// </summary>
-        public string UserName() => Config.UserName();
+        public string UserName() => UserNames.RandomItem(Random);
 
         /// <summary>
         ///     Gives a random bool
@@ -48,33 +64,96 @@ namespace Sharpy.Types {
         /// <summary>
         ///     Gives a random number within below the argument value
         /// </summary>
-        public int Number(int maxNum) => Config.Random.Next(maxNum);
+        public int Number(int maxNum) => Random.Next(maxNum);
 
         /// <summary>
         ///     Gives a random number within within the two arguments
         /// </summary>
-        public int Number(int minNum, int maxNum) => Config.Random.Next(minNum, maxNum);
+        public int Number(int minNum, int maxNum) => Random.Next(minNum, maxNum);
 
         /// <summary>
         ///     Gives a date with random month, date and subtract the current the current year by the argument
         /// </summary>
-        public LocalDate DateByAge(int age) => Config.DateByAge(age);
+        public LocalDate DateByAge(int age) => DateGenerator.RandomDateByAge(age);
 
         /// <summary>
         ///     Gives a random month, date and use the argument given as year
         /// </summary>
-        public LocalDate DateByYear(int year) => Config.DateByYear(year);
+        public LocalDate DateByYear(int year) => DateGenerator.RandomDateByYear(year);
 
         /// <summary>
         ///     gives a random phonenumber using a random country code and lets you specify a number to start with as well as the
         ///     length.
         /// </summary>
         public string PhoneNumber(string preNumber = null, int length = 4) =>
-            Config.RandomNumber(preNumber, length);
+            PhoneNumberGenerator.RandomNumber(length, preNumber);
 
         /// <summary>
         ///     Gives a mail address by concatining the arguments into a mail address.
         /// </summary>
-        public string MailAdress(string name, string secondName = null) => Config.Mail(name, secondName);
+        public string MailAdress(string name, string secondName = null) => MailGenerator.Mail(name, secondName);
+
+        private Fetcher<Name.Name> _names;
+        private Fetcher<string> _userNames;
+
+
+        private Dictionary<NameType, Fetcher<string>> Dictionary { get; } = new Dictionary<NameType, Fetcher<string>>();
+
+        private static Lazy<Fetcher<Name.Name>> LazyNames { get; } =
+            new Lazy<Fetcher<Name.Name>>(
+                () => new Fetcher<Name.Name>(JsonConvert.DeserializeObject<IEnumerable<Name.Name>>(
+                    Encoding.UTF8.GetString(Resources.NamesByOrigin))));
+
+        internal Fetcher<Name.Name> Names {
+            get { return _names ?? LazyNames.Value; }
+            set { _names = value; }
+        }
+
+        internal Random Random { get; set; } = new Random();
+        private DateGenerator DateGenerator { get; }
+
+
+        internal static Lazy<IEnumerable<CountryCode.CountryCode>> LazyCountryCodes { get; } =
+            new Lazy<IEnumerable<CountryCode.CountryCode>>(
+                () => JsonConvert.DeserializeObject<IEnumerable<CountryCode.CountryCode>>(
+                    Encoding.Default.GetString(Resources.CountryCodes)));
+
+        private static Lazy<Fetcher<string>> LazyUsernames { get; } =
+            new Lazy<Fetcher<string>>(() => new Fetcher<string>(Resources.usernames.Split(Convert.ToChar("\n"))));
+
+
+        internal PhoneNumberGenerator PhoneNumberGenerator { get; set; }
+
+
+        internal MailGenerator MailGenerator { get; set; }
+
+        internal Fetcher<string> UserNames {
+            get { return _userNames ?? LazyUsernames.Value; }
+            set { _userNames = value; }
+        }
+
+
+        internal IEnumerable<Name.Name> ByCountry(params Country[] args)
+            => new Fetcher<Name.Name>(Names.Where(name => args.Contains(name.Country)));
+
+
+        internal IEnumerable<Name.Name> ByRegion(params Region[] args)
+            => new Fetcher<Name.Name>(Names.Where(name => args.Contains(name.Region)));
+
+
+        private IEnumerable<Name.Name> Type(NameType nameType) {
+            switch (nameType) {
+                case NameType.FemaleFirstName:
+                    return Names.Where(name => name.Type == 1);
+                case NameType.MaleFirstName:
+                    return Names.Where(name => name.Type == 2);
+                case NameType.LastName:
+                    return Names.Where(name => name.Type == 3);
+                case NameType.MixedFirstName:
+                    return Names.Where(name => name.Type == 1 | name.Type == 2);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(nameType), nameType, null);
+            }
+        }
     }
 }
