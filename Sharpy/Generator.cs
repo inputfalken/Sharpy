041cb1eabe3a575c61003cb427ece1dbb3909bf1
Robert Andersson <inputfalken@gmail.com>
2016-10-14 22:53:24 +0200

    Config is no longer accesseable from generator.
    Former-commit-id: 4dbce375399e539cbb1296a66a82db488f066099

--- Sharpy/Generator.cs
+++ Sharpy/Generator.cs
@@ -12,10 +12,10 @@ namespace Sharpy {
         ///     Creates a Generator which you can use to create one instance or a collection of the given type
         ///     For examples please visit https://github.com/inputfalken/Sharpy
         /// </summary>
-        public Generator(Func<IRandomizer, T> func, IRandomizer randomizer = null)
+        public Generator(Func<IRandomizer, T> func, IRandomizer randomizer = null, Config config = null)
             : base(func, randomizer) {
             FuncArg = randomizer ?? new Randomizer<T>(this);
-            _config = new Config<T>(this);
+            _config = config ?? new Config();
         }
 
         /// <summary>
@@ -23,15 +23,16 @@ namespace Sharpy {
         ///     The integer included will track iterations.
         ///     For examples please visit https://github.com/inputfalken/Sharpy
         /// </summary>
-        public Generator(Func<IRandomizer, int, T> func, IRandomizer randomizer = null)
+        public Generator(Func<IRandomizer, int, T> func, IRandomizer randomizer = null, Config config = null)
             : base(func, randomizer) {
             FuncArg = randomizer ?? new Randomizer<T>(this);
-            _config = new Config<T>(this);
+            _config = config ?? new Config();
         }
-        private readonly Config<T> _config;
+
+        private readonly Config _config;
 
 
-        public Config<T> Config {
+        internal Config Config {
             get {
                 if (FuncArg is Randomizer<T>) return _config;
                 throw new Exception("You cannot use this property with a custom randomizer.");
