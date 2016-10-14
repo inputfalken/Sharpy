9474aad595b676753a96cb0f11b24819f2a63239
Robert Andersson <inputfalken@gmail.com>
2016-10-15 01:17:14 +0200

    Adds documention for enums of stringtype.
    Former-commit-id: 11e7fca92610494b75ad8b21199bab529d7a9608

--- Sharpy/Enums/StringType.cs
+++ Sharpy/Enums/StringType.cs
@@ -4,32 +4,37 @@ namespace Sharpy.Enums {
     /// </summary>
     public enum StringType {
         /// <summary>
+        ///    Will give you back a random female first name.
         /// </summary>
         FemaleFirstName,
 
         /// <summary>
+        ///    Will give you back a random male first name.
         /// </summary>
         MaleFirstName,
 
         /// <summary>
+        ///    Will give you back a random male last name.
         /// </summary>
         LastName,
 
         /// <summary>
+        ///    Will give you back a random first name.
         /// </summary>
         MixedFirstName,
 
         /// <summary>
-        /// 
+        ///    Will give you back a random username.
         /// </summary>
         UserName,
 
         /// <summary>
-        /// 
+        ///    Will give you either a first name, last name, username.
         /// </summary>
         Random,
+
         /// <summary>
-        /// 
+        ///    Will give you either a first name, last name.
         /// </summary>
         AnyName
     }
