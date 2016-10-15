5d068e338f183849fa3aa3f3d50624e3babc5b6e
Robert Andersson <inputfalken@gmail.com>
2016-10-15 02:05:12 +0200

    Adds back Factory pattern for the new abstract Generator design.

--- Sharpy/Generator.cs
+++ Sharpy/Generator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using Sharpy.Enums;
 using Sharpy.Types;
 
@@ -7,27 +8,17 @@ namespace Sharpy {
     ///    Supply the class you want to be generated.
     ///    Use the randomizer to give you data.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public sealed class Generator<T> : GeneratorBase<T, IRandomizer<StringType>> {
-        /// <summary>
-        ///     Creates a Generator which you can use to create one instance or a collection of the given type
-        ///     For examples please visit https://github.com/inputfalken/Sharpy
-        /// </summary>
-        public Generator(Func<IRandomizer<StringType>, T> func, IRandomizer<StringType> randomizer)
-            : base(func, randomizer) {}
+    public static class Generator {
+        public static Generator<T, StringType> CreateNew<T>(Func<IRandomizer<StringType>, int, T> func,
+            Config config = null) => new Generator<T, StringType>(func, new Randomizer(config ?? new Config()));
 
-        /// <summary>
-        ///     Creates a Generator which you can use to create one instance or a collection of the given type
-        ///     The integer included will track iterations.
-        ///     For examples please visit https://github.com/inputfalken/Sharpy
-        /// </summary>
-        public Generator(Func<IRandomizer<StringType>, int, T> func, IRandomizer<StringType> randomizer)
-            : base(func, randomizer) {}
+        public static Generator<T, StringType> CreateNew<T>(Func<IRandomizer<StringType>, T> func, Config config = null)
+            => new Generator<T, StringType>(func, new Randomizer(config ?? new Config()));
 
-        public Generator(Func<IRandomizer<StringType>, int, T> func, Config config = null)
-            : base(func, new Randomizer(config ?? new Config())) {}
+        public static Generator<T, TStringArg> CreateNew<T, TStringArg>(Func<IRandomizer<TStringArg>, T> func,
+            IRandomizer<TStringArg> arg) => new Generator<T, TStringArg>(func, arg);
 
-        public Generator(Func<IRandomizer<StringType>, T> func, Config config = null)
-            : base(func, new Randomizer(config ?? new Config())) {}
+        public static Generator<T, TStringArg> CreateNew<T, TStringArg>(Func<IRandomizer<TStringArg>, int, T> func,
+            IRandomizer<TStringArg> arg) => new Generator<T, TStringArg>(func, arg);
     }
 }
