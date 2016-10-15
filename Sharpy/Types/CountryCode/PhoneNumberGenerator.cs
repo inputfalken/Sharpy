9f77e96bf0625e4a0374eb7694f51c8b3cf9d810
Robert Andersson <inputfalken@gmail.com>
2016-10-15 23:14:58 +0200

    Moves prefix into constructor argument instead of method.
    Former-commit-id: 25575a6d853bb1133c2c52a9fa822970f89aa70d

--- Sharpy/Types/CountryCode/PhoneNumberGenerator.cs
+++ Sharpy/Types/CountryCode/PhoneNumberGenerator.cs
@@ -8,33 +8,35 @@ namespace Sharpy.Types.CountryCode {
     /// <summary>
     /// </summary>
     internal sealed class PhoneNumberGenerator : Unique<int> {
-        internal PhoneNumberGenerator(CountryCode countryCode, Random random, int length, bool unique = false)
+        internal PhoneNumberGenerator(CountryCode countryCode, Random random, int length, string prefix,
+            bool unique = false)
             : base(50, random) {
             CountryCode = countryCode;
+            Prefix = prefix;
             Unique = unique;
             Min = (int) Math.Pow(10, length - 1);
             Max = Min*10 - 1;
         }
 
         private CountryCode CountryCode { get; }
+        private string Prefix { get; }
         private bool Unique { get; }
         private int Min { get; }
         private int Max { get; }
 
         /// <summary>
         ///     Will create a phone number by randoming numbers including country code
-        ///     <param name="preNumber">Optional number that will be used before the random numbers</param>
         /// </summary>
-        public string RandomNumber(string preNumber = null) {
+        public string RandomNumber() {
             var next = Random.Next(Min, Max);
-            if (!Unique) return Build(CountryCode.Code, preNumber, next.ToString());
+            if (!Unique) return Build(CountryCode.Code, Prefix, next.ToString());
             while (HashSet.Contains(next)) {
                 if (next == Max)
                     next = Min;
                 next++;
             }
             HashSet.Add(next);
-            return Build(CountryCode.Code, preNumber, next.ToString());
+            return Build(CountryCode.Code, next.ToString());
         }
 
         private static string Build(params string[] strings) {
