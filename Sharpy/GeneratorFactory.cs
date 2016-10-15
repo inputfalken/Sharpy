50987c379f11e2a99299ad4ea9db37b274180727
Robert Andersson <inputfalken@gmail.com>
2016-10-15 14:42:13 +0200

    Adds documentation for the custom Generator methods.

--- Sharpy/GeneratorFactory.cs
+++ Sharpy/GeneratorFactory.cs
@@ -15,9 +15,26 @@ namespace Sharpy {
         public static Generator<T, StringType> CreateNew<T>(Func<IRandomizer<StringType>, T> func, Config config = null)
             => new Generator<T, StringType>(func, new Randomizer(config ?? new Config()));
 
+        /// <summary>
+        ///    Requires a class which implements IRandomizer&lt;StringType&gt; as a secondary argument.
+        /// </summary>
+        /// <param name="func"></param>
+        /// <param name="arg"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TStringArg"></typeparam>
+        /// <returns></returns>
         public static Generator<T, TStringArg> CreateNew<T, TStringArg>(Func<IRandomizer<TStringArg>, T> func,
             IRandomizer<TStringArg> arg) => new Generator<T, TStringArg>(func, arg);
 
+        /// <summary>
+        ///    Requires a class which implements IRandomizer&lt;StringType&gt; as a secondary argument.
+        ///    Includes an integer which is equal to ammount of iterations done. starts on number zero.
+        /// </summary>
+        /// <param name="func"></param>
+        /// <param name="arg"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TStringArg"></typeparam>
+        /// <returns></returns>
         public static Generator<T, TStringArg> CreateNew<T, TStringArg>(Func<IRandomizer<TStringArg>, int, T> func,
             IRandomizer<TStringArg> arg) => new Generator<T, TStringArg>(func, arg);
     }
