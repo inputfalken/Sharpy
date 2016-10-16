b65117587ab05950597ddce1bf9fe50d8726df08
Robert Andersson <inputfalken@gmail.com>
2016-10-16 23:41:22 +0200

    Updates Summary comments.
    Former-commit-id: 5f98b19cec7e4cc18b792d60310b1b5588bcc28d

--- Sharpy/SharpyGenerator.cs
+++ Sharpy/SharpyGenerator.cs
@@ -4,18 +4,18 @@ using Sharpy.Types;
 
 namespace Sharpy {
     /// <summary>
-    ///    Inherits from Generator class with the type signature &lt;T,StringType&gt;.
+    ///    Inherits from Generator class with the type signature &lt;T, Randomizer&gt;.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class SharpyGenerator<T> : Generator<T, Randomizer> {
         /// <summary>
-        ///    Uses my own implementation of IRandomizer&lt;StringType&gt; Uses an optional config argument as secondary argument.
+        ///    Uses my own implementation of IRandomizer. Has an optional config argument as secondary argument.
         /// </summary>
         public SharpyGenerator(Func<IRandomizer<StringType>, T> func, Config config = null)
             : base(func, new Randomizer(config ?? new Config())) {}
 
         /// <summary>
-        ///    Uses my own implementation of IRandomizer&lt;StringType&gt; Uses an optional config argument as secondary argument.
+        ///    Uses my own implementation of. Has an optional config argument as secondary argument.
         ///    Includes an integer which is equal to ammount of iterations done. starts on number zero.
         /// </summary>
         public SharpyGenerator(Func<IRandomizer<StringType>, int, T> func, Config config = null)
