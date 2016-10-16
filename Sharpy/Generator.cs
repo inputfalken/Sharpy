0723a0d9587b57c98c6392b973268bc395585f93
Robert Andersson <inputfalken@gmail.com>
2016-10-16 22:22:58 +0200

    renames IRandomizer StringTypeArg

--- Sharpy/Generator.cs
+++ Sharpy/Generator.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 
 namespace Sharpy {
-    public class Generator<TItem, TRandomizerType> {
-        private Func<IRandomizer<TRandomizerType>, int, TItem> FuncIterator { get; }
-        private IRandomizer<TRandomizerType> Randomizer { get; }
-        private Func<IRandomizer<TRandomizerType>, TItem> Func { get; }
+    public class Generator<T, TStringArg> {
+        private Func<IRandomizer<TStringArg>, int, T> FuncIterator { get; }
+        private IRandomizer<TStringArg> Randomizer { get; }
+        private Func<IRandomizer<TStringArg>, T> Func { get; }
         private int Iteratation { get; set; }
 
         /// <summary>
         ///     Creates a Generator which you can use to create one instance or a collection of the given type
         ///     For examples please visit https://github.com/inputfalken/Sharpy
         /// </summary>
-        public Generator(Func<IRandomizer<TRandomizerType>, TItem> func, IRandomizer<TRandomizerType> randomizer) {
+        public Generator(Func<IRandomizer<TStringArg>, T> func, IRandomizer<TStringArg> randomizer) {
             Func = func;
             Randomizer = randomizer;
         }
@@ -22,25 +22,25 @@ namespace Sharpy {
         ///     The integer included will track iterations.
         ///     For examples please visit https://github.com/inputfalken/Sharpy
         /// </summary>
-        public Generator(Func<IRandomizer<TRandomizerType>, int, TItem> func, IRandomizer<TRandomizerType> randomizer) {
+        public Generator(Func<IRandomizer<TStringArg>, int, T> func, IRandomizer<TStringArg> randomizer) {
             FuncIterator = func;
             Randomizer = randomizer;
         }
 
-        private TItem Generate(int i) => FuncIterator == null ? Func(Randomizer) : FuncIterator(Randomizer, i);
+        private T Generate(int i) => FuncIterator == null ? Func(Randomizer) : FuncIterator(Randomizer, i);
 
         /// <summary>
         ///     Will give back one instance of the specified Type
         /// </summary>
         /// <returns></returns>
-        public TItem Generate() => FuncIterator == null ? Func(Randomizer) : FuncIterator(Randomizer, Iteratation++);
+        public T Generate() => FuncIterator == null ? Func(Randomizer) : FuncIterator(Randomizer, Iteratation++);
 
         /// <summary>
         ///     Will give back an IEnumerable with the specified type.
         ///     Which contains the ammount of elements.
         /// </summary>
         /// <param name="ammount"></param>
-        public IEnumerable<TItem> GenerateEnumerable(int ammount) {
+        public IEnumerable<T> GenerateEnumerable(int ammount) {
             for (var i = 0; i < ammount; i++)
                 yield return Generate(i);
         }
