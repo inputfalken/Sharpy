39cedb560bc70b8e08d2fc3d121317491b4f99fb
Robert Andersson <inputfalken@gmail.com>
2016-10-17 00:43:37 +0200

    Adds a factory class.

---
+++ Sharpy/Factory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Sharpy.Types;
+
+namespace Sharpy {
+    public static class Factory {
+        public static Generator<T, Randomizer> CreateNew<T>(Func<Randomizer, T> func, Config config = null) {
+            return new Generator<T, Randomizer>(func, new Randomizer(config));
+        }
+    }
+}
