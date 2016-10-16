67a49bfef5288109c57655d6098d9ae41e711d85
Robert Andersson <inputfalken@gmail.com>
2016-10-17 00:46:20 +0200

    Updates test to use Factory.

--- Sharpy/Factory.cs
+++ Sharpy/Factory.cs
@@ -3,8 +3,10 @@ using Sharpy.Types;
 
 namespace Sharpy {
     public static class Factory {
-        public static Generator<T, Randomizer> CreateNew<T>(Func<Randomizer, T> func, Config config = null) {
-            return new Generator<T, Randomizer>(func, new Randomizer(config));
-        }
+        public static Generator<T, Randomizer> CreateNew<T>(Func<Randomizer, T> func, Config config = null)
+            => new Generator<T, Randomizer>(func, new Randomizer(config));
+
+        public static Generator<T, Randomizer> CreateNew<T>(Func<Randomizer, int, T> func, Config config = null)
+            => new Generator<T, Randomizer>(func, new Randomizer(config));
     }
 }
