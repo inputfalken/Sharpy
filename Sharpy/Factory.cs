90e7c85d64fe1c3813e63a5fddb18a4bf33487b4
Robert Andersson <inputfalken@gmail.com>
2016-10-17 00:48:39 +0200

    Forgot to add default behaviour to Factory methods.

--- Sharpy/Factory.cs
+++ Sharpy/Factory.cs
@@ -4,9 +4,9 @@ using Sharpy.Types;
 namespace Sharpy {
     public static class Factory {
         public static Generator<T, Randomizer> CreateNew<T>(Func<Randomizer, T> func, Config config = null)
-            => new Generator<T, Randomizer>(func, new Randomizer(config));
+            => new Generator<T, Randomizer>(func, new Randomizer(config ?? new Config()));
 
         public static Generator<T, Randomizer> CreateNew<T>(Func<Randomizer, int, T> func, Config config = null)
-            => new Generator<T, Randomizer>(func, new Randomizer(config));
+            => new Generator<T, Randomizer>(func, new Randomizer(config ?? new Config()));
     }
 }
