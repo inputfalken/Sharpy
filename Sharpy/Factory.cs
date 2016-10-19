15fc87748afbf4e8ba50d89c4cd3f9aa3fae0335
Robert Andersson <inputfalken@gmail.com>
2016-10-19 19:47:24 +0200

    Adds a summary comment.

--- Sharpy/Factory.cs
+++ Sharpy/Factory.cs
@@ -6,6 +6,12 @@ namespace Sharpy {
         public static Generator<Randomizer> RandomGenerator(Config config = null)
             => new Generator<Randomizer>(new Randomizer(config));
 
+        /// <summary>
+        ///  Requires a implementation of IRandomizer&lt;TStringArg&gt;
+        /// </summary>
+        /// <typeparam name="TStringArg"></typeparam>
+        /// <param name="iRandomizer"></param>
+        /// <returns></returns>
         public static Generator<IRandomizer<TStringArg>> CustomRandomGenerator<TStringArg>(IRandomizer<TStringArg> iRandomizer)
             => new Generator<IRandomizer<TStringArg>>(iRandomizer);
     }
