87419bb53907096e4a84f436b6daf7fa621bacd4
Robert Andersson <inputfalken@gmail.com>
2016-10-19 18:16:36 +0200

    Generator now only takes one constructor argument which will be the source.
    The func will now be provieded in the generation of T.
    
    
    Former-commit-id: 44f96099ca586aaf3dc3a13fa937d3c2373909ee

--- Sharpy/Factory.cs
+++ Sharpy/Factory.cs
@@ -3,10 +3,8 @@ using Sharpy.Types;
 
 namespace Sharpy {
     public static class Factory {
-        public static Generator<T, Randomizer> RandomGenerator<T>(Func<Randomizer, T> func, Config config = null)
-            => new Generator<T, Randomizer>(func, new Randomizer(config ?? new Config()));
 
-        public static Generator<T, Randomizer> RandomGenerator<T>(Func<Randomizer, int, T> func, Config config = null)
-            => new Generator<T, Randomizer>(func, new Randomizer(config ?? new Config()));
+        public static Generator<Randomizer> RandomGenerator(Config config = null) => new Generator<Randomizer>(new Randomizer(config));
+
     }
 }
--- Sharpy/Generator.cs
+++ Sharpy/Generator.cs
@@ -2,47 +2,49 @@
 using System.Collections.Generic;
 
 namespace Sharpy {
-    public class Generator<T, TSource> {
-        private Func<TSource, int, T> FuncIterator { get; }
+    public class Generator<TSource> {
         private TSource Source { get; }
-        private Func<TSource, T> Func { get; }
         private int Iteratation { get; set; }
 
         /// <summary>
         ///     Creates a Generator which you can use to create one instance or a collection of the given type
         ///     For examples please visit https://github.com/inputfalken/Sharpy
         /// </summary>
-        public Generator(Func<TSource, T> func, TSource source) {
-            Func = func;
+        public Generator(TSource source) {
             Source = source;
         }
 
-        /// <summary>
-        ///     Creates a Generator which you can use to create one instance or a collection of the given type
-        ///     The integer included will track iterations.
-        ///     For examples please visit https://github.com/inputfalken/Sharpy
-        /// </summary>
-        public Generator(Func<TSource, int, T> func, TSource source) {
-            FuncIterator = func;
-            Source = source;
-        }
 
-        private T Generate(int i) => FuncIterator == null ? Func(Source) : FuncIterator(Source, i);
+        private T Generate<T>(Func<TSource, int, T> func, int i) => func(Source, i);
 
         /// <summary>
         ///     Will give back one instance of the specified Type
         /// </summary>
         /// <returns></returns>
-        public T Generate() => FuncIterator == null ? Func(Source) : FuncIterator(Source, Iteratation++);
+        public T Generate<T>(Func<TSource, T> func) => func(Source);
+
+        public T Generate<T>(Func<TSource, int, T> func) => func(Source, Iteratation++);
 
         /// <summary>
         ///     Will give back an IEnumerable with the specified type.
         ///     Which contains the ammount of elements.
         /// </summary>
-        /// <param name="ammount"></param>
-        public IEnumerable<T> GenerateEnumerable(int ammount) {
-            for (var i = 0; i < ammount; i++)
-                yield return Generate(i);
+        /// <param name="count"></param>
+        /// <param name="func"></param>
+        public IEnumerable<T> GenerateEnumerable<T>(Func<TSource, T> func, int count = 10) {
+            for (var i = 0; i < count; i++)
+                yield return Generate(func);
+        }
+
+        /// <summary>
+        ///     Will give back an IEnumerable with the specified type.
+        ///     Which contains the ammount of elements.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="func"></param>
+        public IEnumerable<T> GenerateEnumerable<T>(Func<TSource, int, T> func, int count = 10) {
+            for (var i = 0; i < count; i++)
+                yield return Generate(func, count);
         }
     }
 }
