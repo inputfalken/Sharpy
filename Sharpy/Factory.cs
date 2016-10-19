ea714e0962acb001ef1704a2f1fee7e4a3c2b6ae
Robert Andersson <inputfalken@gmail.com>
2016-10-19 19:43:59 +0200

    Adds a method where you can suppply your own implementation of randomizer.
    Former-commit-id: 38e4998d1837ec06a92471e7ea68140b2b769b53

--- Sharpy/Factory.cs
+++ Sharpy/Factory.cs
@@ -5,5 +5,8 @@ namespace Sharpy {
     public static class Factory {
         public static Generator<Randomizer> RandomGenerator(Config config = null)
             => new Generator<Randomizer>(new Randomizer(config));
+
+        public static Generator<IRandomizer<TStringArg>> CustomRandomGenerator<TStringArg>(IRandomizer<TStringArg> iRandomizer)
+            => new Generator<IRandomizer<TStringArg>>(iRandomizer);
     }
 }
