946c1c4681c029294784117f57a8aa34fffa4483
Robert Andersson <inputfalken@gmail.com>
2016-10-21 13:53:42 +0200

    Removes factory class.
    Instead you call the same methods from the the generators.

---
+++ Sharpy/src/RandomGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Sharpy.Enums;
+using Sharpy.Types;
+
+namespace Sharpy {
+    /// <summary>
+    ///     Creates a simple generator using my Implementation of IRandomizer&lt;TStringArg&gt;
+    /// </summary>
+    /// <returns></returns>
+    public class RandomGenerator : Generator<Randomizer> {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Config Config { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        private RandomGenerator(Config config) : base(new Randomizer(config)) {
+            Config = config;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static RandomGenerator CreateNew() => new RandomGenerator(new Config());
+    }
+}
