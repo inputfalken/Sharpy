c7556db723d65e1c1018c961d612b972cb4893f6
Robert Andersson <inputfalken@gmail.com>
2016-10-23 00:16:36 +0200

    Removes indenting.
    Moves Social security method to number generator.
    
    Adds bool for returning formated string.
    
    Also uses stringbuilder rather than concatinating strings.
    Adds a test for formating.

--- Sharpy/src/Types/CountryCode/NumberGenerator.cs
+++ Sharpy/src/Types/CountryCode/NumberGenerator.cs
@@ -6,7 +6,7 @@ namespace Sharpy.Types.CountryCode {
     // Is generated from json
     /// <summary>
     /// </summary>
-    internal sealed class NumberGenerator : Unique<int> {
+    internal sealed class NumberGenerator : Unique<long> {
         internal NumberGenerator(Random random, int length, string prefix,
             bool unique = false)
             : base(random) {
@@ -33,43 +33,30 @@ namespace Sharpy.Types.CountryCode {
             return Build(Prefix, next.ToString());
         }
 
+        internal string SocialSecurity(LocalDate date, bool formated) {
+            var month = date.Month < 10 ? $"0{date.Month}" : date.Month.ToString();
+            var year = date.YearOfCentury < 10 ? $"0{date.YearOfCentury}" : date.YearOfCentury.ToString();
+            var day = date.Day < 10 ? $"0{date.Day}" : date.Day.ToString();
+            var controlNumber = Random.Next(Min, Max);
+            var securityNumber = long.Parse(Build(year, month, day, controlNumber.ToString()));
+            while (HashSet.Contains(securityNumber)) {
+                if (controlNumber == Max) controlNumber = Min;
+                else controlNumber++;
+                securityNumber = long.Parse(Build(year, month, day, controlNumber.ToString()));
+            }
+            HashSet.Add(securityNumber);
+
+            return formated
+                ? Build(year, month, day, "-", controlNumber.ToString())
+                : securityNumber.ToString();
+        }
+
         private static string Build(params string[] strings) {
             foreach (var s in strings)
                 Builder.Append(s);
             var str = Builder.ToString();
             Builder.Clear();
             return str;
-        }
-    }
-
-    /// <summary>
-    ///  Very similiar to NumberGenerator, maybe could make more dry...
-    /// </summary>
-    internal class SocialSecurityGenerator : Unique<long> {
-        public SocialSecurityGenerator(Random random, int length) : base(random) {
-            Min = (int) Math.Pow(10, length - 1);
-            Max = Min*10 - 1;
-        }
-
-        private int Max { get; }
-
-        private int Min { get; }
-
-        internal string Generate(LocalDate date) {
-            var controlNumber = Random.Next(Min, Max);
-            var month = date.Month < 10 ? $"0{date.Month}" : date.Month.ToString();
-            var year = date.YearOfCentury < 10 ? $"0{date.YearOfCentury}" : date.YearOfCentury.ToString();
-            var day = date.Day < 10 ? $"0{date.Day}" : date.Day.ToString();
-            var securityNumber = long.Parse(year + month + day + controlNumber);
-            while (HashSet.Contains(securityNumber)) {
-                if (controlNumber == Max)
-                    controlNumber = Min;
-                else
-                    controlNumber++;
-                securityNumber = long.Parse(year + month + day + controlNumber);
-            }
-            HashSet.Add(securityNumber);
-            return $"{year}{month}{day}-{controlNumber}";
         }
     }
 }
