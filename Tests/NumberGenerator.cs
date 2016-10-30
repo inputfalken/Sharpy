87c7fc223df0f705697996aa753470d5fa2b3d55
Robert Andersson <inputfalken@gmail.com>
2016-10-30 21:32:34 +0100

    Numbergenerator methods now return long instead of string.
    Former-commit-id: 1f5ebc318ac59c2cdf851ff1ed1900000f1566cc

--- Tests/NumberGenerator.cs
+++ Tests/NumberGenerator.cs
@@ -7,7 +7,7 @@ namespace Tests {
     public class NumberGenerator {
         [Test]
         public void CreateRandomNumber_Args_Length5_PreNumber30_UniqueFalse() {
-            var phoneNumberGenerator = new Sharpy.Randomizer.Generators.NumberGenerator(new Random(), 4, "30", false);
+            var phoneNumberGenerator = new Sharpy.Randomizer.Generators.NumberGenerator(new Random(), 4);
             //Runs the randomnumber 1000 times and checks if any number is ever repeated
             Assert.IsFalse(Enumerable.Range(0, 1000)
                     .Select(i => phoneNumberGenerator.RandomNumber())
@@ -18,40 +18,13 @@ namespace Tests {
 
         [Test]
         public void CreateRandomNumber_Args_Length5_PreNumber30_UniqueTrue() {
-            var phoneNumberGenerator = new Sharpy.Randomizer.Generators.NumberGenerator(new Random(), 4, "30", true);
+            var phoneNumberGenerator = new Sharpy.Randomizer.Generators.NumberGenerator(new Random(), 4, true);
             //Runs the randomnumber 1000 times and checks if any number is ever repeated
             Assert.IsTrue(Enumerable.Range(0, 1000)
                     .Select(i => phoneNumberGenerator.RandomNumber())
                     .GroupBy(s => s)
                     .All(grouping => grouping.Count() == 1)
             );
-        }
-
-        [Test]
-        public void CreateRandomNumber_Args_Length5_PreNumber35() {
-            var phoneGenerator = new Sharpy.Randomizer.Generators.NumberGenerator(new Random(), 5, "35");
-            var randomNumber = phoneGenerator.RandomNumber();
-            const int expectedLength = 7;
-            Assert.AreEqual(expectedLength, randomNumber.Length);
-            Assert.IsTrue(randomNumber.Contains("35"));
-        }
-
-        [Test]
-        public void CreateRandomNumber_Args_Length5_PreNumber37() {
-            var phoneGenerator = new Sharpy.Randomizer.Generators.NumberGenerator(new Random(), 5, "37");
-            var randomNumber = phoneGenerator.RandomNumber();
-            const int expectedLength = 7;
-            Assert.AreEqual(expectedLength, randomNumber.Length);
-            Assert.IsTrue(randomNumber.Contains("37"));
-        }
-
-        [Test]
-        public void CreateRandomNumber_Args_Length6_PreNumber35() {
-            var phoneGenerator = new Sharpy.Randomizer.Generators.NumberGenerator(new Random(), 6, "35");
-            var randomNumber = phoneGenerator.RandomNumber();
-            const int expectedLength = 8;
-            Assert.AreEqual(expectedLength, randomNumber.Length);
-            Assert.IsTrue(randomNumber.Contains("35"));
         }
     }
 }
