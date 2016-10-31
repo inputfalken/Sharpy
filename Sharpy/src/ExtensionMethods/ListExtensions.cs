c42ba97f3209c2357459ec91b27c3422dcd97fcd
Robert Andersson <inputfalken@gmail.com>
2016-10-31 01:34:32 +0100

    Adds Shuffle extension for IList.
    Former-commit-id: 4c8f97c5c418670619c6462c028fa451b65c4e01

---
+++ Sharpy/src/ExtensionMethods/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sharpy.ExtensionMethods {
+    public static class ListExtensions {
+        public static void Shuffle<T>(this IList<T> list, Random random) {
+            var n = list.Count;
+            while (n > 1) {
+                n--;
+                var k = random.Next(n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
