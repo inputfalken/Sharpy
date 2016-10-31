c70662585706d8d1ccf8eede46b0bf8b77fec598
Robert Andersson <inputfalken@gmail.com>
2016-11-01 00:43:22 +0100

    Puts SecurityNumberGen in seperate class.
    Former-commit-id: 3010b8d7ddfaacad6eb9cfbbfef9128064ce5be4

--- Sharpy/src/Randomizer/Generators/NumberGenerator.cs
+++ Sharpy/src/Randomizer/Generators/NumberGenerator.cs
@@ -32,23 +32,4 @@ namespace Sharpy.Randomizer.Generators {
             return item;
         }
     }
-
-    internal class SecurityNumberGen : Unique<long> {
-        internal long SecurityNumber(int controlNumber, string dateNumber) {
-            var number = long.Parse(dateNumber + controlNumber);
-            //OnDuplicate will only manipulate control number, DateNumber will be the same all the time.
-            while (HashSet.Contains(number))
-                number = long.Parse(dateNumber + OnDuplicate(controlNumber, 1000, 9999));
-            HashSet.Add(number);
-            return number;
-        }
-
-        private static int OnDuplicate(int item, int min, int max) {
-            if (item == max) item = min;
-            else item++;
-            return item;
-        }
-
-        public SecurityNumberGen(Random random) : base(random) {}
-    }
 }
---
+++ Sharpy/src/Randomizer/Generators/SecurityNumberGen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sharpy.Randomizer.Generators {
+    internal class SecurityNumberGen : Unique<long> {
+        internal long SecurityNumber(int controlNumber, string dateNumber) {
+            var number = long.Parse(dateNumber + controlNumber);
+            //OnDuplicate will only manipulate control number, DateNumber will be the same all the time.
+            while (HashSet.Contains(number))
+                number = long.Parse(dateNumber + OnDuplicate(controlNumber, 1000, 9999));
+            HashSet.Add(number);
+            return number;
+        }
+
+        private static int OnDuplicate(int item, int min, int max) {
+            if (item == max) item = min;
+            else item++;
+            return item;
+        }
+
+        public SecurityNumberGen(Random random) : base(random) {}
+    }
+}
