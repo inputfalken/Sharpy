8bb728d398cd972627d14fe63ba41e725e942cc1
Robert Andersson <inputfalken@gmail.com>
2016-11-02 13:01:32 +0100

    Adds tests for Long method.
    Former-commit-id: e4c880a18a0080806cba232ee33749505cee10ab

--- Tests/Randomize/Long.cs
+++ Tests/Randomize/Long.cs
@@ -1,9 +1,47 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Sharpy;
 
 namespace Tests.Randomize {
     [TestFixture]
     public class Long {
+        private const int Length = 1000000;
+
         [Test]
-        public void Todo() {}
+        public void DoubleArgumentZeroMaxValue() {
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(0, long.MaxValue), Length);
+            Assert.IsTrue(longs.All(l => l > 0));
+        }
+
+        [Test]
+        public void DoubleArgumentMinValueAndZero() {
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(long.MinValue, 0), Length);
+            Assert.IsTrue(longs.All(l => l < 0));
+        }
+
+        [Test]
+        public void NoArgument() {
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(), Length);
+            Assert.IsTrue(longs.All(l => l > long.MinValue && l < long.MaxValue));
+        }
+
+        [Test]
+        public void SingleArgumentMaxValue() {
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(long.MaxValue), Length);
+            Assert.IsTrue(longs.All(l => l > long.MinValue && l < long.MaxValue));
+        }
+
+        [Test]
+        public void SingleArgumentLessThanZero() {
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(-1), Length);
+            Assert.Throws<ArgumentOutOfRangeException>(() => longs.ToArray());
+        }
+
+        [Test]
+        public void SingleArgumentZero() {
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(0), Length);
+            Assert.Throws<ArgumentOutOfRangeException>(() => longs.ToArray());
+        }
     }
 }
