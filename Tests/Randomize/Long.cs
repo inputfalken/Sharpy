9b902d0e9bc3e4bef88d1725caf0601371973e2f
Robert Andersson <inputfalken@gmail.com>
2016-11-02 18:42:57 +0100

    Adds test for long with single generation.
    Former-commit-id: 5f2457c0d6286f8a23e5aa6df31dc0ef887ee2cb

--- Tests/Randomize/Long.cs
+++ Tests/Randomize/Long.cs
@@ -9,39 +9,113 @@ namespace Tests.Randomize {
         private const int Length = 1000000;
 
         [Test]
-        public void DoubleArgumentZeroMaxValue() {
-            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(0, long.MaxValue), Length);
-            Assert.IsTrue(longs.All(l => l > 0));
+        public void DoubleArgumentMinusThousandAndMinusTwoThousand() {
+            const int min = -1000;
+            const int max = -2000;
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(min, max), Length);
+            Assert.Throws<ArgumentOutOfRangeException>(() => longs.ToArray());
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => RandomGenerator.Create().Generate(randomize => randomize.Long(min, max)));
+        }
+
+        [Test]
+        public void DoubleArgumentMinusThousandAndTwoThousand() {
+            const int min = -1000;
+            const int max = 2000;
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(min, max), Length);
+            Assert.IsTrue(longs.All(l => l >= min && l < max));
+
+            var longInstance = RandomGenerator.Create().Generate(randomize => randomize.Long(min, max));
+            Assert.IsTrue(longInstance >= min && longInstance < max);
+        }
+
+        [Test]
+        public void DoubleArgumentMinusTwoThousandAndMinusThousand() {
+            const int min = -2000;
+            const int max = -1000;
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(min, max), Length);
+            Assert.IsTrue(longs.All(l => l >= min && l < max));
+
+            var longInstance = RandomGenerator.Create().Generate(randomize => randomize.Long(min, max));
+            Assert.IsTrue(longInstance >= min && longInstance < max);
         }
 
         [Test]
         public void DoubleArgumentMinValueAndZero() {
             var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(long.MinValue, 0), Length);
             Assert.IsTrue(longs.All(l => l < 0));
+            var longInstance = RandomGenerator.Create().Generate(randomize => randomize.Long(long.MinValue, 0));
+
+            Assert.IsTrue(longInstance < 0);
+        }
+
+
+        [Test]
+        public void DoubleArgumentThousandAndTwoThousand() {
+            const int min = 1000;
+            const int max = 2000;
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(min, max), Length);
+            Assert.IsTrue(longs.All(l => l >= min && l < max));
+
+            var longInstance = RandomGenerator.Create().Generate(randomize => randomize.Long(min, max));
+            Assert.IsTrue(longInstance >= min && longInstance < max);
+        }
+
+        [Test]
+        public void DoubleArgumentZeroMaxValue() {
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(0, long.MaxValue), Length);
+            Assert.IsTrue(longs.All(l => l > 0));
+
+            var longInstance = RandomGenerator.Create().Generate(randomize => randomize.Long(0, long.MaxValue));
+            Assert.IsTrue(longInstance > 0);
         }
 
         [Test]
         public void NoArgument() {
             var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(), Length);
             Assert.IsTrue(longs.All(l => l > long.MinValue && l < long.MaxValue));
-        }
 
-        [Test]
-        public void SingleArgumentMaxValue() {
-            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(long.MaxValue), Length);
-            Assert.IsTrue(longs.All(l => l > long.MinValue && l < long.MaxValue));
+            var longInstance = RandomGenerator.Create().Generate(randomize => randomize.Long());
+            Assert.IsTrue(longInstance > long.MinValue && longInstance < long.MaxValue);
         }
 
         [Test]
         public void SingleArgumentLessThanZero() {
             var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(-1), Length);
             Assert.Throws<ArgumentOutOfRangeException>(() => longs.ToArray());
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => RandomGenerator.Create().Generate(randomize => randomize.Long(-1)));
+        }
+
+        [Test]
+        public void SingleArgumentMaxValue() {
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(long.MaxValue), Length);
+            Assert.IsTrue(longs.All(l => l >= 0));
+
+            var longInstance = RandomGenerator.Create().Generate(randomize => randomize.Long(long.MaxValue));
+            Assert.IsTrue(longInstance >= 0);
+        }
+
+        [Test]
+        public void SingleArgumentThousand() {
+            const int max = 1000;
+            var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(max), Length);
+            Assert.IsTrue(longs.All(l => l >= 0 && l < max));
+
+            var longInstance = RandomGenerator.Create().Generate(randomize => randomize.Long(max));
+            Assert.IsTrue(longInstance >= 0 && longInstance < max);
         }
 
         [Test]
         public void SingleArgumentZero() {
             var longs = RandomGenerator.Create().GenerateMany(randomize => randomize.Long(0), Length);
             Assert.Throws<ArgumentOutOfRangeException>(() => longs.ToArray());
+
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => RandomGenerator.Create().Generate(randomize => randomize.Long(0)));
         }
     }
 }
