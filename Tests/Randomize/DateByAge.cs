1f03a59a5fa7fa7f7bc2cd99e52da24277c28400
Robert Andersson <inputfalken@gmail.com>
2016-11-03 16:35:05 +0100

    Moves tests from DateGeneratorTest.
    To Randomize/DatebyAge & Randomize/DateByYear

---
+++ Tests/Randomize/DateByAge.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Sharpy;
+using Sharpy.Randomize.Generators;
+
+namespace Tests.Randomize {
+    [TestFixture]
+    public class DateByAge {
+        [Test]
+        public void RandomDateByAgeTwentyMinusOne() {
+            //Will throw exception if argument is less than 0
+            var generator = RandomGenerator.Create();
+            Assert.Throws<ArgumentException>(() => generator.Generate(randomize => randomize.DateByAge(-1)));
+        }
+
+        [Test]
+        public void RandomDateByAgeTwentyYears() {
+            var generator = RandomGenerator.Create();
+            var result = generator.Generate(randomize => randomize.DateByAge(20));
+
+            Assert.AreEqual(result.Year, DateGenerator.CurrentLocalDate.Year - 20);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void RandomDateByAgeZeroYears() {
+            var generator = RandomGenerator.Create();
+            var result = generator.Generate(randomize => randomize.DateByAge(0));
+            //will make sure that the date created is earlier than today this year
+            Assert.IsTrue(DateGenerator.CurrentLocalDate > result);
+        }
+    }
+}
