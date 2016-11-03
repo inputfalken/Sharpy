88d076358793192cae93b1b8004a3ac47751e6bc
Robert Andersson <inputfalken@gmail.com>
2016-11-03 16:35:05 +0100

    Moves tests from DateGeneratorTest.
    To Randomize/DatebyAge & Randomize/DateByYear
    
    
    Former-commit-id: 1f03a59a5fa7fa7f7bc2cd99e52da24277c28400

--- Tests/Randomize/DateByYear.cs
+++ Tests/Randomize/DateByYear.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Sharpy;
+using Sharpy.Randomize.Generators;
 
 namespace Tests.Randomize {
     [TestFixture]
@@ -9,6 +11,26 @@ namespace Tests.Randomize {
             var randomGenerator = RandomGenerator.Create();
             var localDate = randomGenerator.Generate(randomize => randomize.DateByYear(2000));
             Assert.AreEqual(2000, localDate.Year);
+        }
+
+        [Test]
+        public void RandomDateByYearMinusOne() {
+            var randomGenerator = RandomGenerator.Create();
+            Assert.Throws<ArgumentException>(() => randomGenerator.Generate(randomize => randomize.DateByYear(-1)));
+        }
+
+        [Test]
+        public void RandomDateByYearTwoThousand() {
+            var randomGenerator = RandomGenerator.Create();
+            var result = randomGenerator.Generate(randomize => randomize.DateByYear(2000));
+            Assert.AreEqual(result.Year, 2000);
+        }
+
+        [Test]
+        public void RandomDateByYearTwoThousandTen() {
+            var randomGenerator = RandomGenerator.Create();
+            var result = randomGenerator.Generate(randomize => randomize.DateByYear(2010));
+            Assert.AreEqual(result.Year, 2010);
         }
     }
 }
