e1cc68c320bfd67f4a47cf4f0fc777ac4c09d410
Robert Andersson <inputfalken@gmail.com>
2016-11-05 03:21:01 +0100

    Updates summary comments.

--- Sharpy/src/IGenerator.cs
+++ Sharpy/src/IGenerator.cs
@@ -22,46 +22,46 @@ namespace Sharpy {
         T CustomCollection<T>(IList<T> items);
 
         /// <summary>
-        ///     <para>Returns a randomized  string based on argument.</para>
+        ///     <para>Returns a generated a string based on argument</para>
         ///     <param name="type">The type of string.</param>
         /// </summary>
         string String(TStringArg type);
 
         /// <summary>
-        ///     <para>Returns a randomized bool</para>
+        ///     <para>Returns a generated bool</para>
         /// </summary>
         bool Bool();
 
         /// <summary>
-        ///     <para>Returns a randomized Integer from 0 to max.</para>
+        ///     <para>Returns a generated Integer from 0 to max.</para>
         ///     <param name="max">The max value</param>
         /// </summary>
         int Integer(int max);
 
         /// <summary>
-        ///     <para>Returns a randomized Integer from min to max.</para>
+        ///     <para>Returns a generated Integer from min to max.</para>
         /// </summary>
         int Integer(int min, int max);
 
         /// <summary>
-        ///     <para>Returns a randomized Integer over all possible values</para>
+        ///     <para>Returns a generated int over all possible values of int (except int.MaxValue)</para>
         /// </summary>
         /// <returns></returns>
         int Integer();
 
         /// <summary>
-        ///     <para>Returns a LocalDate with the CurrentYear minus the age and randomized date & month</para>
+        ///     <para>Returns a LocalDate with the CurrentYear minus the age and generates date and month</para>
         /// </summary>
         LocalDate DateByAge(int age);
 
         /// <summary>
-        ///     <para>Returns a LocalDate with the year supplied and randomized date & month.</para>
+        ///     <para>Returns a LocalDate with the year supplied and generates date and month.</para>
         /// </summary>
         LocalDate DateByYear(int year);
 
         /// <summary>
         ///     <para>Returns a string representing a social security number.</para>
-        ///     <para>Will use the date given and then randomize 4 unique numbers as control numbers.</para>
+        ///     <para>Will use the date given and then generate 4 unique numbers as control numbers.</para>
         /// </summary>
         /// <param name="date">The date of birth</param>
         /// <param name="formated">Determines wether the string should be formated</param>
@@ -82,20 +82,20 @@ namespace Sharpy {
         string PhoneNumber(int length, string prefix = null);
 
         /// <summary>
-        ///     <para>Returns a random long from min (inclusive) to max (exclusive)</para>
+        ///     <para>Returns a generated long from min (inclusive) to max (exclusive)</para>
         /// </summary>
         /// <param name="min">The inclusive minimum bound</param>
         /// <param name="max">The exclusive maximum bound.  Must be greater than min</param>
         long Long(long min, long max);
 
         /// <summary>
-        ///     <para>Returns a random long from 0 (inclusive) to max (exclusive)</para>
+        ///     <para>Returns a generated long from 0 (inclusive) to max (exclusive)</para>
         /// </summary>
         /// <param name="max">The exclusive maximum bound.  Must be greater than 0</param>
         long Long(long max);
 
         /// <summary>
-        ///     <para>Returns a random long over all possible values of long</para>
+        ///     <para>Returns a generated long over all possible values of long</para>
         /// </summary>
         long Long();
     }
