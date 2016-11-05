9952e12f8b60111b45c1ba538e051e3c12292074
Robert Andersson <inputfalken@gmail.com>
2016-11-05 01:36:23 +0100

    Renames IRandomizer -> IGenerator.

--- Sharpy/src/Randomizer/Generator.cs
+++ Sharpy/src/Randomizer/Generator.cs
@@ -6,10 +6,10 @@ using Sharpy.ExtensionMethods;
 using Sharpy.Randomizer.Generators;
 
 namespace Sharpy.Randomizer {
-    internal sealed class Randomizer : IRandomizer<StringType> {
+    internal sealed class Generator : IGenerator<StringType> {
         private Tuple<int, int, int> _phoneState;
 
-        public Randomizer(Config config) {
+        public Generator(Config config) {
             Config = config;
             PhoneNumberGenerator = new NumberGenerator(Config.Random);
         }
