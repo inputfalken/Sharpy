f25cb98f9fdd7a5779ecf91e232a8c8b46e56600
Robert Andersson <inputfalken@gmail.com>
2016-11-07 17:26:41 +0100

    Removes default value from GenerateMany.
    Also updates its usages.

--- Tests/Generator/Params.cs
+++ Tests/Generator/Params.cs
@@ -9,7 +9,7 @@ namespace Tests.Generator {
             var randomGenerator = Sharpy.Generator.Create();
             var args = new[] {"hello", "there", "foo"};
             var generateMany =
-                randomGenerator.GenerateMany(generator => generator.Params("hello", "there", "foo"));
+                randomGenerator.GenerateMany(generator => generator.Params("hello", "there", "foo"), 10);
             Assert.IsTrue(generateMany.All(s => args.Contains(s)));
         }
     }
