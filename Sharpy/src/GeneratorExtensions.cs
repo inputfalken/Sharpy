9ce5a24098140ba68a257dab107630899ee474a5
Robert Andersson <inputfalken@gmail.com>
2016-11-09 01:30:35 +0100

    Puts extension methods on its own file.
    Former-commit-id: 12d7fa270e4ed985bd98f3e9c131c36b3d7656f1

---
+++ Sharpy/src/GeneratorExtensions.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace Sharpy {
+    public static class GeneratorExtensions {
+
+        public static T Generate<T, TStringArg>(this IGenerator<TStringArg> generator,
+            Func<IGenerator<TStringArg>, T> func) => func(generator);
+
+        /// <summary>
+        ///     <para>Generates a IEnumerable&lt;T&gt; </para>
+        /// </summary>
+        /// <param name="count">Count of IEnumerable&lt;T&gt;</param>
+        /// <param name="generator"></param>
+        /// <param name="func">The argument supplied is used to get the data. The item returned will be generated.</param>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TStringArg"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> GenerateMany<T, TStringArg>(this IGenerator<TStringArg> generator,
+            Func<IGenerator<TStringArg>, T> func, int count) {
+            for (var i = 0; i < count; i++)
+                yield return func(generator);
+        }
+
+        /// <summary>
+        ///     <para>Generates a IEnumerable&lt;T&gt; </para>
+        ///     <para>Includes an integer containing the current iteration.</para>
+        /// </summary>
+        /// <param name="count">Count of IEnumerable&lt;T&gt;</param>
+        /// <param name="generator"></param>
+        /// <param name="func">The argument supplied is used to get the data. The item returned will be generated.</param>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TStringArg"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> GenerateMany<T, TStringArg>(this IGenerator<TStringArg> generator,
+            Func<IGenerator<TStringArg>, int, T> func, int count) {
+            for (var i = 0; i < count; i++)
+                yield return func(generator, i);
+        }
+    }
+}
--- Sharpy/src/IGenerator.cs
+++ Sharpy/src/IGenerator.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using NodaTime;
 using Sharpy.Enums;
@@ -100,42 +99,5 @@ namespace Sharpy {
         ///     <para>Returns a generated long over all possible values of long</para>
         /// </summary>
         long Long();
-    }
-
-    public static class GeneratorExtensions {
-
-        public static T Generate<T, TStringArg>(this IGenerator<TStringArg> generator,
-            Func<IGenerator<TStringArg>, T> func) => func(generator);
-
-        /// <summary>
-        ///     <para>Generates a IEnumerable&lt;T&gt; </para>
-        /// </summary>
-        /// <param name="count">Count of IEnumerable&lt;T&gt;</param>
-        /// <param name="generator"></param>
-        /// <param name="func">The argument supplied is used to get the data. The item returned will be generated.</param>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="TStringArg"></typeparam>
-        /// <returns></returns>
-        public static IEnumerable<T> GenerateMany<T, TStringArg>(this IGenerator<TStringArg> generator,
-            Func<IGenerator<TStringArg>, T> func, int count) {
-            for (var i = 0; i < count; i++)
-                yield return func(generator);
-        }
-
-        /// <summary>
-        ///     <para>Generates a IEnumerable&lt;T&gt; </para>
-        ///     <para>Includes an integer containing the current iteration.</para>
-        /// </summary>
-        /// <param name="count">Count of IEnumerable&lt;T&gt;</param>
-        /// <param name="generator"></param>
-        /// <param name="func">The argument supplied is used to get the data. The item returned will be generated.</param>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="TStringArg"></typeparam>
-        /// <returns></returns>
-        public static IEnumerable<T> GenerateMany<T, TStringArg>(this IGenerator<TStringArg> generator,
-            Func<IGenerator<TStringArg>, int, T> func, int count) {
-            for (var i = 0; i < count; i++)
-                yield return func(generator, i);
-        }
     }
 }
