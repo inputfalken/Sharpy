1f658a4f0af578648a25e36b42947714e237b51f
Robert Andersson <inputfalken@gmail.com>
2016-11-10 17:49:28 +0100

    Flips operator for double.
    Former-commit-id: 96205456a8293280641cd03d2d8858ad93d968af

--- Sharpy/src/Implementation/ExtensionMethods/RandomExtensions.cs
+++ Sharpy/src/Implementation/ExtensionMethods/RandomExtensions.cs
@@ -50,7 +50,7 @@ namespace Sharpy.Implementation.ExtensionMethods {
         internal static double NextDouble(this Random random, double max) => NextDouble(random, 0, max);
 
         internal static double NextDouble(this Random random, double min, double max) {
-            if (min >= max) throw new ArgumentOutOfRangeException($"{nameof(max)} must be > {nameof(min)}");
+            if (max <= min) throw new ArgumentOutOfRangeException($"{nameof(max)} must be > {nameof(min)}");
             return random.NextDouble()*(max - min) + min;
         }
     }
