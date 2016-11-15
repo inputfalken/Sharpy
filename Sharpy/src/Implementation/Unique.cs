dedfcaf1f8e56892e22b9b1bbcaaac2a5a9ff840
Robert Andersson <inputfalken@gmail.com>
2016-11-15 18:15:49 +0100

    Removes comments.
    Former-commit-id: 6b7985963a3bad101201b98f2b06a287c7ea2634

--- Sharpy/src/Implementation/Unique.cs
+++ Sharpy/src/Implementation/Unique.cs
@@ -2,39 +2,17 @@ using System;
 using System.Collections.Generic;
 
 namespace Sharpy.Implementation {
-    /// <summary>
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
     internal abstract class Unique<T> {
-        // ReSharper disable once StaticMemberInGenericType
-        /// <summary>
-        /// </summary>
-        //protected static readonly StringBuilder Builder = new StringBuilder();
-        /// <summary>
-        /// </summary>
-        /// <param name="random"></param>
         protected Unique(Random random) {
             Random = random;
             HashSet = new HashSet<T>();
         }
 
-
-        /// <summary>
-        /// </summary>
         protected Random Random { get; }
 
         /// <summary>
         ///     This hashset will be the history of all the created elements in derived classes
         /// </summary>
         protected HashSet<T> HashSet { get; }
-
-        //        Builder.Append(s);
-        //    foreach (var s in strings)
-
-        //protected static string Build(params string[] strings) {
-        //    var str = Builder.ToString();
-        //    Builder.Clear();
-        //    return str;
-        //}
     }
 }
