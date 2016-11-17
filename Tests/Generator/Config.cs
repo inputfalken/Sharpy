0abf798e8d70d87f8617f3e1e2b6beb1287ee6a4
Robert Andersson <inputfalken@gmail.com>
2016-11-17 02:36:13 +0100

    Adds summary for default value.
    Also adds tests checking that the summary comments are correct.
    
    Adds proprety for setting Unique phone number.
    
    Resolves XML error for summary comments.
    
    Removes redundant comments.
    
    Adds setters for Region and country.
    
    Also changes them to IEnumerable.
    
    Updates summary.
    
    Update phone tests names.
    
    Adds tests for PhoneNumber where it's not Unique.

---
+++ Tests/Generator/Config.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests.Generator {
+    [TestFixture]
+    public class Config {
+        [Test]
+        public void UniqueMailAddresses_Default_Value() {
+            var gen = new Sharpy.Generator();
+            Assert.AreEqual(true, gen.UniqueMailAddresses);
+        }
+
+        [Test]
+        public void MailProviders_Default_Value() {
+            var gen = new Sharpy.Generator();
+            Assert.AreEqual(new[] {"gmail.com", "hotmail.com", "yahoo.com"}, gen.MailProviders);
+        }
+
+        [Test]
+        public void UniquePhoneNumber_Default_Value() {
+            var gen = new Sharpy.Generator();
+            Assert.AreEqual(true, gen.UniquePhoneNumbers);
+        }
+
+        [Test]
+        public void Countries_Default_Value_Null() {
+            var gen = new Sharpy.Generator();
+            Assert.IsNull(gen.Countries);
+        }
+
+        [Test]
+        public void Region_Default_Value_Null() {
+            var gen = new Sharpy.Generator();
+            Assert.IsNull(gen.Regions);
+        }
+    }
+}
