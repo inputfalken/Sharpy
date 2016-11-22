1b4cdbbffcab9efe71ba1a0752ac82cb28337eb4
Robert Andersson <inputfalken@gmail.com>
2016-11-22 18:45:41 +0100

    Adds Double property for expliciting IDouble.
    Former-commit-id: 875d68cafb39c7460ce95c040be0be52151150fa

---
+++ Sharpy/src/DoubleRandomizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Sharpy.Implementation.ExtensionMethods;
+
+namespace Sharpy {
+    internal class DoubleRandomizer : IDouble {
+        private readonly Random _random;
+
+        public DoubleRandomizer(Random random) {
+            _random = random;
+        }
+
+        public double Double() => _random.NextDouble();
+
+        public double Double(double max) => _random.NextDouble(max);
+
+        public double Double(double min, double max) => _random.NextDouble(min, max);
+    }
+}
