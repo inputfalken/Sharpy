fd2ecf4ba7e6cd3c2235971fe3814a96983141e0
Robert Andersson <inputfalken@gmail.com>
2016-11-24 23:40:32 +0100

    Updates summary comments and runs code cleanup.

--- Sharpy/src/IProviders/IDoubleProvider.cs
+++ Sharpy/src/IProviders/IDoubleProvider.cs
@@ -1,24 +1,24 @@
 namespace Sharpy.IProviders {
     /// <summary>
-    /// <para>Represents methods for giving doubles.</para>
+    ///     <para>Represents methods for providing doubles.</para>
     /// </summary>
     public interface IDoubleProvider {
         /// <summary>
-        ///     <para>Returns a generated double from 0 to 1</para>
+        ///     <para>Returns a double from 0 to 1.</para>
         /// </summary>
         double Double();
 
         /// <summary>
-        ///     <para>Returns a generated long from 0 (inclusive) to max (exclusive)</para>
+        ///     <para>Returns a double from 0 to max.</para>
         /// </summary>
-        /// <param name="max">The exclusive maximum bound.  Must be greater than 0</param>
+        /// <param name="max">The maximum value.</param>
         double Double(double max);
 
         /// <summary>
-        ///     <para>Returns a generated double from min (inclusive) to max (exclusive)</para>
+        ///     <para>Returns a double from min to max.</para>
         /// </summary>
-        /// <param name="min">The inclusive minimum bound</param>
-        /// <param name="max">The exclusive maximum bound.  Must be greater than min</param>
+        /// <param name="min">The minimum value.</param>
+        /// <param name="max">The maximum value.</param>
         double Double(double min, double max);
     }
 }
