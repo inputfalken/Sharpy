91aa20e6be19c4651e246a1da098e9d50a72265c
Robert Andersson <inputfalken@gmail.com>
2016-11-26 16:03:49 +0100

    Updates summary.

--- Sharpy/src/Generator.cs
+++ Sharpy/src/Generator.cs
@@ -9,16 +9,21 @@ using Sharpy.IProviders;
 
 namespace Sharpy {
     /// <summary>
-    /// </summary>
-    /// <remarks>
-    ///     <para>If you want to generate data just call Generate/GenerateMany.</para>
+    ///     <para>
+    ///         Contains various methods for Generating data and utility methods which you can combine with the generation
+    ///         methods.
+    ///         If you would want the same result every time you invoke these methods you can set the seed for the random
+    ///         required
+    ///         by the constructor.
+    ///     </para>
+    ///     <para>If you want to map this to instantiate another class you can call Generate/GenerateSequence.</para>
     ///     <para>For examples please visit https://github.com/inputfalken/Sharpy </para>
-    /// </remarks>
+    /// </summary>
     public sealed class Generator : GeneratorBase {
-        private readonly Random _random;
-        private Tuple<int, int> _phoneState = new Tuple<int, int>(0, 0);
         private readonly INameProvider _nameProvider;
+        private readonly Random _random;
         private readonly IStringProvider _stringProvider;
+        private Tuple<int, int> _phoneState = new Tuple<int, int>(0, 0);
 
         /// <summary>
         ///     <para>Instantiates a new generator which will generate based on the random supplied</para>
@@ -34,20 +39,18 @@ namespace Sharpy {
             _stringProvider = new UserNameRandomizer(random);
         }
 
-        private Generator(Configurement config)
-            : base(
-                new DoubleRandomizer(config.Random), new IntRandomizer(config.Random), new LongRandomizer(config.Random)
-            ) {
-            _random = config.Random;
-            DateGenerator = new DateGenerator(config.Random);
-            Mailgen = new MailGenerator(config.MailProviders, config.Random);
-            SocialSecurityNumberGenerator = new SecurityNumberGen(config.Random);
-            PhoneNumberGenerator = new NumberGenerator(config.Random);
-            UniqueNumbers = config.UniqueNumbers;
-            _nameProvider = config.Origins == null
-                ? new NameByOrigin(config.Random)
-                : new NameByOrigin(config.Random, config.Origins.ToArray());
-            _stringProvider = new UserNameRandomizer(config.Random);
+        private Generator(Configurement conf)
+            : base(new DoubleRandomizer(conf.Random), new IntRandomizer(conf.Random), new LongRandomizer(conf.Random)) {
+            _random = conf.Random;
+            DateGenerator = new DateGenerator(conf.Random);
+            Mailgen = new MailGenerator(conf.MailProviders, conf.Random);
+            SocialSecurityNumberGenerator = new SecurityNumberGen(conf.Random);
+            PhoneNumberGenerator = new NumberGenerator(conf.Random);
+            UniqueNumbers = conf.UniqueNumbers;
+            _nameProvider = conf.Origins == null
+                ? new NameByOrigin(conf.Random)
+                : new NameByOrigin(conf.Random, conf.Origins.ToArray());
+            _stringProvider = new UserNameRandomizer(conf.Random);
         }
 
         private NumberGenerator PhoneNumberGenerator { get; }
@@ -61,7 +64,7 @@ namespace Sharpy {
         private bool UniqueNumbers { get; } = true;
 
         /// <summary>
-        /// <para>Randomizes one of the arguments.</para>
+        ///     <para>Randomizes one of the arguments.</para>
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="items"></param>
@@ -69,7 +72,7 @@ namespace Sharpy {
         public T Params<T>(params T[] items) => items.RandomItem(_random);
 
         /// <summary>
-        /// <para>Randomizes one of the elements in the IReadOnlyList.</para>
+        ///     <para>Randomizes one of the elements in the IReadOnlyList.</para>
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="items"></param>
@@ -77,27 +80,27 @@ namespace Sharpy {
         public T CustomCollection<T>(IReadOnlyList<T> items) => items.RandomItem(_random);
 
         /// <summary>
-        /// <para>Randomizes a bool.</para>
+        ///     <para>Randomizes a bool.</para>
         /// </summary>
         /// <returns></returns>
         public bool Bool() => _random.Next(2) != 0;
 
         /// <summary>
-        /// <para>Randomizes a date based on age.</para>
+        ///     <para>Randomizes a date based on age.</para>
         /// </summary>
         /// <param name="age"></param>
         /// <returns></returns>
         public LocalDate DateByAge(int age) => DateGenerator.RandomDateByAge(age);
 
         /// <summary>
-        /// <para>Randomizes a date based on year.</para>
+        ///     <para>Randomizes a date based on year.</para>
         /// </summary>
         /// <param name="year"></param>
         /// <returns></returns>
         public LocalDate DateByYear(int year) => DateGenerator.RandomDateByYear(year);
 
         /// <summary>
-        /// <para>Randomizes a unique SocialSecurity Number.</para>
+        ///     <para>Randomizes a unique SocialSecurity Number.</para>
         /// </summary>
         /// <param name="date"></param>
         /// <param name="formated"></param>
@@ -114,7 +117,7 @@ namespace Sharpy {
         }
 
         /// <summary>
-        /// <para>Returns a string representing a mailaddress.</para>
+        ///     <para>Returns a string representing a mailaddress.</para>
         /// </summary>
         /// <param name="name"></param>
         /// <param name="secondName"></param>
@@ -123,7 +126,7 @@ namespace Sharpy {
             => Mailgen.Mail(name, secondName);
 
         /// <summary>
-        /// <para>Returns a number with the length of the argument.</para>
+        ///     <para>Returns a number with the length of the argument.</para>
         /// </summary>
         /// <param name="length"></param>
         /// <returns></returns>
@@ -141,12 +144,16 @@ namespace Sharpy {
         }
 
         /// <summary>
-        /// <para>Returns common names based on argument.</para>
+        ///     <para>Returns common names based on argument.</para>
         /// </summary>
         /// <param name="type"></param>
         /// <returns></returns>
         public string Name(NameType type) => _nameProvider.Name(type);
 
+        /// <summary>
+        ///     <para>Returns a common username.</para>
+        /// </summary>
+        /// <returns></returns>
         public string UserName() => _stringProvider.String();
 
         private static string Prefix<T>(T item, int ammount) => new string('0', ammount).Append(item);
@@ -154,31 +161,39 @@ namespace Sharpy {
         private static string FormatDigit(int i) => i < 10 ? Prefix(i, 1) : i.ToString();
 
         /// <summary>
-        /// <para>Use this class if you want to configure your Generator. then call CreateGenerator method to get the generator.</para>
+        ///     <para>
+        ///         Use this class if you want to configure your Generator. then call CreateGenerator method to get the
+        ///         generator.
+        ///     </para>
         /// </summary>
         public class Configurement {
             /// <summary>
-            /// <para>Gets and Sets the Random which the Generator will use.</para>
+            ///     <para>Gets and Sets the Random which the Generator will use.</para>
             /// </summary>
-            public Random Random { get; set; }
+            public Random Random { get; set; } = new Random();
 
             /// <summary>
             ///     <para>Gets and Sets the mailproviders which will be used for generating MailAddresses.</para>
             ///     <para>This affects Generator's MailAddress method.</para>
             ///     <para>Set to gmail.com, hotmail.com and yahoo.com by default.</para>
             /// </summary>
-            public IReadOnlyList<string> MailProviders { get; set; }
+            public IReadOnlyList<string> MailProviders { get; set; } = new[] {"gmail.com", "hotmail.com", "yahoo.com"};
 
             /// <summary>
-            ///     <para>Gets and Sets if mailaddresses are gonna be unique.</para>
+            ///     <para>Gets and Sets if the generator's MailAddress are return unique mail addresses.</para>
             ///     <para>This affects Generator's MailAddress method.</para>
-            ///     <para>Set to false by Default</para>
+            ///     <para>
+            ///           Set to false by Default
+            ///           NOTE:
+            ///           If this is set to true the following will happen.
+            ///           MailAddress will now append numbers to the end of the mailaddress if it  allready exist.
+            ///     </para>
             /// </summary>
             public bool UniqueMails { get; set; }
 
             /// <summary>
-            /// <para>Gets and Sets the origins of names returned the Generator's Name method.</para>
-            /// <para>Set to nothing by default.</para>
+            ///     <para>Gets and Sets the origins of names returned the Generator's Name method.</para>
+            ///     <para>Set to nothing by default.</para>
             /// </summary>
             public IEnumerable<Origin> Origins { get; set; }
 
@@ -187,19 +202,17 @@ namespace Sharpy {
             ///     <para>Set to false by Default</para>
             ///     <para>
             ///         NOTE:
-            ///         If this is true the following will happen.
-            ///         IGenerator's NumberByLength method will throw an exception if called more than Length^10
+            ///         If this is set to true the following will happen.
+            ///         Generator's NumberByLength method will throw an exception if called more than Length^10
             ///     </para>
             /// </summary>
             public bool UniqueNumbers { get; set; }
 
             /// <summary>
-            /// Creates a Generator with your configurement.
+            ///     Creates a Generator with your configurement.
             /// </summary>
             /// <returns></returns>
             public Generator CreateGenerator() {
-                if (Random == null) Random = new Random();
-                if (MailProviders == null) MailProviders = new[] {"gmail.com", "hotmail.com", "yahoo.com"};
                 return new Generator(this);
             }
         }
