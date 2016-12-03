281153d9d6bf414dad3b57d7a2f55740ce742b22
Robert Andersson <inputfalken@gmail.com>
2016-12-03 16:59:22 +0100

    Removes long extension methods from random.
    Instead it's on one of the implementations of LongProivder.
    
    
    Former-commit-id: 9439875aa696fbf6501a0fceb8f5f335356d6b57

--- Sharpy/src/Implementation/ExtensionMethods/RandomExtensions.cs
+++ Sharpy/src/Implementation/ExtensionMethods/RandomExtensions.cs
@@ -2,51 +2,6 @@
 
 namespace Sharpy.Implementation.ExtensionMethods {
     internal static class RandomExtensionMethods {
-        /// <summary>
-        ///     Returns a random long from min (inclusive) to max (exclusive)
-        /// </summary>
-        /// <param name="random">The given random instance</param>
-        /// <param name="min">The inclusive minimum bound</param>
-        /// <param name="max">The exclusive maximum bound.  Must be greater than min</param>
-        internal static long NextLong(this Random random, long min, long max) {
-            if (max <= min)
-                throw new ArgumentOutOfRangeException(nameof(max), "max must be > min!");
-
-            //Working with ulong so that modulo works correctly with values > long.MaxValue
-            var uRange = (ulong) (max - min);
-
-            //Prevent a modolo bias; see http://stackoverflow.com/a/10984975/238419
-            //for more information.
-            //In the worst case, the expected number of calls is 2 (though usually it's
-            //much closer to 1) so this loop doesn't really hurt performance at all.
-            ulong ulongRand;
-            do {
-                var buf = new byte[8];
-                random.NextBytes(buf);
-                ulongRand = (ulong) BitConverter.ToInt64(buf, 0);
-            } while (ulongRand > ulong.MaxValue - (ulong.MaxValue%uRange + 1)%uRange);
-
-            return (long) (ulongRand%uRange) + min;
-        }
-
-        /// <summary>
-        ///     Returns a random long from 0 (inclusive) to max (exclusive)
-        /// </summary>
-        /// <param name="random">The given random instance</param>
-        /// <param name="max">The exclusive maximum bound.  Must be greater than 0</param>
-        internal static long NextLong(this Random random, long max) {
-            return random.NextLong(0, max);
-        }
-
-        /// <summary>
-        ///     Returns a random long over all possible values of long (except long.MaxValue, similar to
-        ///     random.Next())
-        /// </summary>
-        /// <param name="random">The given random instance</param>
-        internal static long NextLong(this Random random) {
-            return random.NextLong(long.MinValue, long.MaxValue);
-        }
-
         internal static double NextDouble(this Random random, double max) => NextDouble(random, 0, max);
 
         internal static double NextDouble(this Random random, double min, double max) {
