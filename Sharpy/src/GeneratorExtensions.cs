b59dba02c6b7c793966d6a0f4305690542f0b53f
Robert Andersson <inputfalken@gmail.com>
2016-12-26 00:50:27 +0100

    Remove all returns and typeparam comments from extensionmethods

--- Sharpy/src/GeneratorExtensions.cs
+++ Sharpy/src/GeneratorExtensions.cs
@@ -10,9 +10,6 @@ namespace Sharpy {
         /// </summary>
         /// <param name="generator"></param>
         /// <param name="func">The generator supplied is used to provide data for your object.</param>
-        /// <typeparam name="TResult">Mapping result.</typeparam>
-        /// <typeparam name="TGenerator">Data provider.</typeparam>
-        /// <returns></returns>
         public static TResult Generate<TGenerator, TResult>(this TGenerator generator,
             Func<TGenerator, TResult> func) where TGenerator : Generator => func(generator);
 
@@ -22,9 +19,6 @@ namespace Sharpy {
         /// <param name="count">Count of IEnumerable&lt;TResult&gt;</param>
         /// <param name="generator"></param>
         /// <param name="func">The generator supplied is used to provide data for your object.</param>
-        /// <typeparam name="TResult">Mapping result.</typeparam>
-        /// <typeparam name="TGenerator">Data Provider.</typeparam>
-        /// <returns></returns>
         public static IEnumerable<TResult> GenerateSequence<TGenerator, TResult>(this TGenerator generator,
             Func<TGenerator, TResult> func, int count) where TGenerator : Generator {
             for (var i = 0; i < count; i++)
@@ -38,9 +32,6 @@ namespace Sharpy {
         /// <param name="count">Count of IEnumerable&lt;TResult&gt;</param>
         /// <param name="generator"></param>
         /// <param name="func">The argument supplied is used to provide data for your object.</param>
-        /// <typeparam name="TResult">Mapping result.</typeparam>
-        /// <typeparam name="TGenerator">Data provider.</typeparam>
-        /// <returns></returns>
         public static IEnumerable<TResult> GenerateSequence<TGenerator, TResult>(this TGenerator generator,
             Func<TGenerator, int, TResult> func, int count) where TGenerator : Generator {
             for (var i = 0; i < count; i++) yield return func(generator, i);
@@ -51,10 +42,6 @@ namespace Sharpy {
         ///         Iterates through IEnumerable&lt;TSource&gt; exposing each element together with a generator.
         ///     </para>
         /// </summary>
-        /// <typeparam name="TGenerator">Data provider.</typeparam>
-        /// <typeparam name="TSource">Your Collection.</typeparam>
-        /// <typeparam name="TResult">Mapping result.</typeparam>
-        /// <returns></returns>
         public static IEnumerable<TResult> GenerateBySequence<TGenerator, TSource, TResult>(this TGenerator generator,
             IEnumerable<TSource> source, Func<TGenerator, TSource, TResult> func) where TGenerator : Generator {
             foreach (var element in source) yield return func(generator, element);
@@ -66,10 +53,6 @@ namespace Sharpy {
         ///     </para>
         ///     <para>Includes an integer containing the current iteration.</para>
         /// </summary>
-        /// <typeparam name="TGenerator">Data provider.</typeparam>
-        /// <typeparam name="TSource">Your Collection.</typeparam>
-        /// <typeparam name="TResult">Mapping result.</typeparam>
-        /// <returns></returns>
         public static IEnumerable<TResult> GenerateBySequence<TGenerator, TSource, TResult>(this TGenerator generator,
             IEnumerable<TSource> source, Func<TGenerator, TSource, int, TResult> func) where TGenerator : Generator {
             var i = 0;
