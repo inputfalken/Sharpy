c150d7a5a4a81f6ed2750f35a8d8f0e9e81ad31d
Robert Andersson <inputfalken@gmail.com>
2017-03-24 16:37:03 +0100

    Run Resharper's code cleanup.
    Former-commit-id: 3ece678ec6e15f16a84e4f55179c0994d5638799

--- Tests/Integration/NameProvider.cs
+++ Tests/Integration/NameProvider.cs
@@ -23,66 +23,6 @@ namespace Tests.Integration {
         }
 
         [Test]
-        public void Origin_Restricted_Constructor_With_One_Country() {
-            var swedishNameGenerator = new Generator(new Configurement {NameProvider = new NameByOrigin(Origin.Sweden)});
-            var swedishNames = swedishNameGenerator
-                .GenerateSequence(g => g.FirstName(), Count)
-                .ToArray();
-            var allFinishNames = NameByOrigin.GetCollection(Origin.Finland);
-            var allSwedishNames = NameByOrigin.GetCollection(Origin.Sweden);
-            Assert.IsTrue(swedishNames.All(s => allSwedishNames.Contains(s)));
-            Assert.IsFalse(swedishNames.All(s => allFinishNames.Contains(s)));
-        }
-
-        [Test]
-        public void Origin_Restricted_Constructor_With_Two_Countries() {
-            var svDkGenerator = new Generator(
-                new Configurement {
-                    NameProvider = new NameByOrigin(Origin.Sweden, Origin.Denmark)
-                }
-            );
-            var svDkNames = svDkGenerator
-                .GenerateSequence(g => g.FirstName(), Count)
-                .ToArray();
-            var allFinishNames = NameByOrigin.GetCollection(Origin.Finland);
-            var allSvDkNames = NameByOrigin.GetCollection(Origin.Sweden, Origin.Denmark);
-            Assert.IsTrue(svDkNames.All(s => allSvDkNames.Contains(s)));
-            Assert.IsFalse(svDkNames.All(s => allFinishNames.Contains(s)));
-        }
-
-        [Test]
-        public void Origin_Restricted_Constructor_With_One_Country_And_One_Region() {
-            var svDkGenerator = new Generator(
-                new Configurement {
-                    NameProvider = new NameByOrigin(Origin.Sweden, Origin.NorthAmerica)
-                }
-            );
-            var swedishNorthAmericanNames = svDkGenerator
-                .GenerateSequence(g => g.FirstName(), Count)
-                .ToArray();
-            var allSwedishAndNorthAmericanNames = NameByOrigin.GetCollection(Origin.Sweden, Origin.NorthAmerica);
-            var allDanishNames = NameByOrigin.GetCollection(Origin.Denmark);
-            Assert.IsTrue(swedishNorthAmericanNames.All(s => allSwedishAndNorthAmericanNames.Contains(s)));
-            Assert.IsFalse(swedishNorthAmericanNames.All(s => allDanishNames.Contains(s)));
-        }
-
-        [Test]
-        public void Origin_Restricted_Constructor_With_Two_Regions() {
-            var svDkGenerator = new Generator(
-                new Configurement {
-                    NameProvider = new NameByOrigin(Origin.Europe, Origin.NorthAmerica)
-                }
-            );
-            var europeanAndNorthAmericanNames = svDkGenerator
-                .GenerateSequence(g => g.FirstName(), Count)
-                .ToArray();
-            var allEuropeanAndNorthAmericanNames = NameByOrigin.GetCollection(Origin.Europe, Origin.NorthAmerica);
-            var allBrazilianNames = NameByOrigin.GetCollection(Origin.Brazil);
-            Assert.IsTrue(europeanAndNorthAmericanNames.All(s => allEuropeanAndNorthAmericanNames.Contains(s)));
-            Assert.IsFalse(europeanAndNorthAmericanNames.All(s => allBrazilianNames.Contains(s)));
-        }
-
-        [Test]
         public void Female_First_Name_Not_Null_Or_White_Space() {
             var gen = new Generator();
             //Many
@@ -136,6 +76,66 @@ namespace Tests.Integration {
             var name = gen.Generate(g => g.FirstName(Gender.Male));
             Assert.IsFalse(string.IsNullOrWhiteSpace(name));
             Assert.IsFalse(string.IsNullOrWhiteSpace(name));
+        }
+
+        [Test]
+        public void Origin_Restricted_Constructor_With_One_Country() {
+            var swedishNameGenerator = new Generator(new Configurement {NameProvider = new NameByOrigin(Origin.Sweden)});
+            var swedishNames = swedishNameGenerator
+                .GenerateSequence(g => g.FirstName(), Count)
+                .ToArray();
+            var allFinishNames = NameByOrigin.GetCollection(Origin.Finland);
+            var allSwedishNames = NameByOrigin.GetCollection(Origin.Sweden);
+            Assert.IsTrue(swedishNames.All(s => allSwedishNames.Contains(s)));
+            Assert.IsFalse(swedishNames.All(s => allFinishNames.Contains(s)));
+        }
+
+        [Test]
+        public void Origin_Restricted_Constructor_With_One_Country_And_One_Region() {
+            var svDkGenerator = new Generator(
+                new Configurement {
+                    NameProvider = new NameByOrigin(Origin.Sweden, Origin.NorthAmerica)
+                }
+            );
+            var swedishNorthAmericanNames = svDkGenerator
+                .GenerateSequence(g => g.FirstName(), Count)
+                .ToArray();
+            var allSwedishAndNorthAmericanNames = NameByOrigin.GetCollection(Origin.Sweden, Origin.NorthAmerica);
+            var allDanishNames = NameByOrigin.GetCollection(Origin.Denmark);
+            Assert.IsTrue(swedishNorthAmericanNames.All(s => allSwedishAndNorthAmericanNames.Contains(s)));
+            Assert.IsFalse(swedishNorthAmericanNames.All(s => allDanishNames.Contains(s)));
+        }
+
+        [Test]
+        public void Origin_Restricted_Constructor_With_Two_Countries() {
+            var svDkGenerator = new Generator(
+                new Configurement {
+                    NameProvider = new NameByOrigin(Origin.Sweden, Origin.Denmark)
+                }
+            );
+            var svDkNames = svDkGenerator
+                .GenerateSequence(g => g.FirstName(), Count)
+                .ToArray();
+            var allFinishNames = NameByOrigin.GetCollection(Origin.Finland);
+            var allSvDkNames = NameByOrigin.GetCollection(Origin.Sweden, Origin.Denmark);
+            Assert.IsTrue(svDkNames.All(s => allSvDkNames.Contains(s)));
+            Assert.IsFalse(svDkNames.All(s => allFinishNames.Contains(s)));
+        }
+
+        [Test]
+        public void Origin_Restricted_Constructor_With_Two_Regions() {
+            var svDkGenerator = new Generator(
+                new Configurement {
+                    NameProvider = new NameByOrigin(Origin.Europe, Origin.NorthAmerica)
+                }
+            );
+            var europeanAndNorthAmericanNames = svDkGenerator
+                .GenerateSequence(g => g.FirstName(), Count)
+                .ToArray();
+            var allEuropeanAndNorthAmericanNames = NameByOrigin.GetCollection(Origin.Europe, Origin.NorthAmerica);
+            var allBrazilianNames = NameByOrigin.GetCollection(Origin.Brazil);
+            Assert.IsTrue(europeanAndNorthAmericanNames.All(s => allEuropeanAndNorthAmericanNames.Contains(s)));
+            Assert.IsFalse(europeanAndNorthAmericanNames.All(s => allBrazilianNames.Contains(s)));
         }
 
         [Test]
