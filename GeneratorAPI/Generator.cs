51ad0c8a78790989763467a763c38ba50249d84f
Robert Andersson <inputfalken@gmail.com>
2017-04-17 14:08:26 +0200

    Update api

---
+++ GeneratorAPI/Generator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneratorAPI {
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="TProvider"></typeparam>
+    public class Generator<TProvider> {
+        private readonly TProvider _provider;
+
+        public Generator(TProvider provider) => _provider = provider;
+
+        private IEnumerable<TResult> InfiniteEnumerable<TResult>(Func<TProvider, TResult> fn) {
+            while (true) yield return fn(_provider);
+        }
+
+        public Generation<TResult> Generate<TResult>(Func<TProvider, TResult> fn) => new Generation<TResult>(
+            InfiniteEnumerable(fn));
+    }
+}
