c84b2896a010460a33bcd368faee17637ab618c9
Robert Andersson <inputfalken@gmail.com>
2017-04-17 23:23:56 +0200

    Resolve SelectMany bug.
    Added a secondary constructor taking a Func<T> and yield return the invocation of it in a while
    true loop
    
    
    Former-commit-id: a866c21932a7062f8a03d8028d2f5641e6adbaa4

--- GeneratorAPI/Generation.cs
+++ GeneratorAPI/Generation.cs
@@ -4,9 +4,16 @@ using System.Linq;
 
 namespace GeneratorAPI {
     public class Generation<T> {
+        private static IEnumerable<TResult> InfiniteEnumerable<TResult>(Func<TResult> fn) {
+            while (true) yield return fn();
+        }
+
         private readonly IEnumerable<T> _infiniteEnumerable;
 
         public Generation(IEnumerable<T> infiniteEnumerable) => _infiniteEnumerable = infiniteEnumerable;
+
+
+        private Generation(Func<T> fn) : this(InfiniteEnumerable(fn)) { }
 
         public Generation<TResult> Select<TResult>(Func<T, TResult> fn) => new Generation<TResult>(_infiniteEnumerable
             .Select(fn));
@@ -15,17 +22,12 @@ namespace GeneratorAPI {
 
         public IEnumerable<T> Take(int count) => _infiniteEnumerable.Take(count);
 
-        public Generation<TResult> SelectMany<TResult>(Func<T, Generation<TResult>> fn) {
-            return new Generation<TResult>(
-                _infiniteEnumerable.SelectMany(result => fn(result)._infiniteEnumerable));
-        }
+        public Generation<TResult> SelectMany<TResult>(Func<T, Generation<TResult>> fn) => new Generation<TResult>(
+            () => fn(Take()).Take());
 
-        //BUG Runs forever since _infiniteEnumerable never ends.
         public Generation<TCompose> SelectMany<TResult, TCompose>(Func<T, Generation<TResult>> fn,
-            Func<T, TResult, TCompose> composer) {
-            return new Generation<TCompose>(
-                _infiniteEnumerable.SelectMany(result => fn(result)._infiniteEnumerable, composer));
-        }
+            Func<T, TResult, TCompose> composer) => SelectMany(
+            arg => fn(arg).SelectMany(result => new Generation<TCompose>(() => composer(arg, result))));
 
         public Generation<T> Where(Func<T, bool> predicate) => new Generation<T>(
             _infiniteEnumerable.Where(predicate));
