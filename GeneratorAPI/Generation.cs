c1254c650f1b70cb31fcb58ce9aa7be5736e68ee
Robert Andersson <inputfalken@gmail.com>
2017-04-17 23:51:51 +0200

    Add comments about SelectMany
    Former-commit-id: 9d885ec9c0216362e0fd0fecdeae18994af9c2b3

--- GeneratorAPI/Generation.cs
+++ GeneratorAPI/Generation.cs
@@ -22,12 +22,18 @@ namespace GeneratorAPI {
 
         public IEnumerable<T> Take(int count) => _infiniteEnumerable.Take(count);
 
-        public Generation<TResult> SelectMany<TResult>(Func<T, Generation<TResult>> fn) => new Generation<TResult>(
-            () => fn(Take()).Take());
+        public Generation<TResult> SelectMany<TResult>(Func<T, Generation<TResult>> fn) =>
+            // If SelectMany was used on _infiniteEnumerable<T> with _infiniteEnumerable<TResult> the following would happen.
+            // The first generation of _infiniteEnumerable<T> would be used and we would iterate forever on _infiniteEnumerable<TResult>
+            // With current approach a new Generation<TResult> is created and given a Func<TResult> to its constructor.
+            // The Func passed Generates from the result.
+            new Generation<TResult>(() => fn(Take()).Take());
+
 
         public Generation<TCompose> SelectMany<TResult, TCompose>(Func<T, Generation<TResult>> fn,
-            Func<T, TResult, TCompose> composer) => SelectMany(
-            arg => fn(arg).SelectMany(result => new Generation<TCompose>(() => composer(arg, result))));
+            Func<T, TResult, TCompose> composer) {
+            return SelectMany(a => fn(a).SelectMany(r => new Generation<TCompose>(() => composer(a, r))));
+        }
 
         public Generation<T> Where(Func<T, bool> predicate) => new Generation<T>(
             _infiniteEnumerable.Where(predicate));
