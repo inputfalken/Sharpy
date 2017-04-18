7c410dca27ade53b28564f3be46a65e28c615b96
Robert Andersson <inputfalken@gmail.com>
2017-04-18 22:54:44 +0200

    Add comments
    Former-commit-id: 7a2c1f80ca172ef659205b71f78e91b4ee3a494a

--- GeneratorAPI/Generation.cs
+++ GeneratorAPI/Generation.cs
@@ -15,13 +15,35 @@ namespace GeneratorAPI {
 
         private Generation(Func<T> fn) : this(InfiniteEnumerable(fn)) { }
 
-        public Generation<TResult> Select<TResult>(Func<T, TResult> fn) => new Generation<TResult>(_infiniteEnumerable
-            .Select(fn));
+        /// <summary>
+        /// Maps Generation&lt;T&gt; into Generation&lt;TResult&gt;
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="fn"></param>
+        /// <returns></returns>
+        public Generation<TResult> Select<TResult>(Func<T, TResult> fn)
+            => new Generation<TResult>(_infiniteEnumerable.Select(fn));
 
+
+        /// <summary>
+        /// Gives &lt;T&gt;
+        /// </summary>
+        /// <returns></returns>
         public T Take() => _infiniteEnumerable.First();
 
+        /// <summary>
+        /// Yields count ammount of items into an IEnumerable&lt;T&gt;.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
         public IEnumerable<T> Take(int count) => _infiniteEnumerable.Take(count);
 
+        /// <summary>
+        /// Flattens Generation&lt;T&gt;
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="fn"></param>
+        /// <returns></returns>
         public Generation<TResult> SelectMany<TResult>(Func<T, Generation<TResult>> fn) =>
             // If SelectMany was used on _infiniteEnumerable<T> with _infiniteEnumerable<TResult> the following would happen.
             // The first generation of _infiniteEnumerable<T> would be used and we would iterate forever on _infiniteEnumerable<TResult>
@@ -29,13 +51,28 @@ namespace GeneratorAPI {
             // The Func passed Generates from the result.
             new Generation<TResult>(() => fn(Take()).Take());
 
-
+        /// <summary>
+        /// Flattens Generation&lt;T&gt;
+        /// With a compose function using &lt;T&gt; and &lt;TResult&gt;
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <typeparam name="TCompose"></typeparam>
+        /// <param name="fn"></param>
+        /// <param name="composer"></param>
+        /// <returns></returns>
         public Generation<TCompose> SelectMany<TResult, TCompose>(Func<T, Generation<TResult>> fn,
-            Func<T, TResult, TCompose> composer) {
-            return SelectMany(a => fn(a).SelectMany(r => new Generation<TCompose>(() => composer(a, r))));
-        }
+            Func<T, TResult, TCompose> composer)
+            => SelectMany(a => fn(a).SelectMany(r => new Generation<TCompose>(() => composer(a, r))));
 
-        public Generation<T> Where(Func<T, bool> predicate) => new Generation<T>(
-            _infiniteEnumerable.Where(predicate));
+        /// <summary>
+        /// Filters the generation to fit the predicate.
+        /// <remarks>
+        ///     Use with Caution: Bad predicates will make this method run forever.
+        /// </remarks>
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public Generation<T> Where(Func<T, bool> predicate)
+            => new Generation<T>(_infiniteEnumerable.Where(predicate));
     }
 }
--- GeneratorAPI/Generator.cs
+++ GeneratorAPI/Generator.cs
@@ -14,17 +14,40 @@ namespace GeneratorAPI {
             while (true) yield return fn(_provider);
         }
 
+        /// <summary>
+        /// Turn Generator into Generation&lt;TResult&gt;
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="fn"></param>
+        /// <returns></returns>
         public Generation<TResult> Generate<TResult>(Func<TProvider, TResult> fn) => new Generation<TResult>(
             InfiniteEnumerable(fn));
     }
 
     public class Generator {
+        /// <summary>
+        /// Contains methods for creating Generators with various Providers.
+        /// </summary>
         public static GeneratorFactory Factory { get; } = new GeneratorFactory();
     }
 
+    /// <summary>
+    /// Contains methods for creating Generators with various Providers.
+    /// </summary>
     public class GeneratorFactory {
+        /// <summary>
+        /// A Generator using System.Random as provider.
+        /// </summary>
+        /// <param name="random"></param>
+        /// <returns></returns>
         public Generator<Random> RandomGenerator(Random random) => Create(random);
 
+        /// <summary>
+        /// Create a Generator with TProivder as provider.
+        /// </summary>
+        /// <typeparam name="TProvider"></typeparam>
+        /// <param name="provider"></param>
+        /// <returns></returns>
         public Generator<TProvider> Create<TProvider>(TProvider provider) => new Generator<TProvider>(provider);
     }
 }
