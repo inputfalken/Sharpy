093d57674ed09329af065f5df3615d2d2c511f8f
Robert Andersson <inputfalken@gmail.com>
2017-04-30 14:06:50 +0200

    Add tests for null checking

--- Tests/GeneratorAPI/GeneratorTests.cs
+++ Tests/GeneratorAPI/GeneratorTests.cs
@@ -1,4 +1,5 @@
-﻿using GeneratorAPI;
+﻿using System;
+using GeneratorAPI;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 
@@ -20,19 +21,36 @@ namespace Tests.GeneratorAPI {
 
         [Test(
             Author = "Robert",
+            Description = "Check that constructor throws exception if argument is null"
+        )]
+        public void Constructor_Null_Argument() {
+            Assert.Throws<ArgumentNullException>(() => new Generator<Randomizer>(null));
+        }
+
+        [Test(
+            Author = "Robert",
             Description = "Checks that the result from Generate is not null"
         )]
-        public void Test() {
+        public void Generate_Does_Not_Return_With_Valid_Arg() {
             var result = _generator.Generate(randomizer => randomizer.GetString());
             Assert.IsNotNull(result);
         }
 
         [Test(
             Author = "Robert",
+            Description = "Checks that an exception is thrown when generate argument is null"
+        )]
+        public void Generate_With_Null() {
+            Assert.Throws<ArgumentNullException>(() => _generator.Generate<string>(randomizer => null));
+        }
+
+        [Test(
+            Author = "Robert",
             Description = "Checks that provider is assigned in the constructor."
         )]
-        public void Test_Provider_Is_not_Null() {
-            Assert.IsNotNull(_generator);
+        public void Provider_Is_not_Null() {
+            var result = _generator.Provider;
+            Assert.IsNotNull(result);
         }
     }
 }
