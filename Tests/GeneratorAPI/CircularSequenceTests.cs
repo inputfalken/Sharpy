abb34192434a471ee7062878187cd6daa015633e
Robert Andersson <inputfalken@gmail.com>
2017-05-20 14:56:53 +0200

    Add tests

---
+++ Tests/GeneratorAPI/CircularSequenceTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GeneratorAPI;
+using NUnit.Framework;
+
+namespace Tests.GeneratorAPI {
+    [TestFixture]
+    public class CircularSequenceTests {
+        [SetUp]
+        public void Initiate() {
+            _list = new List<string> {
+                "test1",
+                "test2",
+                "test3",
+                "test4",
+                "test5",
+                "test6",
+                "test7",
+                "test8",
+                "test9",
+                "test10"
+            };
+            _generator = Generator.CreateCircularSequence(_list);
+        }
+
+        [TearDown]
+        public void Dispose() {
+            _generator = null;
+        }
+
+        private IGenerator<string> _generator;
+        private List<string> _list;
+
+        [Test(
+            Description = "Verify that generator iterates through the list supplied"
+        )]
+        public void Iterates_Through_List() {
+            Assert.IsTrue(_generator.Take(10).SequenceEqual(_list));
+        }
+
+        [Test(
+            Description = "Verify that passing null to creating of circular sequcene throws"
+        )]
+        public void Null_Enumerable() {
+            Assert.Throws<ArgumentNullException>(() => Generator.CreateCircularSequence<string>(null));
+        }
+
+        [Test(
+            Description = "Verify that generator resets when list ends"
+        )]
+        public void Repeats_When_List_Ends() {
+            var list = _generator.Take(40).ToList();
+            //Verify that you get the correct ammount from take
+            Assert.AreEqual(40, list.Count);
+            // Verify that reseting list works like concat.
+            Assert.IsTrue(list.SequenceEqual(_list.Concat(_list).Concat(_list).Concat(_list)));
+        }
+    }
+}
