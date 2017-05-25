fe703fb1b0a5aa340b66ce61cc5656f6953745b2
Robert Andersson <inputfalken@gmail.com>
2017-05-25 14:33:46 +0200

    Rename class and members of class
    Former-commit-id: f0cec019a1f845fd2cd45fb4f30b8fc0658d6294

--- GeneratorAPI/CircularSequence.cs
+++ GeneratorAPI/CircularSequence.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace GeneratorAPI {
-    internal class CircularEnumerable<T> : IGenerator<T> {
+    internal class CircularSequence<T> : IGenerator<T> {
         private readonly Lazy<IEnumerator<T>> _lazyEnumerator;
 
         private IEnumerator<T> Enumerator {
             get { return _lazyEnumerator.Value; }
         }
 
-        public CircularEnumerable(IEnumerable<T> enumerable) {
+        public CircularSequence(IEnumerable<T> enumerable) {
             _lazyEnumerator = new Lazy<IEnumerator<T>>(enumerable.CacheGeneratedResults().GetEnumerator);
         }
 
-        public CircularEnumerable(Func<IEnumerable<T>> fn) : this(Invoker(fn)) { }
+        public CircularSequence(Func<IEnumerable<T>> fn) : this(Invoker(fn)) { }
 
+        /// <summary>
+        /// QUAS WEX EXORT
+        /// </summary>
+        /// <param name="fn"></param>
+        /// <returns></returns>
         private static IEnumerable<T> Invoker(Func<IEnumerable<T>> fn) {
             while (true) {
-                foreach (var x1 in fn()) {
-                    yield return x1;
+                foreach (var element in fn()) {
+                    yield return element;
                 }
             }
         }
