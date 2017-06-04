363d1a518f1aab6e700190d154b5831cfe8173e2
Robert Andersson <inputfalken@gmail.com>
2017-06-04 16:13:40 +0200

    Move Task based extensions to TaskExtensions.cs

--- GeneratorAPI/TaskExtensions.cs
+++ GeneratorAPI/TaskExtensions.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeneratorAPI
-{
-    internal static class TaskExtensions
-    {
-
+namespace GeneratorAPI {
+    internal static class TaskExtensions {
+        /// <summary>
+        ///     Exposes TSource from IGenerator&lt;Task&lt;TSource&gt;&gt;.
+        /// </summary>
+        public static IGenerator<Task<TSource>> Do<TSource>(this IGenerator<Task<TSource>> taskGenerator,
+            Action<TSource> actionTask) {
+            if (actionTask == null) throw new ArgumentNullException(nameof(actionTask));
+            if (taskGenerator == null) throw new ArgumentNullException(nameof(taskGenerator));
+            return Generator.Function(async () => {
+                var generation = await taskGenerator.Generate();
+                actionTask(generation);
+                return generation;
+            });
+        }
     }
 }
