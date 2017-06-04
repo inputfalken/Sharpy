a224f146b4302c91d9735fb11e2c29ad48b22cf6
Robert Andersson <inputfalken@gmail.com>
2017-06-04 16:12:37 +0200

    Move Task based extensions to TaskExtensions.cs
    Former-commit-id: bbae888b57d50e4a74ea389da566c60688255a56

--- GeneratorAPI/Generator.cs
+++ GeneratorAPI/Generator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace GeneratorAPI {
     /// <summary>
@@ -57,39 +56,6 @@ namespace GeneratorAPI {
             return new Seq<T>(enumerable);
         }
 
-        /// <summary>
-        ///     <para>
-        ///         Maps Generator&lt;Task&lt;TSource&gt;&gt; into Generator&lt;Task&lt;TResult&gt;&gt;
-        ///     </para>
-        /// </summary>
-        public static IGenerator<Task<TResult>> Select<TSource, TResult>(
-            this IGenerator<Task<TSource>> taskGenerator,
-            Func<TSource, TResult> selector) {
-            return taskGenerator.Select(async s => selector(await s));
-        }
-
-
-        /// <summary>
-        ///     <para>
-        ///         Flattens Generator with nested tasks to Generator&lt;Task&lt;TResult&gt;&gt;
-        ///     </para>
-        /// </summary>
-        public static IGenerator<Task<TResult>> SelectMany<TSource, TResult>(
-            this IGenerator<Task<TSource>> taskGenerator,
-            Func<TSource, Task<TResult>> taskSelector) {
-            return taskGenerator.Select(async task => await taskSelector(await task));
-        }
-
-        /// <summary>
-        ///     <para>
-        ///         Maps Generator&lt;&lt;Task&gt;T&gt; into Generator&lt;&lt;Task&gt;T&gt;
-        ///     </para>
-        /// </summary>
-        public static IGenerator<Task<TResult>> SelectMany<TSource, TResult>(
-            this IGenerator<TSource> taskGenerator,
-            Func<TSource, Task<TResult>> taskSelector) {
-            return taskGenerator.Select(async source => await taskSelector(source));
-        }
 
         /// <summary>
         ///     Filters the generation to fit the predicate.
