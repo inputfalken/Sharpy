229259f7adf2436e36d42baf00232cbcd31c8dec
Robert Andersson <inputfalken@gmail.com>
2017-06-05 22:39:05 +0200

    Add async where extension
    Former-commit-id: 56de20dea561ae75e54375944aabfbfc7d915c42

--- GeneratorAPI/TaskExtensions.cs
+++ GeneratorAPI/TaskExtensions.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeneratorAPI
-{
-    internal static class TaskExtensions
-    {
-
+namespace GeneratorAPI {
+    internal static class TaskExtensions {
+        public static IGenerator<Task<TSource>> Where<TSource>(this IGenerator<Task<TSource>> taskGenerator,
+            Func<TSource, bool> predicate, int threshold = 100000) {
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            if (taskGenerator == null) throw new ArgumentNullException(nameof(taskGenerator));
+            // Duplicated from Generator.Where but with async.
+            return Generator.Function(async () => {
+                for (var i = 0; i < threshold; i++) {
+                    var generation = await taskGenerator.Generate();
+                    if (predicate(generation)) return generation;
+                }
+                throw new ArgumentException($"Could not match the predicate with {threshold} attempts. ");
+            });
+        }
     }
 }
