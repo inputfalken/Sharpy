ac8ec49567007aeefe7b980930a744f641a6e84b
Robert Andersson <inputfalken@gmail.com>
2017-06-05 22:45:37 +0200

    Add async Zip methods
    Former-commit-id: 26bd8a2b39964da6d455f74917c2ee69b5b42638

--- GeneratorAPI/TaskExtensions.cs
+++ GeneratorAPI/TaskExtensions.cs
@@ -4,10 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeneratorAPI
-{
-    internal static class TaskExtensions
-    {
+namespace GeneratorAPI {
+    public static class TaskExtensions {
+        public static IGenerator<Task<TResult>> Zip<TFirst, TSecond, TResult>(
+            this IGenerator<Task<TFirst>> firstTaskGenerator,
+            IGenerator<TSecond> second, Func<TFirst, TSecond, TResult> composer) {
+            return firstTaskGenerator.Zip(second, async (l, r) => composer(await l, r));
+        }
 
+        public static IGenerator<Task<TResult>> Zip<TFirst, TSecond, TResult>(this IGenerator<TFirst> first,
+            IGenerator<Task<TSecond>> secondTaskGenerator, Func<TFirst, TSecond, TResult> composer) {
+            return first.Zip(secondTaskGenerator, async (l, r) => composer(l, await r));
+        }
+
+        public static IGenerator<Task<TResult>> Zip<TFirst, TSecond, TResult>(
+            this IGenerator<Task<TFirst>> firstTaskGenerator,
+            IGenerator<Task<TSecond>> secondTaskGenerator, Func<TFirst, TSecond, TResult> composer) {
+            return firstTaskGenerator.Zip(secondTaskGenerator, async (l, r) => composer(await l, await r));
+        }
     }
 }
