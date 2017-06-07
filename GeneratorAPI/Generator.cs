03f5e19eb3216a02434f043ee9d3305246496c1e
RobertOfferta <robert.andersson@offerta.se>
2017-06-07 18:02:08 +0200

    Put IEnumerable consuming methods into EnumerableExtensions

--- GeneratorAPI/Generator.cs
+++ GeneratorAPI/Generator.cs
@@ -6,7 +6,7 @@ namespace GeneratorAPI {
     /// <summary>
     ///     Contains various methods for creating IGenerator&lt;T&gt;
     /// </summary>
-    public static class Generator {
+    public static partial class Generator {
         /// <summary>
         ///     Contains methods for creating Generators with various Providers.
         ///     <remarks>
@@ -137,29 +137,6 @@ namespace GeneratorAPI {
             if (generatorSelector == null) throw new ArgumentNullException(nameof(generatorSelector));
             if (composer == null) throw new ArgumentNullException(nameof(composer));
             return generator.SelectMany(a => generatorSelector(a).SelectMany(r => Function(() => composer(a, r))));
-        }
-
-        /// <summary>
-        ///     Flattens a IGenerator with a IEnumerable
-        /// </summary>
-        public static IGenerator<TResult> SelectMany<TSource, TResult>(this IGenerator<TSource> generator,
-            Func<TSource, IEnumerable<TResult>> enumerableSelector) {
-            if (generator == null) throw new ArgumentNullException(nameof(generator));
-            if (enumerableSelector == null) throw new ArgumentNullException(nameof(enumerableSelector));
-            var sequence = new Seq<TResult>(() => enumerableSelector(generator.Generate()));
-            return Function(() => sequence.Generate());
-        }
-
-        /// <summary>
-        ///     Flattens a IGenerator with a IEnumerable And composes the values
-        /// </summary>
-        public static IGenerator<TCompose> SelectMany<TSource, TResult, TCompose>(this IGenerator<TSource> generator,
-            Func<TSource, IEnumerable<TResult>> enumerableSelector, Func<TSource, TResult, TCompose> composer) {
-            if (generator == null) throw new ArgumentNullException(nameof(generator));
-            if (enumerableSelector == null) throw new ArgumentNullException(nameof(enumerableSelector));
-            if (composer == null) throw new ArgumentNullException(nameof(composer));
-            return generator.SelectMany(source => enumerableSelector(source)
-                .Select(result => composer(source, result)));
         }
 
         /// <summary>
