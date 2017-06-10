e7fa9359e25854fb95d3a50741caf7f213a6be96
Robert Andersson <inputfalken@gmail.com>
2017-06-10 14:51:50 +0200

    Rename test class

--- Tests/GeneratorAPI/GeneratorFactoryTests.cs
+++ Tests/GeneratorAPI/GeneratorFactoryTests.cs
@@ -4,7 +4,26 @@ using NUnit.Framework;
 
 namespace Tests.GeneratorAPI {
     [TestFixture]
-    public class GeneratoeFactoryTests {
+    public class GeneratorFactoryTests {
+        [Test(
+            Description = "Verify that Randomizer behaves like System.Random"
+        )]
+        public void Randomizer_With_Min_Max_Behaves_Like_Same_As_Random() {
+            const int seed = 100;
+            const int length = 1000000;
+            const int maxValue = 100000;
+            const int minValue = 1000;
+            var result = Generator.Factory
+                .Randomizer(minValue, maxValue, seed)
+                .ToArray(length);
+            var expected = Generator
+                .Create(new Random(seed))
+                .Select(rnd => rnd.Next(minValue, maxValue))
+                .ToArray(length);
+
+            Assert.AreEqual(expected, result);
+        }
+
         [Test(
             Description = "Verify that Randomizer behaves like System.Random"
         )]
@@ -25,35 +44,16 @@ namespace Tests.GeneratorAPI {
         [Test(
             Description = "Verify that Randomizer behaves like System.Random"
         )]
-        public void Randomizer_With_Min_Max_Behaves_Like_Same_As_Random() {
-            const int seed = 100;
-            const int length = 1000000;
-            const int maxValue = 100000;
-            const int minValue = 1000;
-            var result = Generator.Factory
-                .Randomizer(minValue, max: maxValue, seed: seed)
-                .ToArray(length);
-            var expected = Generator
-                .Create(new Random(seed))
-                .Select(rnd => rnd.Next(minValue, maxValue))
-                .ToArray(length);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test(
-            Description = "Verify that Randomizer behaves like System.Random"
-        )]
         public void Randomizer_Without_MinValue_Behaves_Like_Random() {
             const int seed = 100;
             const int length = 1000000;
             const int maxValue = 1000;
             var result = Generator.Factory
-                .Randomizer(max: maxValue, seed: seed)
+                .Randomizer(maxValue, seed: seed)
                 .ToArray(length);
             var expected = Generator
                 .Create(new Random(seed))
-                .Select(rnd => rnd.Next(maxValue: maxValue))
+                .Select(rnd => rnd.Next(maxValue))
                 .ToArray(length);
 
             Assert.AreEqual(expected, result);
