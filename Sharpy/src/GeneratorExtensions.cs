8cd2a3d8a6f0099a6404f2361857eb83c9f4e382
Robert Andersson <inputfalken@gmail.com>
2017-06-14 20:45:00 +0200

    Add FirstName generator
    Former-commit-id: 35dc981e8b78e62bbbf2690266e02ff5b81132b6

--- Sharpy/src/GeneratorExtensions.cs
+++ Sharpy/src/GeneratorExtensions.cs
@@ -1,4 +1,7 @@
+using System.Security.Claims;
 using GeneratorAPI;
+using Sharpy.Enums;
+using Sharpy.Implementation;
 using Sharpy.IProviders;
 
 namespace Sharpy {
@@ -21,15 +24,31 @@ namespace Sharpy {
 
         /// <summary>
         ///     <para>
-        ///         INameProvider contains methods which returns strings representing names.
+        ///         Returns a generator which randomizes First names.
         ///     </para>
+        ///     <remarks>
+        ///         If INameProvider is not supplied the implementation will get defaulted to <see cref="NameByOrigin"/>
+        ///     </remarks>
         /// </summary>
-        /// <param name="factory"></param>
-        /// <param name="provider"></param>
-        /// <returns></returns>
-        public static IGenerator<INameProvider> Name(this GeneratorFactory factory, INameProvider provider) {
-            return Generator.Create(provider);
+        public static IGenerator<string> FirstName(this GeneratorFactory factory, INameProvider provider = null) {
+            provider = provider ?? new NameByOrigin();
+            return Generator.Function(provider.FirstName);
         }
+
+        /// <summary>
+        ///     <para>
+        ///         Returns a generator which randomizes First names based on gender supplied.
+        ///     </para>
+        ///     <remarks>
+        ///         If INameProvider is not supplied the implementation will get defaulted to <see cref="NameByOrigin"/>
+        ///     </remarks>
+        /// </summary>
+        public static IGenerator<string> FirstName(this GeneratorFactory factory, Gender gender,
+            INameProvider provider = null) {
+            provider = provider ?? new NameByOrigin();
+            return Generator.Function(() => provider.FirstName(gender));
+        }
+
 
         /// <summary>
         ///     <para>
