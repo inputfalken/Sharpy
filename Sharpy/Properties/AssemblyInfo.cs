5fb5c064979e58a297d5142bf7f1b7e34af69d0f
Robert Andersson <inputfalken@gmail.com>
2017-06-22 23:27:37 +0200

    AssemblyInfo.cs tracks local NuGet versioning

--- Sharpy/Properties/AssemblyInfo.cs
+++ Sharpy/Properties/AssemblyInfo.cs
@@ -40,4 +40,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.121.0")]
+[assembly: AssemblyInformationalVersion("3.0.121")]
