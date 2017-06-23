e67edde19f2b302e245431cdb78ef4e720f00714
Robert Andersson <inputfalken@gmail.com>
2017-06-23 14:05:27 +0200

    Add Cast extension method
    Former-commit-id: df1a926249813e1a8699c16f2a093635dbece441

--- GeneratorAPI/IGenerator.cs
+++ GeneratorAPI/IGenerator.cs
@@ -3,10 +3,14 @@
     ///     Represents a generic generator which can generate any ammount of elements.
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public interface IGenerator<out T> {
+    public interface IGenerator<out T> : IGenerator {
         /// <summary>
         ///     Generate next element.
         /// </summary>
         T Generate();
+    }
+
+    public interface IGenerator {
+        object Generate();
     }
 }
