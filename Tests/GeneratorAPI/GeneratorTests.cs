fe187cf3ac8382a7b5abc083330d6388d779728a
Robert Andersson <inputfalken@gmail.com>
2017-06-24 17:43:04 +0200

    Add Skip extension method and tests verifying that it works
    Former-commit-id: c348bae20ab21b17f5030418909114acb18f96db

--- Tests/GeneratorAPI/GeneratorTests.cs
+++ Tests/GeneratorAPI/GeneratorTests.cs
@@ -900,6 +900,7 @@ namespace Tests.GeneratorAPI {
             Description = "Verify that char casting works"
         )]
         public void Cast_Object_To_Char() {
+            Enumerable.Range(0, 1).Skip(Seed);
             var generator = Generator
                 .CircularSequence(new List<object> {'1', '2'})
                 .Cast<char>();
@@ -936,6 +937,86 @@ namespace Tests.GeneratorAPI {
                 .Factory
                 .Incrementer();
             Assert.IsInstanceOf<int>(noneGeneric.Generate());
+        }
+
+        [Test(
+            Description = "Verify that you can skip fifty elements"
+        )]
+        public void Skip_Fifty() {
+            var generator = Generator
+                .Factory
+                .Incrementer(1)
+                .Skip(50);
+            Assert.AreEqual(51, generator.Generate());
+        }
+
+        [Test(
+            Description = "Verify that you can skip zero elements"
+        )]
+        public void Skip_Zero() {
+            var generator = Generator
+                .Factory
+                .Incrementer(1)
+                .Skip(0);
+            Assert.AreEqual(1, generator.Generate());
+        }
+
+        [Test(
+            Description = "Verify that you can skip zero elements and it does nothing"
+        )]
+        public void Skip_Zero_Does_Nothing() {
+            var result = Generator
+                .Factory
+                .Incrementer(1)
+                .Skip(0);
+
+            var expected = Generator
+                .Factory
+                .Incrementer(1);
+            Assert.AreEqual(expected.Generate(), result.Generate());
+        }
+        [Test(
+            Description = "Verify that skipping 0 elements returns same instance"
+        )]
+        public void Skip_Zero_Returns_Same_instance() {
+            var generator = Generator
+                .Factory
+                .Incrementer(1);
+
+            Assert.AreSame(generator, generator.Skip(0));
+            Assert.AreNotSame(generator, generator.Skip(1));
+        }
+
+        [Test(
+            Description = "Verify that you can't skip negative numbers"
+        )]
+        public void Skip_Negative_Number_Throws() {
+            var generator = Generator
+                .Factory
+                .Incrementer(1);
+            Assert.Throws<ArgumentException>(() => generator.Skip(-1));
+        }
+
+        [Test(
+            Description = "Verify that using skip with null generator throws"
+        )]
+        public void Skip_Null_Generator_Throws() {
+            IGenerator<int> generator = null;
+            Assert.Throws<ArgumentNullException>(() => generator.Skip(1));
+        }
+
+        [Test(
+            Description = "Verify that skip is lazy Evaluated"
+        )]
+        public void Skip_Is_Lazy_Evaluated() {
+            var invoked = false;
+             Generator
+                .Function<string>(() => {
+                    invoked = true;
+                    return null;
+                })
+                .Skip(50);
+            Assert.AreEqual(false, invoked);
         }
     }
 }
