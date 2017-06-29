64f7ead7837732b2f709a938500900245541e866
Robert Andersson <inputfalken@gmail.com>
2017-06-29 23:36:25 +0200

    Remove unused methods

--- Sharpy/src/GeneratorExtensions.cs
+++ Sharpy/src/GeneratorExtensions.cs
@@ -1,5 +1,4 @@
 using GeneratorAPI;
-using Sharpy.IProviders;
 
 namespace Sharpy {
     /// <summary>
@@ -15,54 +14,6 @@ namespace Sharpy {
         /// <param name="provider"></param>
         /// <returns></returns>
         public static IGenerator<Provider> Provider(this GeneratorFactory factory, Provider provider) {
-            return Generator.Create(provider);
-        }
-
-        /// <summary>
-        ///     <para>
-        ///         INameProvider contains methods which returns strings representing names.
-        ///     </para>
-        /// </summary>
-        /// <param name="factory"></param>
-        /// <param name="provider"></param>
-        /// <returns></returns>
-        public static IGenerator<INameProvider> Name(this GeneratorFactory factory, INameProvider provider) {
-            return Generator.Create(provider);
-        }
-
-        /// <summary>
-        ///     <para>
-        ///         ILongProvider contains methods which returns longs.
-        ///     </para>
-        /// </summary>
-        /// <param name="factory"></param>
-        /// <param name="provider"></param>
-        /// <returns></returns>
-        public static IGenerator<ILongProvider> Long(this GeneratorFactory factory, ILongProvider provider) {
-            return Generator.Create(provider);
-        }
-
-        /// <summary>
-        ///     <para>
-        ///         IDoubleProvider contains methods which returns doubles.
-        ///     </para>
-        /// </summary>
-        /// <param name="factory"></param>
-        /// <param name="provider"></param>
-        /// <returns></returns>
-        public static IGenerator<IDoubleProvider> Double(this GeneratorFactory factory, IDoubleProvider provider) {
-            return Generator.Create(provider);
-        }
-
-        /// <summary>
-        ///     <para>
-        ///         IIntegerProvider contains methods which returns integers.
-        ///     </para>
-        /// </summary>
-        /// <param name="factory"></param>
-        /// <param name="provider"></param>
-        /// <returns></returns>
-        public static IGenerator<IIntegerProvider> Integer(this GeneratorFactory factory, IIntegerProvider provider) {
             return Generator.Create(provider);
         }
     }
