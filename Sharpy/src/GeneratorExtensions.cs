92610a1af8dd834d62aa6d5d77fe31de0a3cfbd9
Robert Andersson <inputfalken@gmail.com>
2017-07-01 12:09:57 +0200

    Add extension method LastName

--- Sharpy/src/GeneratorExtensions.cs
+++ Sharpy/src/GeneratorExtensions.cs
@@ -46,5 +46,18 @@ namespace Sharpy {
             provider = provider ?? new NameByOrigin();
             return Generator.Function(() => provider.FirstName(gender));
         }
+
+        /// <summary>
+        ///     Creates <see cref="IGenerator{T}"/> whose generic argument is <see cref="string"/>.
+        ///     Each invokation of <see cref="IGenerator{T}.Generate"/> will return a <see cref="string"/> representing a last name.
+        ///     <para> </para>
+        ///     <remarks>
+        ///         If an implementation of <see cref="INameProvider"/> is not supplied the implementation will get defaulted to <see cref="NameByOrigin"/>
+        ///     </remarks>
+        /// </summary>
+        public static IGenerator<string> LastName(this GeneratorFactory factory, INameProvider lastNameProvider = null) {
+            lastNameProvider = lastNameProvider ?? new NameByOrigin();
+            return Generator.Function(lastNameProvider.LastName);
+        }
     }
 }
