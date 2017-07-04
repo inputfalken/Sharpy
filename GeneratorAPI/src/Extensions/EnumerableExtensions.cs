41abd70cad4c0c4e67e1f12ef953d0a891fa0f75
Robert Andersson <inputfalken@gmail.com>
2017-07-04 15:00:36 +0200

    Rename Type parameters

--- GeneratorAPI/src/Extensions/EnumerableExtensions.cs
+++ GeneratorAPI/src/Extensions/EnumerableExtensions.cs
@@ -39,17 +39,17 @@ namespace GeneratorAPI.Extensions {
         /// <param name="enumerableSelector">The function to consume <see cref="IEnumerable{T}" />.</param>
         /// <param name="resultSelector">The function to compose .</param>
         /// <typeparam name="TSource">The type of <paramref name="generator" />.</typeparam>
-        /// <typeparam name="TResult">The type of the <see cref="IEnumerable{T}" />.</typeparam>
-        /// <typeparam name="TCompose">The type of the <see cref="IEnumerable{T}" />.</typeparam>
+        /// <typeparam name="TCollection">The type of the <see cref="IEnumerable{T}" />.</typeparam>
+        /// <typeparam name="TResult">The type of the returning <see cref="IGenerator{T}"/>.</typeparam>
         /// <returns>
         ///     TODO
         /// </returns>
         /// <exception cref="ArgumentNullException">Argument <paramref name="generator" /> is null</exception>
         /// <exception cref="ArgumentNullException">Argument <paramref name="enumerableSelector" /> is null</exception>
         /// <exception cref="ArgumentNullException">Argument <paramref name="resultSelector" /> is null</exception>
-        public static IGenerator<TCompose> SelectMany<TSource, TResult, TCompose>(
+        public static IGenerator<TResult> SelectMany<TSource, TCollection, TResult>(
             this IGenerator<TSource> generator,
-            Func<TSource, IEnumerable<TResult>> enumerableSelector, Func<TSource, TResult, TCompose> resultSelector) {
+            Func<TSource, IEnumerable<TCollection>> enumerableSelector, Func<TSource, TCollection, TResult> resultSelector) {
             if (generator == null) throw new ArgumentNullException(nameof(generator));
             if (enumerableSelector == null) throw new ArgumentNullException(nameof(enumerableSelector));
             if (resultSelector == null) throw new ArgumentNullException(nameof(resultSelector));
