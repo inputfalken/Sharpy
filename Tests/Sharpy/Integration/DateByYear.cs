6943777796e56ee97dc3518196d3461fb8f827eb
Robert Andersson <inputfalken@gmail.com>
2017-07-08 23:30:05 +0200

    Refactor the usage of Sharpy with GeneratorAPI
    Former-commit-id: dbfbb28238f157b24639463145dd86ed2ad23122

--- Tests/Sharpy/Integration/DateByYear.cs
+++ Tests/Sharpy/Integration/DateByYear.cs
@@ -9,21 +9,21 @@ namespace Tests.Sharpy.Integration {
     public class DateByYear {
         [Test]
         public void Arg_MinusOne() {
-            var randomGenerator = Generator.Factory.Provider(new Provider());
+            var randomGenerator = Generator.Create(new Provider());
             Assert.Throws<ArgumentException>(
                 () => randomGenerator.Select(generator => generator.DateByYear(-1)).Generate());
         }
 
         [Test]
         public void Arg_TwoThousand() {
-            var randomGenerator = Generator.Factory.Provider(new Provider());
+            var randomGenerator = Generator.Create(new Provider());
             var result = randomGenerator.Select(generator => generator.DateByYear(2000)).Generate();
             Assert.AreEqual(result.Year, 2000);
         }
 
         [Test]
         public void Arg_TwoThousandTen() {
-            var randomGenerator = Generator.Factory.Provider(new Provider());
+            var randomGenerator = Generator.Create(new Provider());
             var result = randomGenerator.Select(generator => generator.DateByYear(2010)).Generate();
             Assert.AreEqual(result.Year, 2010);
         }
