c141206d966b5ec21cd17814f2fe6b7658391870
Robert Andersson <inputfalken@gmail.com>
2017-07-18 19:37:55 +0200

    Update travis script
    Former-commit-id: 4b18dd79cf0427e30c793dbf3eaf6ce3a26fa9c5

--- travis.sh
+++ travis.sh
@@ -10,30 +10,29 @@ reset=$(tput sgr0)
 #                                              Setup
 ####################################################################################################
 mkdir .nuget
+
 echo "${yellow}Downloading NuGet ${reset}"
-if ! wget -O .nuget/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-then
+if wget -O .nuget/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe ; then
   echo "${green}Download Successfull${reset}"
 fi
 
 echo "${yellow}Restoring solution with NuGet${reset}"
-if ! mono .nuget/nuget.exe restore Sharpy.sln -Verbosity quiet 
-then
+if mono .nuget/nuget.exe restore Sharpy.sln -Verbosity quiet ; then
   echo "${green}Restore Successfull${reset}"
 fi
 
 mkdir testrunner
+
 echo "${yellow}Installing NUnit 3.6.1 with NuGet${reset}"
-if ! mono .nuget/nuget.exe install NUnit.Runners -Version 3.6.1 -OutputDirectory testrunner -Verbosity quiet
-then
+if mono .nuget/nuget.exe install NUnit.Runners -Version 3.6.1 -OutputDirectory testrunner -Verbosity quiet ; then
   echo "${green}Installation Successfull${reset}"
 fi
+
 ####################################################################################################
 #                                              Build
 ####################################################################################################
 echo "${yellow}Starting build on solution with msbuild ${reset}"
-if ! msbuild /v:minimal /p:Configuration=Release Sharpy.sln
-then
+if msbuild /v:minimal /p:Configuration=Release Sharpy.sln ; then
   echo "${green}Build Succeeded"
 fi
 ####################################################################################################
