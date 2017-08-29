3f551e283c7d522cc632fed2ca8f274126ae024d
Robert Andersson <robert.andersson@offerta.se>
2017-08-29 09:54:49 +0200

    Add tests

---
+++ Tests/Sharpy/FactoryTests/ListRandomizer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using Sharpy;
+using Sharpy.Generator;
+using Sharpy.Generator.Linq;
+
+namespace Tests.Sharpy.FactoryTests {
+    [TestFixture]
+    internal class ListRandomizer {
+        [Test]
+        public void Null_List_Throws() {
+            Assert.Throws<ArgumentNullException>(() => Factory.ListRandomizer<int>(null));
+        }
+
+        [Test]
+        public void Randomizes_As_Expected_With_Seed() {
+            var items = new List<string> {"Foo", "Bar", "Doe"};
+            var result = Factory
+                .ListRandomizer(items, 20)
+                .ToList(100);
+            var expected = Generator
+                .Create(new Random(20))
+                .Select(rnd => items[rnd.Next(items.Count)])
+                .ToList(100);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Randomizes_Different_Values_If_No_Seed_Is_Provided() {
+            var items = new List<string> {"Foo", "Bar", "Doe"};
+            var result = Factory
+                .ListRandomizer(items)
+                .ToList(100);
+            // So the seed can change
+            Thread.Sleep(100);
+            var expected = Generator
+                .Create(new Random())
+                .Select(rnd => items[rnd.Next(items.Count)])
+                .ToList(100);
+            Assert.AreNotEqual(expected, result);
+        }
+    }
+}
