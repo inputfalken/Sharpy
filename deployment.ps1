23f599c04736f0866369ab890433b6ea85e4d61f
Robert Andersson <robert.andersson@offerta.se>
2017-08-31 16:06:48 +0200

    Deploying is more dynamic

--- deployment.ps1
+++ deployment.ps1
@@ -1,4 +1,10 @@
-param([string] $project, [string] $repo)
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $project,
+    [Parameter(Mandatory=$false)]
+    [string] $repo
+)
 # Pack package to root directory of project and returns the file.
 function Pack ([string] $project, [bool] $isAlpha) {
   $currentDirectory = (Resolve-Path .\).Path
@@ -23,28 +29,29 @@ function Deploy ([string] $package) {
 function Is-Alpha([string] $branch) {
   switch ($branch) {
     "development" {
-      Write-Host "Proceeding script with alpha version for branch: $branch." -ForegroundColor yellow
+      Write-Host "Proceeding script with alpha version for branch: $branch." -ForegroundColor Yellow
       return 1
     }
     "master" {
-      Write-Host "Proceeding script with stable version for branch: $branch." -ForegroundColor yellow
+      Write-Host "Proceeding script with stable version for branch: $branch." -ForegroundColor Yellow
       return 0
     }
     default {
-      Write-Host "$branch is not a deployable branch exiting..." -ForegroundColor yellow
+      Write-Host "$branch is not a deployable branch exiting..." -ForegroundColor Yellow
       exit
     }
   }
 }
 # Fetch the online version
 function Fetch-OnlineVersion ([string] $listSource, [string] $projectName, [bool] $isAlpha) {
-  Write-Host "Fetching NuGet version from $listSource" -ForegroundColor yellow
+  Write-Host "Fetching NuGet Package '$projectName' from $listSource" -ForegroundColor Yellow
   # Use alpha version if the current branch is development.
   if ($isAlpha) {
     $packageName = NuGet list $projectName -PreRelease -Source $listSource
   } else {
     $packageName = NuGet list $projectName -Source $listSource
   }
+  Write-Host "Found package '$packageName'." -ForegroundColor Green
   # $packageName comes in format: "packageName 1.0.0".
   $version = ($packageName.Split(" ") | Select-Object -Last 1)
   # In alpha version the version also includes the string "version-alpha" where version is the semver.
@@ -81,20 +88,24 @@ function Update-GHPages {
   git push origin gh-pages -q
 }
 
+Write-Host "Starting script with project $project$(if($repo){" and for repo $repo"} )." -ForegroundColor Magenta
 [string] $branch = $env:APPVEYOR_REPO_BRANCH
 [bool] $isAlpha = Is-Alpha $branch
-[string] $name = ($project.Split('\\') | select -last 1).Split('.') | select -first 1
+[string] $name = $project.SubString(0, $project.Length - 7).split('\\') | select -last 1
 [version] $onlineVersion = Fetch-OnlineVersion 'https://nuget.org/api/v2/' $name $isAlpha
 [version] $localVersion = Get-LocalVersion $project
 
-Write-Host "Comparing Local version($localVersion) with online version($onlineVersion)"
+Write-Host "Comparing Local $name version($localVersion) with online $name version($onlineVersion)" -ForegroundColor Yellow
 if ($localVersion -gt $onlineVersion) {
   Write-Host "Local version($localVersion) is greater than the online version($onlineVersion), performing deployment" -ForegroundColor Yellow
   $nupkg = Pack $project $isAlpha
   Deploy $nupkg.Name
   # If it's the master branch
   #if(!$isAlpha) {
-    Update-GHPages
+    # If repo is null, it means that GH-Pages will not be updated.
+    if ($repo) {
+      Update-GHPages
+    }
   #} else {
   #  Write-Host "Alpha version, skipping documentation update." -ForegroundColor Yellow
   #}
