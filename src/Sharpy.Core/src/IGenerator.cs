50f8d5c6d819af00166e61fb7db0024b497eada5
Robert Andersson <robert.andersson@offerta.se>
2017-09-04 09:44:54 +0200

    Remove new keyword from interface
    Former-commit-id: e271328ab161d18c97babd8a011a6806ce7b1981

--- src/Sharpy.Core/src/IGenerator.cs
+++ src/Sharpy.Core/src/IGenerator.cs
@@ -3,14 +3,13 @@
     ///     <para>Represent a generator which can generate any amount of elements by invoking method <see cref="Generate" />.</para>
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public interface IGenerator<out T>  {
+    public interface IGenerator<out T> {
         /// <summary>
         ///     <para>Generate next element.</para>
         /// </summary>
         /// <returns>
         ///     A generated element.
         /// </returns>
-        new T Generate();
+        T Generate();
     }
-
 }
