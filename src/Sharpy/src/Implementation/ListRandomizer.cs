0650e6de481e5841258d11700215f53f9633e432
Robert Andersson <robert.andersson@offerta.se>
2017-09-15 16:18:29 +0200

    Add interface to handle collections
    Former-commit-id: db211ada1d535adaf950f78a2ac403530916e410

---
+++ src/Sharpy/src/Implementation/ListRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Sharpy.Implementation.ExtensionMethods;
+using Sharpy.IProviders;
+
+namespace Sharpy.Implementation {
+    public class ListRandomizer : IListElementPicker {
+        private readonly Random _random;
+
+        public ListRandomizer(Random random) => _random = random ?? throw new ArgumentNullException(nameof(random));
+
+        public T TakeElement<T>(IReadOnlyList<T> list) => list.RandomItem(_random);
+
+        public T TakeArgument<T>(params T[] list) => list.RandomItem(_random);
+    }
+}
