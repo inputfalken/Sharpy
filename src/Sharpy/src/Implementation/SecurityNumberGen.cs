793533046a6b16d99614787c4410af6c5baa6497
Robert Andersson <robert.andersson@offerta.se>
2017-09-15 17:19:12 +0200

    Create controlnumber in Securitynumber generator

--- src/Sharpy/src/Implementation/SecurityNumberGen.cs
+++ src/Sharpy/src/Implementation/SecurityNumberGen.cs
@@ -5,7 +5,8 @@ namespace Sharpy.Implementation {
     internal class SecurityNumberGen : Unique<long> {
         internal SecurityNumberGen(Random random) : base(random) { }
 
-        internal long SecurityNumber(int controlNumber, string dateNumber) {
+        internal long SecurityNumber(string dateNumber) {
+            var controlNumber = Random.Next(10000);
             var number = long.Parse(dateNumber + controlNumber);
             var resets = 0;
             while (HashSet.Contains(number)) {
