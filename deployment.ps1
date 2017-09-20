3e7323a9a415e186415d6ef11a8ecfe1f286f8ed
Robert Andersson <robert.andersson@offerta.se>
2017-09-20 10:58:38 +0200

    Improve deploy script

--- deployment.ps1
+++ deployment.ps1
@@ -12,10 +12,10 @@ param(
     [bool] $deletePackage = $false
 )
 # Pack package to root directory of project and returns the file.
-function Pack ([string] $project, [bool] $isAlpha) {
+function Pack ([string] $project, [bool] $preRelease, [string] $suffix) {
   $currentDirectory = (Resolve-Path .\).Path
-  if ($isAlpha) {
-    dotnet pack $project --version-suffix alpha -c Release -o $currentDirectory
+  if ($preRelease) {
+    dotnet pack $project --version-suffix $suffix -c Release -o $currentDirectory
   } else {
     dotnet pack $project -c Release -o $currentDirectory
   }
@@ -31,28 +31,11 @@ function Deploy ([string] $package) {
     throw "$project could not be pushed by command 'dotnet nuget push'."
   }
 }
-# If returns true if the branch is development and false if it's master.
-function Is-Alpha([string] $branch) {
-  switch ($branch) {
-    "development" {
-      Write-Host "Proceeding script with alpha version for branch: $branch." -ForegroundColor Yellow
-      return 1
-    }
-    "master" {
-      Write-Host "Proceeding script with stable version for branch: $branch." -ForegroundColor Yellow
-      return 0
-    }
-    default {
-      Write-Host "$branch is not a deployable branch exiting..." -ForegroundColor Yellow
-      exit
-    }
-  }
-}
 # Fetch the online version
-function Fetch-OnlineVersion ([string] $listSource, [string] $projectName, [bool] $isAlpha) {
+function Fetch-OnlineVersion ([string] $listSource, [string] $projectName, [bool] $preRelease) {
   Write-Host "Fetching NuGet Package '$projectName' from $listSource" -ForegroundColor Yellow
   # Use alpha version if the current branch is development.
-  if ($isAlpha) {
+  if ($preRelease) {
     $packageName = NuGet list $projectName -PreRelease -Source $listSource
   } else {
     $packageName = NuGet list $projectName -Source $listSource
@@ -62,7 +45,7 @@ function Fetch-OnlineVersion ([string] $listSource, [string] $projectName, [bool
   # $packageName comes in format: "packageName 1.0.0" and can contain multiple strings!.
   $version = (($packageName | select -First 1).Split(" ") | Select-Object -Last 1)
   # In alpha version the version also includes the string "version-alpha" where version is the semver.
-  if ($isAlpha) {
+  if ($preRelease) {
     $version = ($version | select -Last 1).Split("-") | select -First 1
   }
   # A hack to get set the revision property to zero.
@@ -104,7 +87,7 @@ function Delete-Package ([string] $package, [string] $source = 'https://www.nuge
   $sections = $package.Split('-')
   $packageName = $sections[0]
   $version = $sections[1..$sections.length] -join '-'
-  Write-Host "Attempting to delete package '$packageName' version '$version'."
+  Write-Host "Attempting to delete package '$packageName', version '$version'."
   NuGet delete $packageName $version -NonInteractive -Source $source -ApiKey $env:NUGET_API_KEY
   if (!$?) {
     throw "Could not delete package '$packageName' version '$version'."
@@ -113,31 +96,51 @@ function Delete-Package ([string] $package, [string] $source = 'https://www.nuge
   }
 }
 
-Write-Host "Starting script with project $project$(if($repo){" and for repo $repo"} )." -ForegroundColor Green
-[string] $branch = $env:APPVEYOR_REPO_BRANCH
-[bool] $isAlpha = Is-Alpha $branch
-[string] $name = $project.SubString(0, $project.Length - 7).split('\\') | select -last 1
-[version] $onlineVersion = Fetch-OnlineVersion 'https://nuget.org/api/v2/' $name $isAlpha
-[version] $localVersion = Get-LocalVersion $project
+function Start-Deployment ([bool] $preRelease, [string] $suffix = 'alpha') {
+  [string] $name = $project.SubString(0, $project.Length - 7).split('\\') | select -last 1
+  [string] $suffix = 'alpha'
+  [version] $onlineVersion = Fetch-OnlineVersion 'https://nuget.org/api/v2/' $name $preRelease
+  [version] $localVersion = Get-LocalVersion $project
 
-Write-Host "Comparing Local $name version($localVersion) with online $name version($onlineVersion)" -ForegroundColor Yellow
-if ($localVersion -gt $onlineVersion) {
-  Write-Host "Local version($localVersion) is greater than the online version($onlineVersion), performing deployment" -ForegroundColor Yellow
-  if ($deploy) {
-    $nupkg = Pack $project $isAlpha
-    Deploy $nupkg.Name
-    if ($deletePackage) {
-      if ($isAlpha) {
-        # find a common way to toggle the alpha
-        Delete-Package "$($name)-$($onlineVersion)-alpha"
-      } else {
-        Delete-Package "$($name)-$($onlineVersion)"
+  Write-Host "Comparing Local $name version '$localVersion' with online $name version '$onlineVersion'" -ForegroundColor Yellow
+  if ($localVersion -gt $onlineVersion) {
+    Write-Host "Local version '$localVersion' is greater than the online version '$onlineVersion', continuing..." -ForegroundColor Yellow
+    if ($deploy) {
+      $nupkg = Pack $project $preRelease $suffix
+      Deploy $nupkg.Name
+      if ($deletePackage) {
+        if ($preRelease) {
+          Delete-Package "$($name)-$($onlineVersion)-$suffix"
+        } else {
+          Delete-Package "$($name)-$($onlineVersion)"
+        }
       }
     }
+    if ($useDocfx) {
+      Update-GHPages
+    }
+  } else {
+    Write-Host "Local version '$localVersion' is not greater than online version '$onlineVersion', skipping deployment" -ForegroundColor Yellow
   }
-  if ($useDocfx) {
-    Update-GHPages
+}
+####################################################################################################
+#                                              Start                                               #
+####################################################################################################
+Write-Host "Starting script for project '$project'" -ForegroundColor Green
+switch ($env:APPVEYOR_REPO_BRANCH) {
+  'development' {
+    Write-Host "Proceeding script with alpha version for branch: $branch." -ForegroundColor Yellow
+    Start-Deployment -PreRelease $true
   }
-} else {
-  Write-Host "Local version($localVersion) is not greater than online version($onlineVersion), skipping deployment" -ForegroundColor Yellow
+  'master' {
+    Write-Host "Proceeding script with stable version for branch: $branch." -ForegroundColor Yellow
+    Start-Deployment -PreRelease $false
+  }
+  [string]::Empty {
+    throw 'Branch can not be empty'
+  }
+  default {
+    Write-Host "$branch is not a deployable branch exiting..." -ForegroundColor Yellow
+    exit
+  }
 }
