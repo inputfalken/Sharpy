5746f8880172aa968aa023ef67928548d2b6f30c
Robert Andersson <inputfalken@gmail.com>
2017-09-24 23:17:50 +0200

    Split Argument method from listprovider
    Former-commit-id: dfead7f07ed6805925048b221dc92e22c0b326c9

---
+++ src/Sharpy/src/Implementation/ArgumentRandomizer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Sharpy.IProviders;
+
+namespace Sharpy.Implementation {
+    /// <summary>
+    ///     Randomizes from the arguments.
+    /// </summary>
+    public class ArgumentRandomizer : IArgumentProvider {
+        private readonly Random _random;
+        public ArgumentRandomizer(Random random) => _random = random ?? throw new ArgumentNullException(nameof(random));
+
+        /// <summary>
+        ///     Returns a randomized element from the arguments supplied.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type of the arguments.
+        /// </typeparam>
+        /// <param name="first">
+        ///     First argument.
+        /// </param>
+        /// <param name="second">
+        ///     Second argument.
+        /// </param>
+        /// <param name="additional">
+        ///     Additional arguments after argument <paramref name="first" /> and argument <paramref name="second" /> has been
+        ///     supplied.
+        /// </param>
+        /// <returns>
+        ///     One of the arguments supplied.
+        /// </returns>
+        public T Argument<T>(T first, T second, params T[] additional) {
+            var res = _random.Next(-2, additional.Length);
+            switch (res) {
+                case -2: return first;
+                case -1: return second;
+                default: return additional[res];
+            }
+        }
+    }
+}
