e98381c9fbac5e8c2a33cdbbfd67d8d6fb391089
Robert Andersson <robert.andersson@offerta.se>
2017-09-27 11:54:27 +0200

    Remove unused class

--- src/Sharpy/src/Implementation/UniqueIntegerRandomizer.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Sharpy.Implementation {
-    internal sealed class UniqueRandomizerIntegerRandomizer : UniqueRandomizer<int> {
-        internal UniqueRandomizerIntegerRandomizer(Random random)
-            : base(random) { }
-    }
-}
