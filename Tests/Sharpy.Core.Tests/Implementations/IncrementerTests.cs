449658830d115e48ec8cb7e127703f3ecb00ee6b
Robert Andersson <robert.andersson@offerta.se>
2017-10-09 16:50:17 +0200

    Add incrementer tests

--- Tests/Sharpy.Core.Tests/Implementations/IncrementerTests.cs
+++ Tests/Sharpy.Core.Tests/Implementations/IncrementerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 using Sharpy.Core;
@@ -14,10 +15,33 @@ namespace Tests.Sharpy.Core.Tests.Implementations {
         }
 
         [Test]
+        public void Long_MinValue_Does_Not_Throw() {
+            var result = Generator.Incrementer(long.MinValue).Take(500);
+            Assert.DoesNotThrow(() => result.ToArray());
+        }
+
+        [Test]
         public void Start_Below_Int_MinValue_Throws() {
             var start = int.MinValue;
             var result = Generator.Incrementer(start - 1).Take(1);
             Assert.Throws<OverflowException>(() => result.ToArray());
+        }
+
+        [Test]
+        public void Start_Below_Long_MinValue_Throws() {
+            var start = long.MinValue;
+            var result = Generator.Incrementer(start - 1).Take(1);
+            Assert.Throws<OverflowException>(() => result.ToArray());
+        }
+
+        [Test]
+        public void Start_Int_MaxValue_Minus_Thousand() {
+            var start = int.MaxValue - 1000;
+            var count = 500;
+            var result = Generator.Incrementer(start).Take(count);
+            var expected = new List<int>();
+            for (var i = start; i < start + count; i++) expected.Add(i);
+            Assert.AreEqual(expected, result);
         }
 
         [Test]
@@ -27,29 +51,43 @@ namespace Tests.Sharpy.Core.Tests.Implementations {
         }
 
         [Test]
-        public void Start_Minus_Twenty() {
-            var result = Generator.Incrementer(-20).Take(500);
-            var expected = Enumerable.Range(-20, 500);
+        public void Start_Long_MaxValue_Minus_Thousand() {
+            var start = long.MaxValue - 1000;
+            var count = 500;
+            var result = Generator.Incrementer(start).Take(count);
+            var expected = new List<long>();
+            for (var i = start; i < start + count; i++) expected.Add(i);
             Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void Start_Twenty() {
-            var result = Generator.Incrementer(20).Take(500);
-            var expected = Enumerable.Range(20, 500);
-            Assert.AreEqual(expected, result);
+        public void Start_Long_MaxValue_Throws() {
+            var result = Generator.Incrementer(long.MaxValue).Take(500);
+            Assert.Throws<OverflowException>(() => result.ToArray());
         }
 
         [Test]
         public void To_Int_Max_Value() {
-            const int start = int.MaxValue - 500;
-            var result = Generator.Incrementer(start).Take(500);
-            var expected = Enumerable.Range(start, 500);
+            var count = 500;
+            var start = int.MaxValue - count;
+            var result = Generator.Incrementer(start).Take(count);
+            var expected = new List<int>();
+            for (var i = start; i < start + count; i++) expected.Add(i);
             Assert.AreEqual(expected, result);
         }
 
         [Test]
-        public void To_More_Than_Int_Max_Value() {
+        public void To_Long_Max_Value() {
+            var count = 500;
+            var start = long.MaxValue - count;
+            var result = Generator.Incrementer(start).Take(count);
+            var expected = new List<long>();
+            for (var i = start; i < start + count; i++) expected.Add(i);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void To_More_Than_Int_Max_Value_Throws() {
             const int start = int.MaxValue - 500;
             var result = Generator.Incrementer(start).Take(501);
             Assert.Throws<OverflowException>(() => result.ToArray());
