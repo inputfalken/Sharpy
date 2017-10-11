114012653c586bab4c9770f5762532807a494c5c
Robert Andersson <inputfalken@gmail.com>
2017-10-11 14:13:29 +0200

    Update docfx
    Former-commit-id: 21ed98338d543adebd28489a8bda1e5ddca367e7

--- deployment.ps1
+++ deployment.ps1
@@ -60,8 +60,8 @@ function Get-LocalVersion ([string] $project) {
 }
 # Updates DocFx documentation.
 function Update-GHPages {
-  & nuget install docfx.console -Version 2.24.0
-  & docfx.console.2.24.0\tools\docfx docfx.json
+  & nuget install docfx.console -Version 2.25.1
+  & docfx.console.2.25.1\tools\docfx docfx.json
   if ($lastexitcode -ne 0) {
     throw [System.Exception] "docfx build failed with exit code $lastexitcode."
   }
