7a60205c5367c874f13506e4f53be056f5cf7d1e
Robert Andersson <inputfalken@gmail.com>
2017-10-11 20:31:33 +0200

    Seal classes
    Former-commit-id: 59da0a27e9ac0c75760518076f9ce0d88c9f4023

--- Tests/Sharpy.BuilderTests/Implementations/IntRandomizerTests.cs
+++ Tests/Sharpy.BuilderTests/Implementations/IntRandomizerTests.cs
@@ -11,7 +11,7 @@ namespace Tests.Sharpy.BuilderTests.Implementations {
 
         [Test]
         public void Double_Argument_NotDefaultValue() {
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var list = new List<int>(100);
             for (var i = 0; i < 100; i++) list.Add(intRandomizer.Integer(0, 100));
             Assert.IsFalse(list.All(i => i == 0));
@@ -19,7 +19,7 @@ namespace Tests.Sharpy.BuilderTests.Implementations {
 
         [Test]
         public void No_Argument_NotDefaultValue() {
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var list = new List<int>(100);
             for (var i = 0; i < 100; i++) list.Add(intRandomizer.Integer());
             Assert.IsFalse(list.All(i => i == 0));
@@ -28,7 +28,7 @@ namespace Tests.Sharpy.BuilderTests.Implementations {
         [Test]
         [Repeat(Amount)]
         public void NoArgument() {
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var result = intRandomizer.Integer();
             Assert.IsTrue(result > int.MinValue && result < int.MaxValue);
         }
@@ -37,7 +37,7 @@ namespace Tests.Sharpy.BuilderTests.Implementations {
         [Repeat(Amount)]
         public void One_Arg() {
             const int max = 1000;
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var result = intRandomizer.Integer(max);
             Assert.IsTrue(result >= 0 && result < max);
         }
@@ -45,20 +45,20 @@ namespace Tests.Sharpy.BuilderTests.Implementations {
         [Test]
         [Repeat(Amount)]
         public void One_Arg_MaxValue() {
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var result = intRandomizer.Integer(int.MaxValue);
             Assert.IsTrue(result > 0 && result < int.MaxValue);
         }
 
         [Test]
         public void One_Arg_minusOne_Throws() {
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             Assert.Throws<ArgumentOutOfRangeException>(() => intRandomizer.Integer(-1));
         }
 
         [Test]
         public void One_Argument_NotDefaultValue() {
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var list = new List<int>(100);
             for (var i = 0; i < 100; i++) list.Add(intRandomizer.Integer(100));
             Assert.IsFalse(list.All(i => i == 0));
@@ -69,7 +69,7 @@ namespace Tests.Sharpy.BuilderTests.Implementations {
         public void Two_Args() {
             const int min = -1000;
             const int max = 2000;
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var result = intRandomizer.Integer(min, max);
             Assert.IsTrue(result >= min && result < max);
         }
@@ -78,14 +78,14 @@ namespace Tests.Sharpy.BuilderTests.Implementations {
         public void Two_Args_Maxx_Less_Than_Min_Throws() {
             const int min = -1000;
             const int max = -2000;
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             Assert.Throws<ArgumentOutOfRangeException>(() => intRandomizer.Integer(min, max));
         }
 
         [Test]
         [Repeat(Amount)]
         public void Two_Args_MinValue_Zero() {
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var result = intRandomizer.Integer(int.MinValue, 0);
             Assert.IsTrue(result < 0);
         }
@@ -95,7 +95,7 @@ namespace Tests.Sharpy.BuilderTests.Implementations {
         public void Two_Args_Thousand_And_TwoThousand() {
             const int min = 1000;
             const int max = 2000;
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var result = intRandomizer.Integer(min, max);
             Assert.IsTrue(result >= min && result < max);
         }
@@ -103,7 +103,7 @@ namespace Tests.Sharpy.BuilderTests.Implementations {
         [Test]
         [Repeat(Amount)]
         public void Two_Args_Zero_And_MaxValue() {
-            var intRandomizer = new IntRandomizer(new Random());
+            var intRandomizer = new IntegerRandomizer(new Random());
             var result = intRandomizer.Integer(0, int.MaxValue);
             Assert.IsTrue(result > 0 && result < int.MaxValue);
         }
