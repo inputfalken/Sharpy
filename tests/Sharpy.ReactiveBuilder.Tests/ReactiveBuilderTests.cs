1d2ed6f3b639791d89760c9be3c599640c8ce9f9
Robert Andersson <inputfalken@gmail.com>
2017-12-02 15:06:02 +0100

    Add project 'Sharpy.ReactiveBuilder'
    Former-commit-id: 7b14e9a0db9c22617357c395b0af31b0bf722712

---
+++ tests/Sharpy.ReactiveBuilder.Tests/ReactiveBuilderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sharpy.ReactiveBuilder.Tests {
+    [TestFixture]
+    internal class ReactiveBuilderTests {
+        [Test]
+        public void Not_Null() => Assert.NotNull(new Builder.Builder().Observable(b => b.Integer()));
+
+        [Test]
+        public void Not_Null_With_Counter() => Assert.NotNull(new Builder.Builder().Observable((b, i) => b.Integer()));
+
+        [Test]
+        public void Null_Builder_Throws() {
+            Builder.Builder builder = null;
+            Assert.Throws<ArgumentNullException>(() => builder.Observable(b => b.Integer()));
+        }
+
+        [Test]
+        public void Null_Selector_Throws() {
+            Func<Builder.Builder, int> selector = null;
+            Assert.Throws<ArgumentNullException>(() => new Builder.Builder().Observable(selector));
+        }
+
+        [Test]
+        public void Null_Selector_With_Counter_Throws() {
+            Func<Builder.Builder, int, int> selector = null;
+            Assert.Throws<ArgumentNullException>(() => new Builder.Builder().Observable(selector));
+        }
+    }
+}
