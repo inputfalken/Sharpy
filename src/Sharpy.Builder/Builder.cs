2f6d7d64995f85661fb1c712e751cbde26eff416
Robert Andersson <robert.andersson.swe@gmail.com>
2020-10-20 23:37:27 +0200

    Adds additonal overloads for creating DateTime based on min and max values

--- src/Sharpy.Builder/Builder.cs
+++ src/Sharpy.Builder/Builder.cs
@@ -83,6 +83,10 @@ namespace Sharpy.Builder {
         /// <inheritdoc />
         public DateTime Date() => _dateProvider.Date();
 
+        public DateTime Date(DateTime max) => _dateProvider.Date(max);
+
+        public DateTime Date(DateTime min, DateTime max) => _dateProvider.Date(min, max);
+
         /// <inheritdoc />
         public double Double() => _doubleProvider.Double();
 
--- src/Sharpy.Builder/Implementation/DateRandomizer.cs
+++ src/Sharpy.Builder/Implementation/DateRandomizer.cs
@@ -2,11 +2,13 @@
 using Sharpy.Builder.IProviders;
 using static System.DateTime;
 
-namespace Sharpy.Builder.Implementation {
+namespace Sharpy.Builder.Implementation
+{
     /// <summary>
     ///     Randomizes <see cref="DateTime" /> elements by using <see cref="Random" />.
     /// </summary>
-    public sealed class DateRandomizer : IDateProvider {
+    public sealed class DateRandomizer : IDateProvider
+    {
         private readonly Random _random;
 
         /// <summary>
@@ -15,63 +17,108 @@ namespace Sharpy.Builder.Implementation {
         /// <param name="random"></param>
         public DateRandomizer(Random random) => _random = random;
 
-        /// <summary>
-        ///     <para>
-        ///         Randomizes a <see cref="DateTime" /> based on argument <paramref name="age" />.
-        ///     </para>
-        /// </summary>
-        /// <param name="age">
-        ///     The age of the date.
-        /// </param>
-        /// <returns>
-        ///     A randomized <see cref="DateTime" /> based on <paramref name="age" />.
-        /// </returns>
-        public DateTime DateByAge(int age) {
+        /// <inheritdoc cref="IDateProvider.DateByAge(int)"/>
+        public DateTime DateByAge(int age)
+        {
             if (age < 0)
                 throw new ArgumentException($"{nameof(age)} cannot be negative");
+
             var date = Now
                 .AddYears(-age)
                 .AddMonths(-_random.Next(1, 12) + 1);
 
             return date
-                .AddDays(-_random.Next(1, DaysInMonth(date.Year, date.Month)) + 1)
-                .AddHours(-_random.Next(1, 24))
-                .AddMinutes(-_random.Next(1, 60))
-                .AddSeconds(-_random.Next(1, 60));
+                .AddDays(-RandomizeDay(date.Year, date.Month))
+                .AddHours(RandomizeHour())
+                .AddMinutes(RandomizeMinute())
+                .AddSeconds(RandomizeSecond())
+                .AddMilliseconds(RandomizeMilliSecond());
         }
-        /// <summary>
-        ///     <para>
-        ///         Randomizes a <see cref="DateTime" /> based on argument <paramref name="year" />.
-        ///     </para>
-        /// </summary>
-        /// <param name="year">
-        ///     The year of the date.
-        /// </param>
-        /// <returns>
-        ///     A <see cref="DateTime" /> with the argument <paramref name="year" /> as the year.
-        ///     The month and date has been randomized.
-        /// </returns>
-        public DateTime DateByYear(int year) {
+
+        /// <inheritdoc cref="IDateProvider.DateByYear(int)"/>
+        public DateTime DateByYear(int year)
+        {
             if (year <= MinValue.Year) throw new ArgumentException($"{nameof(year)} cannot be negative");
-            var month = _random.Next(1, 13);
+            var month = RandomizeMonth();
             return new DateTime(
                 year,
                 month,
-                DaysInMonth(year, month),
-                _random.Next(1, 24),
-                _random.Next(1, 60),
-                _random.Next(1, 60)
+                RandomizeDay(year, month),
+                RandomizeHour(),
+                RandomizeMinute(),
+                RandomizeSecond(),
+                RandomizeMilliSecond()
             );
         }
 
-        /// <summary>
-        ///     <para>
-        ///         Randomizes a <see cref="DateTime" /> between 20 and 61 years old.
-        ///     </para>
-        /// </summary>
-        /// <returns>
-        ///     A randomized <see cref="DateTime" />.
-        /// </returns>
-        public DateTime Date() => DateByAge(_random.Next(20, 61));
+        private int RandomizeDay(int year, int month) => _random.Next(1, DaysInMonth(year, month) + 1);
+        private int RandomizeMonth() => _random.Next(1, 13);
+        private int RandomizeHour() => _random.Next(0, 24);
+        private int RandomizeMinute() => _random.Next(0, 60);
+        private int RandomizeSecond() => _random.Next(0, 60);
+        private int RandomizeMilliSecond() => _random.Next(0, 1000);
+
+        public DateTime Date() => Date(MinValue, MaxValue);
+
+        /// <inheritdoc cref="IDateProvider.Date(DateTime)"/>
+        public DateTime Date(DateTime max)
+        {
+            return Date(MinValue, max);
+        }
+
+        /// <inheritdoc cref="IDateProvider.Date(DateTime, DateTime)"/>
+        public DateTime Date(DateTime min, DateTime max)
+        {
+            var isSameYear = min.Year == max.Year;
+            var year = isSameYear ? min.Year : _random.Next(min.Year, max.Year);
+
+            var isSameMonth = min.Month == max.Month;
+            var month = isSameYear
+                ? isSameMonth
+                    ? min.Month
+                    : _random.Next(1, max.Month)
+                : RandomizeMonth();
+
+            var isSameDay = min.Day == max.Day;
+            var day = isSameYear && isSameMonth
+                ? isSameDay
+                    ? min.Day
+                    : _random.Next(1, max.Day)
+                : RandomizeDay(year, month);
+
+            var isSameHour = min.Hour == max.Hour;
+            var hour = isSameYear && isSameMonth && isSameDay
+                ? isSameHour
+                    ? min.Hour
+                    : _random.Next(1, max.Hour)
+                : RandomizeHour();
+
+            var isSameMinute = min.Minute == max.Minute;
+            var minute = isSameYear && isSameMonth && isSameDay && isSameHour
+                ? isSameMinute
+                    ? min.Minute
+                    : _random.Next(1, max.Minute)
+                : RandomizeMinute();
+
+            var isSameSecond = min.Second == max.Second;
+
+            var second = isSameYear && isSameMonth && isSameDay && isSameHour
+                ? isSameSecond
+                    ? min.Second
+                    : _random.Next(1, max.Second)
+                : RandomizeSecond();
+
+
+            var isSameMillisSecond = min.Millisecond == max.Millisecond;
+
+            var milliSecond = isSameYear && isSameMonth && isSameDay && isSameHour && isSameMinute && isSameSecond
+                ? isSameMillisSecond
+                    ? min.Millisecond
+                    : _random.Next(1, max.Millisecond)
+                : RandomizeMilliSecond();
+
+
+            return new DateTime(year, month, day, hour, minute, second, milliSecond);
+        }
     }
 }
