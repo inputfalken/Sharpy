d92f617029fcdc8ba4fcedc369286f08c299342c
Robert Andersson <robert.andersson.swe@gmail.com>
2020-12-27 14:35:20 +0100

    Add documentation about how the rules are applied

--- src/RandomExtended/Rule.cs
+++ src/RandomExtended/Rule.cs
@@ -2,6 +2,16 @@ namespace RandomExtended
 {
     /// <summary>
     /// Specifies <see cref="Include"/> or <see cref="Exclude"/> behaviour.
+    /// <remarks>
+    ///   The <see cref="Include"/> <see cref="Rule"/> will always have precedence over <see cref="Exclude"/>, see the example.
+    /// </remarks>
+    /// <example>
+    ///     <code language="c#">
+    ///         var random = new Random();
+    ///         var includePrecedence1 = random.Int(1,2, Rule.Include, Rule.Exclude); // Can either be 1 or 2.
+    ///         var includePrecedence2 = random.Int(1,2, Rule.Exclude, Rule.Include); // Can either be 1 or 2.
+    ///     </code>
+    /// </example>
     /// </summary>
     public enum Rule
     {
